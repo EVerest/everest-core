90109231532f153a43df54253852609a4a7048b1
Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
2021-12-21 16:36:04 +0100

    Add license headers
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>

--- run_hil.sh
+++ run_hil.sh
@@ -1,1 +1,18 @@
+##
+## SPDX-License-Identifier: Apache-2.0
+##
+## Copyright 2020 - 2021 Pionix GmbH and Contributors to EVerest
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
+##
+##     http://www.apache.org/licenses/LICENSE-2.0
+##
+## Unless required by applicable law or agreed to in writing, software
+## distributed under the License is distributed on an "AS IS" BASIS,
+## WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+## See the License for the specific language governing permissions and
+## limitations under the License.
+##
 LD_LIBRARY_PATH=`pwd`/_deps/everest-framework-build/lib dist/bin/manager --log_conf ../config/logging.ini --main_dir dist --schemas_dir dist/schemas --conf ../config/config-hil.json --modules_dir dist/modules --interfaces_dir dist/interfaces
