3d53137309bd83914beff5c4b7bff5b4ea94e7e5
Kai Hermann <kai-uwe.hermann@pionier-manufaktur.de>
2021-12-21 19:18:23 +0100

    Add license headers (#1)
    * Add license headers
    
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
    
    * Updated license header style
    
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>

--- examples/light_switch/async_fsm.cpp
+++ examples/light_switch/async_fsm.cpp
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2020 - 2021 Pionix GmbH and Contributors to EVerest
 #include "async_fsm.hpp"
 
 extern void set_brightness(int value);
--- include/fsm/specialization/async/freertos.hpp
+++ include/fsm/specialization/async/freertos.hpp
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2020 - 2021 Pionix GmbH and Contributors to EVerest
 #ifndef FSM_SPECIALIZATION_FREERTOS_HPP
 #define FSM_SPECIALIZATION_FREERTOS_HPP
 
