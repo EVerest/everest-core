5e2e84cb82569e3e63e0334429d4883bf5fe60af
Kai Hermann <kai-uwe.hermann@pionier-manufaktur.de>
2022-01-12 09:29:15 +0100

    Fix docker dependencies (#4)
    * Add missing dependencies to Dockerfiles
    
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
    
    * Do not hide git clone erros
    
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>

--- everest-cpp/init.sh
+++ everest-cpp/init.sh
@@ -4,7 +4,9 @@
 ## Copyright 2020 - 2021 Pionix GmbH and Contributors to EVerest
 ##
 echo "Installing required tools, initializing workspace, changing into everest-core/build"
-git clone git@github.com:EVerest/everest-dev-environment.git &> /dev/null || true
+git clone git@github.com:EVerest/everest-dev-environment.git
+echo "PATH=/home/docker/.local/bin:\$PATH" >> /home/docker/.profile
+. /home/docker/.profile
 (cd everest-dev-environment/dependency_manager && python3 -m pip install .)
 edm --register-cmake-module
 edm --config everest-dev-environment/everest-complete.yaml --workspace .
