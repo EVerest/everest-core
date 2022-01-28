2d1bc5c221fa4777f6852943a0fdf54bc9e94022
Kai Hermann <kai-uwe.hermann@pionier-manufaktur.de>
2022-01-28 16:42:51 +0100

    Various improvements to init.sh and dockerfiles (#6)
    * Add gitignore for everest-cpp directory content
    * Improve init.sh error handling and add info output for users
    * Fix typos in README
    * Added Ubuntu 20.04 based devcontainer
    * Use docker ENV instead of custom code for environment variables
    * Change devcontainer names to include used distribution
    * Modernize terminal integration
    * Show a message to the user explaining how to initialize the workspace
    * Fix devcontainer json
    * Improve apt-get install directives in Ubuntu 20.04 Dockerfile
    * Modernize call to nproc in init.sh
    * Improve apt-get install directives in Debian dockerfile
    * Split BASE_IMAGE in in BASE_IMAGE and BASE_IMAGE_VERSION
    
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>

--- everest-cpp/README.md
+++ everest-cpp/README.md
@@ -1,11 +1,10 @@
-To initialize the EVerest workspace you can source to *[init.sh](./init.sh)* file:
+To initialize the EVerest workspace you can source the *[init.sh](./init.sh)* file:
 
 ```bash
-chmod u+x init.sh
 . init.sh
 ```
 
-The working directory will be changed to everest-core/build. Here you can use cmake and make to build the project:
+The working directory will be changed to *everest-core/build*. Here you can use cmake and make to build the project:
 
 ```bash
 cmake .. && make install
--- everest-cpp/init.sh
+++ everest-cpp/init.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 ##
 ## SPDX-License-Identifier: Apache-2.0
-## Copyright 2020 - 2021 Pionix GmbH and Contributors to EVerest
+## Copyright 2020 - 2022 Pionix GmbH and Contributors to EVerest
 ##
 echo "Installing required tools, initializing workspace, changing into everest-core/build"
-git clone git@github.com:EVerest/everest-dev-environment.git
-echo "PATH=/home/docker/.local/bin:\$PATH" >> /home/docker/.profile
-. /home/docker/.profile
+if [ -d "everest-dev-environment" ] ; then
+    echo "Directory 'everest-dev-environment' already exists, trying to update its contents..."
+    (cd everest-dev-environment && git pull)
+else
+    git clone git@github.com:EVerest/everest-dev-environment.git || { echo "Could not clone everest-dev-environment. Did you add your SSH key on GitHub and made it available to ssh-agent?"; return; }
+fi
 (cd everest-dev-environment/dependency_manager && python3 -m pip install .)
 edm --register-cmake-module
 edm --config everest-dev-environment/everest-complete.yaml --workspace .
@@ -14,3 +17,7 @@ edm --update
 (cd everest-utils/ev-dev-tools && python3 -m pip install .)
 mkdir -p everest-core/build
 cd everest-core/build
+echo ""
+echo "You can now build everest-core with the following commands:"
+echo ""
+echo "cmake .. && make install -j$(nproc)"
