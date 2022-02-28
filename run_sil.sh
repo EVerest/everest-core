041ab1ceffef3cf6b28ff0c506005711b0b9303d
Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
2022-02-28 12:32:02 +0100

    Add ability to pass arbitrary command line flags to run_sil.sh
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>

--- run_sil.sh
+++ run_sil.sh
@@ -2,4 +2,4 @@
 ## SPDX-License-Identifier: Apache-2.0
 ## Copyright 2020 - 2021 Pionix GmbH and Contributors to EVerest
 ##
-LD_LIBRARY_PATH=`pwd`/_deps/everest-framework-build/lib dist/bin/manager --log_conf ../config/logging.ini --main_dir dist --schemas_dir dist/schemas --conf ../config/config-sil.json --modules_dir dist/modules --interfaces_dir dist/interfaces
+LD_LIBRARY_PATH=`pwd`/_deps/everest-framework-build/lib dist/bin/manager --log_conf ../config/logging.ini --main_dir dist --schemas_dir dist/schemas --conf ../config/config-sil.json --modules_dir dist/modules --interfaces_dir dist/interfaces $@
