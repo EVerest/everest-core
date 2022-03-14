c58dd20f25940142623b0f80fec8a4784628239b
aw <75637418+a-w50@users.noreply.github.com>
2022-03-14 15:25:52 +0100

    Refactor EvseSlac module (#26)
    - replaced open-plc-utils with libslac
    - using libfsm for state machine handling
    
    Signed-off-by: aw <aw@pionix.de>
    Signed-off-by: Cornelius Claussen <cc@pionix.de>
    Co-authored-by: Cornelius Claussen <cc@pionix.de>

---
+++ modules/EvseSlac/main/slacImpl.hpp
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Pionix GmbH and Contributors to EVerest
+#ifndef MAIN_SLAC_IMPL_HPP
+#define MAIN_SLAC_IMPL_HPP
+
+//
+// AUTO GENERATED - MARKED REGIONS WILL BE KEPT
+// template version 1
+//
+
+#include <generated/slac/Implementation.hpp>
+
+#include "../EvseSlac.hpp"
+
+// ev@75ac1216-19eb-4182-a85c-820f1fc2c091:v1
+// insert your custom include headers here
+// ev@75ac1216-19eb-4182-a85c-820f1fc2c091:v1
+
+namespace module {
+namespace main {
+
+struct Conf {
+    std::string device;
+    std::string evse_id;
+    std::string nid;
+    int number_of_sounds;
+};
+
+class slacImpl : public slacImplBase {
+public:
+    slacImpl() = delete;
+    slacImpl(Everest::ModuleAdapter* ev, const Everest::PtrContainer<EvseSlac>& mod, Conf& config) :
+        slacImplBase(ev, "main"), mod(mod), config(config){};
+
+    // ev@8ea32d28-373f-4c90-ae5e-b4fcc74e2a61:v1
+    // insert your public definitions here
+    // ev@8ea32d28-373f-4c90-ae5e-b4fcc74e2a61:v1
+
+protected:
+    // command handler functions (virtual)
+    virtual void handle_reset() override;
+    virtual bool handle_enter_bcd() override;
+    virtual bool handle_leave_bcd() override;
+    virtual bool handle_error_sequence_reset(bool& successful) override;
+    virtual bool handle_dlink_terminate() override;
+    virtual bool handle_dlink_error() override;
+    virtual bool handle_dlink_pause() override;
+
+    // ev@d2d1847a-7b88-41dd-ad07-92785f06f5c4:v1
+    // insert your protected definitions here
+    // ev@d2d1847a-7b88-41dd-ad07-92785f06f5c4:v1
+
+private:
+    const Everest::PtrContainer<EvseSlac>& mod;
+    const Conf& config;
+
+    virtual void init() override;
+    virtual void ready() override;
+
+    // ev@3370e4dd-95f4-47a9-aaec-ea76f34a66c9:v1
+    void run();
+    // ev@3370e4dd-95f4-47a9-aaec-ea76f34a66c9:v1
+};
+
+// ev@3d7da0ad-02c2-493d-9920-0bbbd56b9876:v1
+// insert other definitions here
+// ev@3d7da0ad-02c2-493d-9920-0bbbd56b9876:v1
+
+} // namespace main
+} // namespace module
+
+#endif // MAIN_SLAC_IMPL_HPP
