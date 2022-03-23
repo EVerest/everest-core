4cfcbe47bc62c6110f9bd4bf23b851d454f0f2ed
LAD101work <96466764+LAD101work@users.noreply.github.com>
2022-03-23 07:17:21 +0000

    Switch strings to single quotes
    Signed-off-by: LAD101work <96466764+LAD101work@users.noreply.github.com>

--- modules/JsTibber/index.js
+++ modules/JsTibber/index.js
@@ -27,14 +27,14 @@ async function fetch_tibber_api_data(mod) {
   let apiKeyField = `Bearer ${mod.config.impl.main.api_key}`;
   let requestConfig = {
     headers: {
-      "authorization": apiKeyField,
-      "Content-Type": "application/json"
+      'authorization': apiKeyField,
+      'Content-Type': 'application/json'
     }
   }
   let postData = {
     query: graphQLQuery
   }
-  await axios.post("https://api.tibber.com/v1-beta/gql", postData, requestConfig).then((response) => {
+  await axios.post('https://api.tibber.com/v1-beta/gql', postData, requestConfig).then((response) => {
 
     if (response === undefined || response.status === undefined || response.status !== 200) {
       evlog.error('Could not retrieve response data from Tibber API');
@@ -45,17 +45,17 @@ async function fetch_tibber_api_data(mod) {
     let tomorrow;
 
     // validate input from tibber
-    if (response.hasOwnProperty("data") &&
-      response.data.hasOwnProperty("data") &&
-      response.data.data.hasOwnProperty("viewer") &&
-      response.data.data.viewer.hasOwnProperty("homes") &&
+    if (response.hasOwnProperty('data') &&
+      response.data.hasOwnProperty('data') &&
+      response.data.data.hasOwnProperty('viewer') &&
+      response.data.data.viewer.hasOwnProperty('homes') &&
       Array.isArray(response.data.data.viewer.homes) &&
-      !(response.data.data.viewer.homes[0] === "undefined") &&
-      response.data.data.viewer.homes[0].hasOwnProperty("currentSubscription") &&
-      response.data.data.viewer.homes[0].currentSubscription.hasOwnProperty("priceInfo") &&
-      response.data.data.viewer.homes[0].currentSubscription.priceInfo.hasOwnProperty("today") &&
+      !(response.data.data.viewer.homes[0] === 'undefined') &&
+      response.data.data.viewer.homes[0].hasOwnProperty('currentSubscription') &&
+      response.data.data.viewer.homes[0].currentSubscription.hasOwnProperty('priceInfo') &&
+      response.data.data.viewer.homes[0].currentSubscription.priceInfo.hasOwnProperty('today') &&
       Array.isArray(response.data.data.viewer.homes[0].currentSubscription.priceInfo.today) &&
-      response.data.data.viewer.homes[0].currentSubscription.priceInfo.hasOwnProperty("tomorrow") &&
+      response.data.data.viewer.homes[0].currentSubscription.priceInfo.hasOwnProperty('tomorrow') &&
       Array.isArray(response.data.data.viewer.homes[0].currentSubscription.priceInfo.tomorrow)) {
       today = response.data.data.viewer.homes[0].currentSubscription.priceInfo.today;
       tomorrow = response.data.data.viewer.homes[0].currentSubscription.priceInfo.tomorrow;
@@ -70,23 +70,23 @@ async function fetch_tibber_api_data(mod) {
 
     for (const response_entry of today.concat(tomorrow)) {
       // validate input from tibber
-      if (response_entry.hasOwnProperty("startsAt") &&
-        response_entry.hasOwnProperty("total") &&
-        response_entry.hasOwnProperty("currency") &&
-        typeof response_entry.currency === "string" &&
+      if (response_entry.hasOwnProperty('startsAt') &&
+        response_entry.hasOwnProperty('total') &&
+        response_entry.hasOwnProperty('currency') &&
+        typeof response_entry.currency === 'string' &&
         response_entry.currency.length == 3 &&
-        typeof response_entry.total === "number" &&
-        typeof response_entry.startsAt === "string") {
+        typeof response_entry.total === 'number' &&
+        typeof response_entry.startsAt === 'string') {
         // create one entry for the schedule
         let entry = {};
-        entry["timestamp"] = new Date(response_entry.startsAt).toISOString();
-        entry["price_per_kwh"] = {
+        entry['timestamp'] = new Date(response_entry.startsAt).toISOString();
+        entry['price_per_kwh'] = {
           // Tibber returns the total energy cost including taxes and fees. Add constant offset if needed for other costs.
           value: response_entry.total + mod.config.impl.main.additional_cost_per_kwh,
           currency: response_entry.currency
         }
         // copy into schedule_import array
-        schedule["schedule_import"].push(entry);
+        schedule['schedule_import'].push(entry);
       } else {
         evlog.error('Received malformed entry in today from Tibber API');
       }
