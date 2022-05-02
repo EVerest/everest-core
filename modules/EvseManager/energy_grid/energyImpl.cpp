8ce0436a479a83ab015a3c7f57402a52f3d42627
Cornelius Claussen <cc@pionix.de>
2022-05-02 12:51:34 +0200

    fix resume pause
    Signed-off-by: Cornelius Claussen <cc@pionix.de>

--- modules/EvseManager/energy_grid/energyImpl.cpp
+++ modules/EvseManager/energy_grid/energyImpl.cpp
@@ -36,16 +36,18 @@ void energyImpl::init() {
         updateAndPublishEnergyObject();
     });
 
-    mod->mqtt.subscribe("/external/" + mod->info.id + ":" + mod->info.name + "/cmd/set_price_limit", [this](json lim) { 
+    mod->mqtt.subscribe("/external/" + mod->info.id + ":" + mod->info.name + "/cmd/set_price_limit", [this](json lim) {
         std::string sLim = lim;
         double new_price_limit = std::stod(sLim);
 
-        EVLOG(debug) << "price limit changed to: " << new_price_limit << " EUR / kWh"; // TODO(LAD): adapt to other currencies
+        EVLOG(debug) << "price limit changed to: " << new_price_limit
+                     << " EUR / kWh"; // TODO(LAD): adapt to other currencies
 
         // update price limits
         if (new_price_limit > 0.0F) {
             // save price limit to not be lost on switching to manual limits
-            _price_limit_previous_value = new_price_limit;   // TODO(LAD): add storage on more permanent medium (config/hdd?)
+            _price_limit_previous_value =
+                new_price_limit; // TODO(LAD): add storage on more permanent medium (config/hdd?)
             _price_limit = new_price_limit;
             _optimizer_mode = EVSE_OPTIMIZER_MODE_PRICE_DRIVEN;
             EVLOG(debug) << "switched to \"price_driven\" optimizer mode";
@@ -58,20 +60,21 @@ void energyImpl::init() {
         updateAndPublishEnergyObject();
     });
 
-    mod->mqtt.subscribe("/external/" + mod->info.id + ":" + mod->info.name + "/cmd/switch_optimizer_mode", [this](json mode) { 
-        if (mode == EVSE_OPTIMIZER_MODE_MANUAL_LIMITS) {
-            _optimizer_mode = EVSE_OPTIMIZER_MODE_MANUAL_LIMITS;
-            EVLOG(debug) << "switched to \"manual_limits\" optimizer mode";
-        } else if (mode == EVSE_OPTIMIZER_MODE_PRICE_DRIVEN) {
-            _optimizer_mode = EVSE_OPTIMIZER_MODE_PRICE_DRIVEN;
-            EVLOG(debug) << "switched to \"price_driven\" optimizer mode";
-        } else {
-            // error
-            EVLOG(error) << "received unknown optimizer mode: " << mode;
-        }
+    mod->mqtt.subscribe("/external/" + mod->info.id + ":" + mod->info.name + "/cmd/switch_optimizer_mode",
+                        [this](json mode) {
+                            if (mode == EVSE_OPTIMIZER_MODE_MANUAL_LIMITS) {
+                                _optimizer_mode = EVSE_OPTIMIZER_MODE_MANUAL_LIMITS;
+                                EVLOG(debug) << "switched to \"manual_limits\" optimizer mode";
+                            } else if (mode == EVSE_OPTIMIZER_MODE_PRICE_DRIVEN) {
+                                _optimizer_mode = EVSE_OPTIMIZER_MODE_PRICE_DRIVEN;
+                                EVLOG(debug) << "switched to \"price_driven\" optimizer mode";
+                            } else {
+                                // error
+                                EVLOG(error) << "received unknown optimizer mode: " << mode;
+                            }
 
-        updateAndPublishEnergyObject();
-    });
+                            updateAndPublishEnergyObject();
+                        });
 }
 
 void energyImpl::ready() {
@@ -95,7 +98,7 @@ void energyImpl::handle_enforce_limits(std::string& uuid, Object& limits_import,
     // is it for me?
     if (uuid == energy["uuid"]) {
         // apply enforced limits
-  
+
         // 3 or one phase only when we have the capability to actually switch during charging?
         // if we have capability we'll switch while charging. otherwise in between sessions.
         // LAD: FIXME implement phase count limiting here
@@ -107,10 +110,10 @@ void energyImpl::handle_enforce_limits(std::string& uuid, Object& limits_import,
         // apply local limit
         if (mod->getLocalMaxCurrentLimit() < limit) {
             limit = mod->getLocalMaxCurrentLimit();
-        }            
+        }
 
         // apply enforced AC current limits
-        if (!limits_import["request_parameters"].is_null() && 
+        if (!limits_import["request_parameters"].is_null() &&
             !limits_import["request_parameters"]["ac_current_A"].is_null() &&
             !limits_import["request_parameters"]["ac_current_A"]["current_A"].is_null() &&
             limits_import["request_parameters"]["ac_current_A"]["current_A"] < limit) {
@@ -120,25 +123,25 @@ void energyImpl::handle_enforce_limits(std::string& uuid, Object& limits_import,
         // update limit at the charger
         if (!limits_import["valid_until"].is_null()) {
             mod->charger->setMaxCurrent(limit, from_rfc3339(limits_import["valid_until"]));
-            mod->charger->resumeCharging();
+            if (limit > 0)
+                mod->charger->resumeChargingPowerAvailable();
         }
 
         // set phase count limits
         auto phase_count_limit = json::object();
         phase_count_limit["max_phase_count"] = mod->get_hw_capabilities()["max_phase_count"];
         phase_count_limit["min_phase_count"] = mod->get_hw_capabilities()["min_phase_count"];
-        phase_count_limit["supports_changing_phases_during_charging"] = mod->get_hw_capabilities()["supports_changing_phases_during_charging"];
+        phase_count_limit["supports_changing_phases_during_charging"] =
+            mod->get_hw_capabilities()["supports_changing_phases_during_charging"];
 
         if (energy["energy_usage"]["power_W"]["total"] > 0) {
             if (phase_count_limit["supports_changing_phases_during_charging"] != true) {
                 EVLOG(debug) << "Cannot apply phase limit: Setting during charging not supported!";
-            }
-            else {
+            } else {
                 // set phase count
                 // ---not implemented---
             }
-        }
-        else {
+        } else {
             // set phase count
             // ---not implemented---
         }
@@ -149,14 +152,14 @@ void energyImpl::handle_enforce_limits(std::string& uuid, Object& limits_import,
     // if not, ignore as we do not have children.
 }
 
-void energyImpl::initializeEnergyObject(){
+void energyImpl::initializeEnergyObject() {
     energy["node_type"] = "Evse";
-    
+
     // UUID must be unique also beyond this charging station -> will be handled on framework level and above later
     energy["uuid"] = mod->info.id;
-}   
+}
 
-void energyImpl::updateAndPublishEnergyObject(){
+void energyImpl::updateAndPublishEnergyObject() {
 
     // update optimizer mode
     if (_optimizer_mode == EVSE_OPTIMIZER_MODE_MANUAL_LIMITS) {
@@ -175,7 +178,8 @@ void energyImpl::updateAndPublishEnergyObject(){
         {
             std::lock_guard<std::mutex> lock(this->energy_mutex);
             if (energy.contains("schedule_import")) {
-                energy.at("schedule_import").at(0).at("request_parameters").at("ac_current_A").at("max_current_A") = mod->getLocalMaxCurrentLimit();
+                energy.at("schedule_import").at(0).at("request_parameters").at("ac_current_A").at("max_current_A") =
+                    mod->getLocalMaxCurrentLimit();
             } else {
                 return;
             }
@@ -188,7 +192,8 @@ void energyImpl::updateAndPublishEnergyObject(){
             energy["optimizer_target"] = json::object();
             energy["optimizer_target"]["price_limit"] = _price_limit;
             if (energy.contains("schedule_import")) {
-                energy.at("schedule_import").at(0).at("request_parameters").at("ac_current_A").at("max_current_A") = mod->getLocalMaxCurrentLimit();
+                energy.at("schedule_import").at(0).at("request_parameters").at("ac_current_A").at("max_current_A") =
+                    mod->getLocalMaxCurrentLimit();
             } else {
                 return;
             }
@@ -197,7 +202,7 @@ void energyImpl::updateAndPublishEnergyObject(){
 
     // publish to energy tree
     publish_energy(energy);
-}   
+}
 
 } // namespace energy_grid
 } // namespace module
