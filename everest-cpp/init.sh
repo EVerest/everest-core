f16edcf15cf00c8fb0163579e36377825cde534f
Kai Hermann <kai-uwe.hermann@pionix.de>
2022-05-09 15:25:47 +0200

    Add readonly init script that uses https git urls (#17)
    * Add readonly init script that uses https git urls
    
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
    
    * Detect if git clone failed because of a missing public key on GitHub
    
    Fallback automatically to https protocol for checkout and warn the user.
    
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
    
    * Remove init-readonly.sh from .gitignore
    
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
    
    * Use ssh -T to check if a ssh key has been uploaded to github
    
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>

--- everest-cpp/init.sh
+++ everest-cpp/init.sh
@@ -4,15 +4,32 @@
 ## Copyright 2020 - 2022 Pionix GmbH and Contributors to EVerest
 ##
 echo "Installing required tools, initializing workspace, changing into everest-core/build"
+GIT_PROTOCOL="git"
 if [ -d "everest-dev-environment" ] ; then
     echo "Directory 'everest-dev-environment' already exists, trying to update its contents..."
     (cd everest-dev-environment && git pull)
 else
-    git clone git@github.com:EVerest/everest-dev-environment.git || { echo "Could not clone everest-dev-environment. Did you add your SSH key on GitHub and made it available to ssh-agent?"; return; }
+    ssh -T git@github.com
+    SSH_TEST_RETVAL=$?
+    if [ $SSH_TEST_RETVAL -ne 1 ] ; then
+        echo "Could not clone everest-dev-environment with git protocol."
+        echo "Did you add your SSH key on GitHub and made it available to ssh-agent?"
+        echo "Trying to continue with https protocol."
+        GIT_PROTOCOL="https"
+    fi
+    if [ "${GIT_PROTOCOL}" = "https" ] ; then
+        git clone https://github.com/EVerest/everest-dev-environment.git || { echo "Could not clone everest-dev-environment."; return; }
+    else
+        git clone git@github.com:EVerest/everest-dev-environment.git
+    fi
 fi
 (cd everest-dev-environment/dependency_manager && python3 -m pip install .)
 edm --register-cmake-module
-edm --config everest-dev-environment/everest-complete.yaml --workspace .
+if [ "${GIT_PROTOCOL}" = "https" ] ; then
+    edm --config everest-dev-environment/everest-complete-readonly.yaml --workspace .
+else
+    edm --config everest-dev-environment/everest-complete.yaml --workspace .
+fi
 edm --update
 (cd everest-utils/ev-dev-tools && python3 -m pip install .)
 mkdir -p everest-core/build
