f0bdc4db6bdb271b458b55ce044fbe81a7cb2640
Cornelius Claussen <cc@pionix.de>
2022-09-22 12:44:59 +0200

    Improve code, no change in functionality
    Signed-off-by: Cornelius Claussen <cc@pionix.de>

--- modules/SerialCommHub/main/serial_communication_hubImpl.cpp
+++ modules/SerialCommHub/main/serial_communication_hubImpl.cpp
@@ -16,6 +16,109 @@
 namespace module {
 namespace main {
 
+static everest::connection::SerialDeviceConfiguration::BaudRate baudrate_from_int(int baudrate) {
+    switch (baudrate) {
+    case 1200:
+        return everest::connection::SerialDeviceConfiguration::BaudRate::Baud_1200;
+        break;
+    case 2400:
+        return everest::connection::SerialDeviceConfiguration::BaudRate::Baud_2400;
+        break;
+    case 4800:
+        return everest::connection::SerialDeviceConfiguration::BaudRate::Baud_4800;
+        break;
+    case 9600:
+        return everest::connection::SerialDeviceConfiguration::BaudRate::Baud_9600;
+        break;
+    case 19200:
+        return everest::connection::SerialDeviceConfiguration::BaudRate::Baud_19200;
+        break;
+    case 38400:
+        return everest::connection::SerialDeviceConfiguration::BaudRate::Baud_38400;
+        break;
+    case 57600:
+        return everest::connection::SerialDeviceConfiguration::BaudRate::Baud_57600;
+        break;
+    case 115200:
+        return everest::connection::SerialDeviceConfiguration::BaudRate::Baud_115200;
+        break;
+    case 230400:
+        return everest::connection::SerialDeviceConfiguration::BaudRate::Baud_230400;
+        break;
+    default:
+        EVLOG_error << "Error setting up connection to serialport: Undefined baudrate! Defaulting to 9600.\n";
+        return everest::connection::SerialDeviceConfiguration::BaudRate::Baud_9600;
+        break;
+    }
+}
+
+template <typename T, typename U> static void append_array(std::vector<T>& m, const std::vector<U>& a) {
+    for (auto it = a.begin(); it != a.end(); ++it)
+        m.push_back(*it);
+}
+
+static bool check_crc(const uint8_t* buffer, size_t len) {
+    if (len <= 2)
+        return false;
+    // check crc
+    const uint16_t crc_sum = everest::modbus::utils::calcCRC_16_ANSI(buffer, len - 2);
+    const uint8_t crc_low = (uint8_t)(crc_sum >> 8);
+    const uint8_t crc_high = (uint8_t)(crc_sum & 0x00FF);
+
+    if (crc_low == buffer[len - 2] && crc_high == buffer[len - 1])
+        return true;
+    else
+        return false;
+}
+
+// Helper functions
+
+/*
+ This function converts from a 8 bit raw binary vector to 16 bit little endian ints stored in an vector<uint16_t>.
+*/
+static std::vector<uint16_t> to_little_endian_16(const std::vector<uint8_t>& response) {
+    std::vector<uint16_t> r;
+
+    for (auto it = response.begin(); it != response.end();) {
+        uint8_t hi = *it++;
+        if (it == response.end())
+            break;
+        uint8_t lo = *it++;
+        r.push_back((uint16_t)(hi << 8) | lo);
+    }
+    return r;
+}
+
+/*
+ This function converts from a 8 bit raw binary vector to 16 bit little endian ints stored in an vector<int>.
+ This is a little awkward but we need a vector<int> instead of vector<uint16_t> in some places because the json types
+ cannot specify the integer size.
+*/
+
+static std::vector<int> to_little_endian_int(const std::vector<uint8_t>& response) {
+    std::vector<int> r;
+    uint8_t hi, lo;
+    for (auto it = response.begin(); it != response.end();) {
+        hi = *it++;
+        if (it == response.end())
+            break;
+        lo = *it++;
+        r.push_back((uint16_t)((hi << 8) | lo));
+    }
+    return r;
+}
+
+static void add_crc(std::vector<uint8_t>& message) {
+    const uint16_t crc_sum = everest::modbus::utils::calcCRC_16_ANSI(message.data(), message.size());
+    const uint8_t crc_low = (uint8_t)(crc_sum >> 8);
+    const uint8_t crc_high = (uint8_t)(crc_sum & 0x00FF);
+
+    message.push_back(crc_low);
+    message.push_back(crc_high);
+}
+
+// Implementation
+
 void serial_communication_hubImpl::init() {
 
     everest::connection::SerialDeviceConfiguration cfg(config.serial_port);
@@ -35,18 +138,17 @@ void serial_communication_hubImpl::init() {
         cfg.set_parity(everest::connection::SerialDeviceConfiguration::Parity::Even);
         break;
     }
-
-    serial_device = std::unique_ptr<everest::connection::SerialDevice>(new everest::connection::SerialDevice(cfg));
-    modbus_connection =
-        std::unique_ptr<everest::connection::RTUConnection>(new everest::connection::RTUConnection(*serial_device));
-    modbus_client =
-        std::unique_ptr<everest::modbus::ModbusRTUClient>(new everest::modbus::ModbusRTUClient(*modbus_connection));
+    serial_device = std::make_unique<everest::connection::SerialDevice>(cfg);
 
     try {
         serial_device->open();
     } catch (const everest::connection::exceptions::tty::tty_error& e) {
         EVLOG_AND_THROW(Everest::EverestConfigError(fmt::format("Cannot open serial port {}.", config.serial_port)));
     }
+
+    modbus_connection = std::make_unique<everest::connection::RTUConnection>(*serial_device);
+
+    modbus_client = std::make_unique<everest::modbus::ModbusRTUClient>(*modbus_connection);
 }
 
 void serial_communication_hubImpl::ready() {
@@ -66,12 +168,7 @@ serial_communication_hubImpl::handle_send_raw(int& target_device_id,
 
     // do we need to add CRC at the end?
     if (add_crc16) {
-        const uint16_t crc_sum = everest::modbus::utils::calcCRC_16_ANSI(message.data(), message.size());
-        const uint8_t crc_low = (uint8_t)(crc_sum >> 8);
-        const uint8_t crc_high = (uint8_t)(crc_sum & 0x00FF);
-
-        message.push_back(crc_low);
-        message.push_back(crc_high);
+        add_crc(message);
     }
 
     int bytes_written = 0;
@@ -104,12 +201,7 @@ types::serial_comm_hub_requests::ResultRaw serial_communication_hubImpl::handle_
 
     // do we need to add CRC at the end?
     if (add_crc16) {
-        const uint16_t crc_sum = everest::modbus::utils::calcCRC_16_ANSI(message.data(), message.size());
-        const uint8_t crc_low = (uint8_t)(crc_sum >> 8);
-        const uint8_t crc_high = (uint8_t)(crc_sum & 0x00FF);
-
-        message.push_back(crc_low);
-        message.push_back(crc_high);
+        add_crc(message);
     }
 
     int bytes_received = 0;
@@ -211,11 +303,11 @@ serial_communication_hubImpl::handle_modbus_read_holding_registers(int& target_d
         } while (!done);
     }
 
-    EVLOG_debug << fmt::format("Process response (size {})", (int)response.size());
+    EVLOG_debug << fmt::format("Process response (size {})", response.size());
     // process response
     if (response.size() > 1) {
         result.status_code = types::serial_comm_hub_requests::StatusCodeEnum::Success;
-        result.value = toLittleEndianInt(response);
+        result.value = to_little_endian_int(response);
     }
     return result;
 }
@@ -277,90 +369,6 @@ types::serial_comm_hub_requests::StatusCodeEnum serial_communication_hubImpl::ha
     EVLOG_debug << fmt::format("Done writing");
 
     return types::serial_comm_hub_requests::StatusCodeEnum::Success;
-}
-
-// Helper functions
-
-std::vector<uint16_t> serial_communication_hubImpl::toLittleEndian16(const std::vector<uint8_t> response) {
-    std::vector<uint16_t> r;
-
-    for (auto it = response.begin(); it != response.end();) {
-        uint8_t hi = *it++;
-        if (it == response.end())
-            break;
-        uint8_t lo = *it++;
-        r.push_back((uint16_t)(hi << 8) | lo);
-    }
-    return r;
-}
-
-std::vector<int> serial_communication_hubImpl::toLittleEndianInt(const std::vector<uint8_t> response) {
-    std::vector<int> r;
-    uint8_t hi, lo;
-    for (auto it = response.begin(); it != response.end();) {
-        hi = *it++;
-        if (it == response.end())
-            break;
-        lo = *it++;
-        r.push_back((uint16_t)((hi << 8) | lo));
-    }
-    return r;
-}
-
-everest::connection::SerialDeviceConfiguration::BaudRate serial_communication_hubImpl::baudrate_from_int(int baudrate) {
-    switch (baudrate) {
-    case 1200:
-        return everest::connection::SerialDeviceConfiguration::BaudRate::Baud_1200;
-        break;
-    case 2400:
-        return everest::connection::SerialDeviceConfiguration::BaudRate::Baud_2400;
-        break;
-    case 4800:
-        return everest::connection::SerialDeviceConfiguration::BaudRate::Baud_4800;
-        break;
-    case 9600:
-        return everest::connection::SerialDeviceConfiguration::BaudRate::Baud_9600;
-        break;
-    case 19200:
-        return everest::connection::SerialDeviceConfiguration::BaudRate::Baud_19200;
-        break;
-    case 38400:
-        return everest::connection::SerialDeviceConfiguration::BaudRate::Baud_38400;
-        break;
-    case 57600:
-        return everest::connection::SerialDeviceConfiguration::BaudRate::Baud_57600;
-        break;
-    case 115200:
-        return everest::connection::SerialDeviceConfiguration::BaudRate::Baud_115200;
-        break;
-    case 230400:
-        return everest::connection::SerialDeviceConfiguration::BaudRate::Baud_230400;
-        break;
-    default:
-        EVLOG_error << "Error setting up connection to serialport: Undefined baudrate! Defaulting to 9600.\n";
-        return everest::connection::SerialDeviceConfiguration::BaudRate::Baud_9600;
-        break;
-    }
-}
-
-template <typename T, typename U>
-void serial_communication_hubImpl::append_array(std::vector<T>& m, const std::vector<U>& a) {
-    for (auto it = a.begin(); it != a.end(); ++it)
-        m.push_back(*it);
-}
-
-bool serial_communication_hubImpl::check_crc(const uint8_t* buffer, size_t len) {
-    if (len <= 2)
-        return false;
-    // check crc
-    const uint16_t crc_sum = everest::modbus::utils::calcCRC_16_ANSI(buffer, len - 2);
-    const uint8_t crc_low = (uint8_t)(crc_sum >> 8);
-    const uint8_t crc_high = (uint8_t)(crc_sum & 0x00FF);
-
-    if (crc_low == buffer[len - 2] && crc_high == buffer[len - 1])
-        return true;
-    else
-        return false;
 }
 
 } // namespace main
