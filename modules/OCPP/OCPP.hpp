cb63b778ae4a3d05cbc1eeee2678b7294e83c040
pietfried <piet.goempel@pionix.de>
2022-09-22 12:15:33 +0200

    removed scripts path from ocpp module, not needed anymore
    Signed-off-by: pietfried <piet.goempel@pionix.de>

--- modules/OCPP/OCPP.hpp
+++ modules/OCPP/OCPP.hpp
@@ -43,7 +43,6 @@ struct Conf {
     std::string ChargePointConfigPath;
     std::string DatabasePath;
     std::string SchemasPath;
-    std::string ScriptsPath;
     bool EnableExternalWebsocketControl;
 };
 
