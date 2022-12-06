1348e877b63003860ecd506b198a411b395c62ef
aw <aw@pionix.de>
2022-12-06 15:04:51 +0100

    Improve out-of-core build support
    - creating proper cmake config file for everest-core so that it's cmake
      functionality for generation can be used by a depending package, which
      requires everest-core via find_package
    - remove stale find_package(s)
    
    Signed-off-by: aw <aw@pionix.de>

---
+++ cmake/ev-project-bootstrap.cmake
@@ -0,0 +1,11 @@
+
+# FIXME (aw): clean up this inclusion chain
+include(${CMAKE_CURRENT_LIST_DIR}/ev-cli.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/config-run-script.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/config-run-nodered-script.cmake)
+
+# dependencies
+require_ev_cli_version("0.0.16")
+
+# source generate scripts / setup
+include(${CMAKE_CURRENT_LIST_DIR}/everest-generate.cmake)
