dc6334e20343075112098071348cb8e6cc5315be
pietfried <piet.goempel@pionix.de>
2023-01-05 10:12:41 +0100

    - Adjusted OCPP module to new libocpp namespaces and structure - Added basic OCPP201 module - Removed config parameters from ocpp201 module and added evse manager requirement
    Signed-off-by: pietfried <piet.goempel@pionix.de>

--- modules/OCPP201/OCPP201.cpp
+++ modules/OCPP201/OCPP201.cpp
@@ -44,6 +44,22 @@ void OCPP201::init() {
 
     this->charge_point = std::make_unique<ocpp::v201::ChargePoint>(json_config, this->ocpp_share_path.string(),
                                                                    this->config.MessageLogPath);
+
+    int evse_id = 1;
+    for (const auto &evse : this->r_evse_manager) {
+        evse->subscribe_session_event([this, evse_id](types::evse_manager::SessionEvent session_event) {
+            switch (session_event.event) {
+                case types::evse_manager::SessionEventEnum::SessionStarted:
+                    this->charge_point->on_session_started(evse_id, 1);
+                    break;
+                case types::evse_manager::SessionEventEnum::SessionFinished:
+                    this->charge_point->on_session_finished(evse_id, 1);
+                    break;
+            }
+        });
+        evse_id++;
+    }
+
     this->charge_point->start();
 }
 
