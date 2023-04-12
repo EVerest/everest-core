1db35feffa714560a907c21e12af5486154ec9fd
Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
2023-04-12 16:41:17 +0200

    Fix tests to use temporary config in test control module
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>

--- tests/core_tests/startup_tests.py
+++ tests/core_tests/startup_tests.py
@@ -25,7 +25,7 @@ async def test_001_start_test_module(everest_core: EverestCore, test_control_mod
     everest_core.start(standalone_module='test_control_module')
     logging.info("everest-core ready, waiting for test control module")
 
-    test_control_module_handle = test_control_module.start()
+    test_control_module_handle = test_control_module.start(everest_core.everest_config_path)
     logging.info("test_control_module started")
 
     test_control_module_handle.start_charging(mode = 'raw', charge_sim_string = "sleep 1;iec_wait_pwr_ready;sleep 1;draw_power_regulated 16,3;sleep 5;unplug")
