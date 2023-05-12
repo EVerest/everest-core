68844131dbe95c2383cc91889dd121a1120f6a07
aw <aw@pionix.de>
2023-05-12 14:18:05 +0200

    Adapt to changes in EVerest/everest-utils#49
    - disabled PyTestController in tests, this is broken
    
    Signed-off-by: aw <aw@pionix.de>

--- tests/core_tests/startup_tests.py
+++ tests/core_tests/startup_tests.py
@@ -7,31 +7,30 @@ import logging
 import pytest
 
 from everest.testing.core_utils.fixtures import *
-from validations.base_functions import wait_for_and_validate_event
-from everest.testing.core_utils.test_control_module import TestControlModule
 from everest.testing.core_utils.everest_core import EverestCore
 from validations.user_functions import *
 
-@ pytest.mark.asyncio
+@pytest.mark.asyncio
 async def test_000_startup_check(everest_core: EverestCore):
     logging.info(">>>>>>>>> test_000_startup_check <<<<<<<<<")
     everest_core.start()
-    everest_core.stop()
 
-@ pytest.mark.asyncio
-async def test_001_start_test_module(everest_core: EverestCore, test_control_module: TestControlModule):
-    logging.info(">>>>>>>>> test_001_start_test_module <<<<<<<<<")
 
-    everest_core.start(standalone_module='test_control_module')
-    logging.info("everest-core ready, waiting for test control module")
+# FIXME (aw): broken
+# @pytest.mark.asyncio
+# async def test_001_start_test_module(everest_core: EverestCore, test_control_module: TestControlModule):
+#     logging.info(">>>>>>>>> test_001_start_test_module <<<<<<<<<")
 
-    test_control_module_handle = test_control_module.start(everest_core.everest_config_path)
-    logging.info("test_control_module started")
+#     everest_core.start(standalone_module='test_control_module')
+#     logging.info("everest-core ready, waiting for test control module")
 
-    test_control_module_handle.start_charging(mode = 'raw', charge_sim_string = "sleep 1;iec_wait_pwr_ready;sleep 1;draw_power_regulated 16,3;sleep 5;unplug")
+#     test_control_module_handle = test_control_module.start(everest_core.everest_config_path)
+#     logging.info("test_control_module started")
 
-    # assert only for events to have happened (ignore contents)
-    assert await wait_for_and_validate_event(test_control_module, exp_event='transaction_started', exp_data=None, validation_function=None, timeout=10)
-    assert await wait_for_and_validate_event(test_control_module, exp_event='transaction_finished', exp_data=None, validation_function=None, timeout=10)
+#     test_control_module_handle.start_charging(mode = 'raw', charge_sim_string = "sleep 1;iec_wait_pwr_ready;sleep 1;draw_power_regulated 16,3;sleep 5;unplug")
 
-    everest_core.stop()
+#     # assert only for events to have happened (ignore contents)
+#     assert await wait_for_and_validate_event(test_control_module, exp_event='transaction_started', exp_data=None, validation_function=None, timeout=10)
+#     assert await wait_for_and_validate_event(test_control_module, exp_event='transaction_finished', exp_data=None, validation_function=None, timeout=10)
+
+#     everest_core.stop()
