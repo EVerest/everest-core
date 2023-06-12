c273590065efedcfe7bcc87f792e6b4b877de8e1
Cornelius Claussen <cc@pionix.de>
2023-06-12 16:36:21 +0200

    Fix negative watt values in api set_limit_watts
    Signed-off-by: Cornelius Claussen <cc@pionix.de>

--- modules/API/API.cpp
+++ modules/API/API.cpp
@@ -50,9 +50,9 @@ types::energy::ExternalLimits get_external_limits(const std::string& data, bool 
     zero_entry.limits_to_leaves.total_power_W = 0;
 
     if (is_watts) {
-        target_entry.limits_to_leaves.total_power_W = limit;
+        target_entry.limits_to_leaves.total_power_W = std::fabs(limit);
     } else {
-        target_entry.limits_to_leaves.ac_max_current_A = limit;
+        target_entry.limits_to_leaves.ac_max_current_A = std::fabs(limit);
     }
 
     if (limit > 0) {
