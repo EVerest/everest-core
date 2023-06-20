452e98bb8b9ce9c0a14eb6de000592fbfbbfc66a
Sebastian Lukas <sebastian.lukas@pionix.de>
2023-06-20 08:51:34 +0200

    Adding the PyEvJosev module to replace the JsCarV2G module
    Signed-off-by: Sebastian Lukas <sebastian.lukas@pionix.de>

---
+++ modules/PyEvJosev/module.py
@@ -0,0 +1,113 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2020 - 2023 Pionix GmbH and Contributors to EVerest
+import asyncio
+import sys
+from pathlib import Path
+import threading
+
+from everest.framework import Module, RuntimeSession, log
+
+# fmt: off
+JOSEV_WORK_DIR = Path(__file__).parent / '../../3rd_party/josev'
+sys.path.append(JOSEV_WORK_DIR.as_posix())
+
+from iso15118.evcc import EVCCHandler
+from iso15118.evcc.controller.simulator import SimEVController
+from iso15118.evcc.evcc_config import EVCCConfig
+from iso15118.evcc.everest import context as JOSEV_CONTEXT
+from iso15118.shared.exificient_exi_codec import ExificientEXICodec
+from iso15118.shared.settings import set_PKI_PATH
+
+from utilities import (
+    setup_everest_logging,
+    determine_network_interface,
+    patch_josev_config
+)
+
+setup_everest_logging()
+
+EVEREST_CERTS_SUB_DIR = 'certs'
+
+async def evcc_handler_main_loop(module_config: dict):
+    """
+    Entrypoint function that starts the ISO 15118 code running on
+    the EVCC (EV Communication Controller)
+    """
+    iface = determine_network_interface(module_config['device'])
+
+    evcc_config = EVCCConfig()
+    patch_josev_config(evcc_config, module_config)
+
+    await EVCCHandler(
+        evcc_config=evcc_config,
+        iface=iface,
+        exi_codec=ExificientEXICodec(),
+        ev_controller=SimEVController(evcc_config),
+    ).start()
+
+
+class PyEVJosevModule():
+    def __init__(self) -> None:
+        self._es = JOSEV_CONTEXT.ev_state
+        self._session = RuntimeSession()
+        m = Module(self._session)
+
+        log.update_process_name(m.info.id)
+
+        self._setup = m.say_hello()
+
+        etc_certs_path = m.info.paths.etc / EVEREST_CERTS_SUB_DIR
+        set_PKI_PATH(str(etc_certs_path.resolve()))
+
+        # setup publishing callback
+        def publish_callback(variable_name: str, value: any):
+            m.publish_variable('ev', variable_name, value)
+
+        # set publish callback for context
+        JOSEV_CONTEXT.set_publish_callback(publish_callback)
+
+        # setup handlers
+        for cmd in m.implementations['ev'].commands:
+            m.implement_command(
+                'ev', cmd, getattr(self, f'_handler_{cmd}'))
+
+        # init ready event
+        self._ready_event = threading.Event()
+
+        self._mod = m
+        self._mod.init_done(self._ready)
+
+    def start_evcc_handler(self):
+        while True:
+            self._ready_event.wait()
+            try:
+                asyncio.run(evcc_handler_main_loop(self._setup.configs.module))
+                self._mod.publish_variable('ev', 'V2G_Session_Finished', None)
+            except KeyboardInterrupt:
+                log.debug("SECC program terminated manually")
+                break
+            self._ready_event.clear()
+
+    def _ready(self):
+        log.debug("ready!")
+
+    # implementation handlers
+
+    def _handler_start_charging(self, args) -> bool:
+
+        self._es.PaymentOption =args['PaymentOption']
+        self._es.EnergyTransferMode = args['EnergyTransferMode']
+
+        self._ready_event.set()
+
+        return True
+
+    def _handler_stop_charging(self, args):
+        self._es.StopCharging = True
+
+    def _handler_set_fault(self, args):
+        pass
+
+
+py_ev_josev = PyEVJosevModule()
+py_ev_josev.start_evcc_handler()
