da148215d7385a08e2c5694a4e3c36b8af265182
Kai Hermann <kai-uwe.hermann@pionix.de>
2023-07-10 15:12:54 +0200

    Add data transfer to ocpp module (#281)
    * Add data transfer command to OCPP 1.6 module
    * Add optional ocpp requirement to PyProbeModule to allow testing
    
    ---------
    
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>

--- modules/OCPP/main/ocpp_1_6_charge_pointImpl.cpp
+++ modules/OCPP/main/ocpp_1_6_charge_pointImpl.cpp
@@ -20,5 +20,29 @@ bool ocpp_1_6_charge_pointImpl::handle_restart() {
     return mod->charge_point->restart();
 }
 
+types::ocpp::DataTransferStatus convert_ocpp_data_transfer_status(ocpp::v16::DataTransferStatus status) {
+    switch (status) {
+    case ocpp::v16::DataTransferStatus::Accepted:
+        return types::ocpp::DataTransferStatus::Accepted;
+    case ocpp::v16::DataTransferStatus::Rejected:
+        return types::ocpp::DataTransferStatus::Rejected;
+    case ocpp::v16::DataTransferStatus::UnknownMessageId:
+        return types::ocpp::DataTransferStatus::UnknownMessageId;
+    case ocpp::v16::DataTransferStatus::UnknownVendorId:
+        return types::ocpp::DataTransferStatus::UnknownVendorId;
+    default:
+        return types::ocpp::DataTransferStatus::UnknownVendorId;
+    }
+}
+
+types::ocpp::DataTransferResponse
+ocpp_1_6_charge_pointImpl::handle_data_transfer(std::string& vendor_id, std::string& message_id, std::string& data) {
+    auto ocpp_response = mod->charge_point->data_transfer(vendor_id, message_id, data);
+    types::ocpp::DataTransferResponse response;
+    response.status = convert_ocpp_data_transfer_status(ocpp_response.status);
+    response.data = ocpp_response.data;
+    return response;
+}
+
 } // namespace main
 } // namespace module
