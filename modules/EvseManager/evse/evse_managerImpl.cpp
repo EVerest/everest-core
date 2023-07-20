e0943b882f79c07045c8f9d794601f119906c65f
Cornelius Claussen <cc@pionix.de>
2023-07-20 10:28:29 +0200

    add emergency stop nodered external command
    Signed-off-by: Cornelius Claussen <cc@pionix.de>

--- modules/EvseManager/evse/evse_managerImpl.cpp
+++ modules/EvseManager/evse/evse_managerImpl.cpp
@@ -70,6 +70,16 @@ void evse_managerImpl::init() {
                             mod->charger->cancelTransaction(request);
                         });
 
+    mod->mqtt.subscribe(fmt::format("everest_external/nodered/{}/cmd/emergency_stop", mod->config.connector_id),
+                        [this](const std::string& data) {
+                            if (mod->get_hlc_enabled()) {
+                                mod->r_hlc[0]->call_set_EVSE_EmergencyShutdown(true);
+                            }
+                            types::evse_manager::StopTransactionRequest request;
+                            request.reason = types::evse_manager::StopTransactionReason::EmergencyStop;
+                            mod->charger->cancelTransaction(request);
+                        });
+
     // /Interface to Node-RED debug UI
 
     if (mod->r_powermeter_billing().size() > 0) {
