c661a68fa5efbeda6e2420baacf97777b5435f29
Wojciech Kula <wku@qwello.eu>
2023-10-23 15:23:57 +0200

    Add tests checking if proper state is published
    Signed-off-by: Wojciech Kula <wku@qwello.eu>

--- modules/Auth/tests/auth_tests.cpp
+++ modules/Auth/tests/auth_tests.cpp
@@ -1,5 +1,6 @@
 
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
 #include <iostream>
 #include <memory>
 #include <thread>
@@ -9,6 +10,10 @@
 
 #include <AuthHandler.hpp>
 #include <FakeAuthReceiver.hpp>
+
+using ::testing::MockFunction;
+using ::testing::StrictMock;
+using ::testing::_;
 
 namespace module {
 
@@ -48,6 +53,7 @@ class AuthTest : public ::testing::Test {
 protected:
     std::unique_ptr<AuthHandler> auth_handler;
     std::unique_ptr<FakeAuthReceiver> auth_receiver;
+    StrictMock<MockFunction<void(types::authorization::TokenValidationStatus status)>> mock_publish_token_validation_status_callback;
 
     void SetUp() override {
         std::vector<int32_t> evse_indices{0, 1};
@@ -102,6 +108,8 @@ protected:
             EVLOG_info << "Signaling reservating cancelled to evse#" << evse_index;
         });
 
+        this->auth_handler->register_publish_token_validation_status_callback(mock_publish_token_validation_status_callback.AsStdFunction());
+
         this->auth_handler->init_connector(1, 0);
         this->auth_handler->init_connector(2, 1);
     }
@@ -122,6 +130,8 @@ TEST_F(AuthTest, test_simple_authorization) {
     std::vector<int32_t> connectors{1};
     ProvidedIdToken provided_token = get_provided_token(VALID_TOKEN_1, connectors);
 
+    EXPECT_CALL(mock_publish_token_validation_status_callback, Call(types::authorization::TokenValidationStatus::Accepted));
+
     const auto result = this->auth_handler->on_token(provided_token);
     ASSERT_TRUE(result == TokenHandlingResult::ACCEPTED);
     ASSERT_TRUE(this->auth_receiver->get_authorization(0));
@@ -138,6 +148,7 @@ TEST_F(AuthTest, test_two_referenced_connectors) {
     std::vector<int32_t> connectors{1, 2};
     ProvidedIdToken provided_token = get_provided_token(VALID_TOKEN_1, connectors);
 
+    EXPECT_CALL(mock_publish_token_validation_status_callback, Call(types::authorization::TokenValidationStatus::Accepted));
     const auto result = this->auth_handler->on_token(provided_token);
     ASSERT_TRUE(result == TokenHandlingResult::ACCEPTED);
     ASSERT_TRUE(this->auth_receiver->get_authorization(1));
@@ -160,6 +171,7 @@ TEST_F(AuthTest, test_stop_transaction) {
 
     this->auth_handler->handle_session_event(1, session_event1);
 
+    EXPECT_CALL(mock_publish_token_validation_status_callback, Call(types::authorization::TokenValidationStatus::Accepted)).Times(2);
     auto result = this->auth_handler->on_token(provided_token);
     ASSERT_TRUE(result == TokenHandlingResult::ACCEPTED);
     ASSERT_TRUE(this->auth_receiver->get_authorization(0));
@@ -181,6 +193,8 @@ TEST_F(AuthTest, test_authorize_first) {
 
     std::vector<int32_t> connectors{1, 2};
     ProvidedIdToken provided_token = get_provided_token(VALID_TOKEN_1, connectors);
+
+    EXPECT_CALL(mock_publish_token_validation_status_callback, Call(types::authorization::TokenValidationStatus::Accepted));
 
     TokenHandlingResult result;
 
@@ -204,6 +218,8 @@ TEST_F(AuthTest, test_authorize_first) {
 TEST_F(AuthTest, test_swipe_multiple_times_with_timeout) {
     std::vector<int32_t> connectors{1, 2};
     ProvidedIdToken provided_token = get_provided_token(VALID_TOKEN_1, connectors);
+
+    EXPECT_CALL(mock_publish_token_validation_status_callback, Call(types::authorization::TokenValidationStatus::Accepted)).Times(2);
 
     TokenHandlingResult result1;
     TokenHandlingResult result2;
@@ -256,6 +272,8 @@ TEST_F(AuthTest, test_two_id_tokens) {
     ProvidedIdToken provided_token_1 = get_provided_token(VALID_TOKEN_1, connectors);
     ProvidedIdToken provided_token_2 = get_provided_token(VALID_TOKEN_2, connectors);
 
+    EXPECT_CALL(mock_publish_token_validation_status_callback, Call(types::authorization::TokenValidationStatus::Accepted)).Times(2);
+
     TokenHandlingResult result1;
     TokenHandlingResult result2;
 
@@ -294,6 +312,8 @@ TEST_F(AuthTest, test_two_plugins) {
     ProvidedIdToken provided_token_1 = get_provided_token(VALID_TOKEN_1, connectors);
     ProvidedIdToken provided_token_2 = get_provided_token(VALID_TOKEN_2, connectors);
 
+    EXPECT_CALL(mock_publish_token_validation_status_callback, Call(types::authorization::TokenValidationStatus::Accepted)).Times(2);
+
     std::thread t3([this, provided_token_1, &result1]() { result1 = this->auth_handler->on_token(provided_token_1); });
     std::thread t4([this, provided_token_2, &result2]() { result2 = this->auth_handler->on_token(provided_token_2); });
 
@@ -323,6 +343,8 @@ TEST_F(AuthTest, test_two_plugins_with_invalid_rfid) {
     std::vector<int32_t> connectors{1, 2};
     ProvidedIdToken provided_token_1 = get_provided_token(VALID_TOKEN_1, connectors);
     ProvidedIdToken provided_token_2 = get_provided_token(INVALID_TOKEN, connectors);
+
+    EXPECT_CALL(mock_publish_token_validation_status_callback, Call(types::authorization::TokenValidationStatus::Accepted));
 
     std::thread t3([this, provided_token_1, &result1]() { result1 = this->auth_handler->on_token(provided_token_1); });
     std::thread t4([this, provided_token_2, &result2]() { result2 = this->auth_handler->on_token(provided_token_2); });
@@ -402,6 +424,8 @@ TEST_F(AuthTest, test_transaction_finish) {
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
     std::thread t2([this, session_event1]() { this->auth_handler->handle_session_event(2, session_event1); });
 
+    EXPECT_CALL(mock_publish_token_validation_status_callback, Call(types::authorization::TokenValidationStatus::Accepted)).Times(4);
+
     std::thread t3([this, provided_token_1, &result1]() { result1 = this->auth_handler->on_token(provided_token_1); });
     std::thread t4([this, provided_token_2, &result2]() { result2 = this->auth_handler->on_token(provided_token_2); });
 
@@ -455,6 +479,8 @@ TEST_F(AuthTest, test_parent_id_finish) {
     transaction_event1.timestamp = Everest::Date::to_rfc3339(date::utc_clock::now());
     session_event2.transaction_started = transaction_event1;
 
+    EXPECT_CALL(mock_publish_token_validation_status_callback, Call(types::authorization::TokenValidationStatus::Accepted)).Times(2);
+
     // swipe VALID_TOKEN_1
     std::thread t2([this, provided_token_1, &result]() { result = this->auth_handler->on_token(provided_token_1); });
 
@@ -494,6 +520,8 @@ TEST_F(AuthTest, test_parent_id_no_finish) {
     std::vector<int32_t> connectors{1, 2};
     ProvidedIdToken provided_token_1 = get_provided_token(VALID_TOKEN_1, connectors);
     ProvidedIdToken provided_token_2 = get_provided_token(VALID_TOKEN_3, connectors);
+
+    EXPECT_CALL(mock_publish_token_validation_status_callback, Call(types::authorization::TokenValidationStatus::Accepted)).Times(2);
 
     // swipe VALID_TOKEN_1
     std::thread t2([this, provided_token_1, &result]() { result = this->auth_handler->on_token(provided_token_1); });
@@ -537,6 +565,7 @@ TEST_F(AuthTest, test_parent_id_finish_because_no_available_connector) {
     ProvidedIdToken provided_token_1 = get_provided_token(VALID_TOKEN_1, connectors);
     ProvidedIdToken provided_token_2 = get_provided_token(VALID_TOKEN_3, connectors);
 
+    EXPECT_CALL(mock_publish_token_validation_status_callback, Call(types::authorization::TokenValidationStatus::Accepted)).Times(2);
     // swipe VALID_TOKEN_1
     std::thread t3([this, provided_token_1, &result]() { result = this->auth_handler->on_token(provided_token_1); });
 
@@ -626,6 +655,8 @@ TEST_F(AuthTest, test_reservation_with_authorization) {
     ProvidedIdToken provided_token_1 = get_provided_token(VALID_TOKEN_1, connectors);
     ProvidedIdToken provided_token_2 = get_provided_token(VALID_TOKEN_2, connectors);
 
+    EXPECT_CALL(mock_publish_token_validation_status_callback, Call(types::authorization::TokenValidationStatus::Accepted)).Times(2);
+
     // this token is not valid for the reservation
     std::thread t3([this, provided_token_1, &result]() { result = this->auth_handler->on_token(provided_token_1); });
     t3.join();
@@ -678,6 +709,8 @@ TEST_F(AuthTest, test_complete_event_flow) {
     session_event_6.event = SessionEventEnum::SessionFinished;
 
     this->auth_handler->handle_session_event(1, session_event_1);
+
+    EXPECT_CALL(mock_publish_token_validation_status_callback, Call(types::authorization::TokenValidationStatus::Accepted)).Times(2);
 
     result = this->auth_handler->on_token(provided_token);
 
@@ -737,6 +770,8 @@ TEST_F(AuthTest, test_reservation_with_parent_id_tag) {
     ProvidedIdToken provided_token_1 = get_provided_token(VALID_TOKEN_2, connectors);
     ProvidedIdToken provided_token_2 = get_provided_token(VALID_TOKEN_3, connectors);
 
+    EXPECT_CALL(mock_publish_token_validation_status_callback, Call(types::authorization::TokenValidationStatus::Accepted)).Times(2);
+
     // this token is not valid for the reservation
     std::thread t3([this, provided_token_1, &result]() { result = this->auth_handler->on_token(provided_token_1); });
     t3.join();
@@ -759,6 +794,8 @@ TEST_F(AuthTest, test_authorization_timeout_and_reswipe) {
 
     std::vector<int32_t> connectors{1, 2};
     ProvidedIdToken provided_token = get_provided_token(VALID_TOKEN_1, connectors);
+
+    EXPECT_CALL(mock_publish_token_validation_status_callback, Call(types::authorization::TokenValidationStatus::Accepted)).Times(2);
 
     TokenHandlingResult result;
     std::thread t1([this, provided_token, &result]() { result = this->auth_handler->on_token(provided_token); });
@@ -786,6 +823,8 @@ TEST_F(AuthTest, test_authorization_without_transaction) {
 
     std::vector<int32_t> connectors{1};
     ProvidedIdToken provided_token = get_provided_token(VALID_TOKEN_1, connectors);
+
+    EXPECT_CALL(mock_publish_token_validation_status_callback, Call(types::authorization::TokenValidationStatus::Accepted)).Times(2);
 
     TokenHandlingResult result;
     std::thread t1([this, provided_token, &result]() { result = this->auth_handler->on_token(provided_token); });
@@ -844,6 +883,8 @@ TEST_F(AuthTest, test_two_transactions_start_stop) {
     transaction_event_2.timestamp = Everest::Date::to_rfc3339(date::utc_clock::now());
     session_event3.transaction_started = transaction_event_2;
 
+    EXPECT_CALL(mock_publish_token_validation_status_callback, Call(types::authorization::TokenValidationStatus::Accepted)).Times(4);
+
     std::thread t3([this, provided_token_1, &result1]() { result1 = this->auth_handler->on_token(provided_token_1); });
     std::thread t4([this, provided_token_2, &result2]() { result2 = this->auth_handler->on_token(provided_token_2); });
 
@@ -888,6 +929,8 @@ TEST_F(AuthTest, test_plug_and_charge) {
     provided_token.authorization_type = types::authorization::AuthorizationType::PlugAndCharge;
     provided_token.certificate.emplace("TestCertificate");
 
+    EXPECT_CALL(mock_publish_token_validation_status_callback, Call(types::authorization::TokenValidationStatus::Accepted));
+
     const auto result = this->auth_handler->on_token(provided_token);
     ASSERT_TRUE(result == TokenHandlingResult::ACCEPTED);
     ASSERT_TRUE(this->auth_receiver->get_authorization(0));
@@ -929,6 +972,8 @@ TEST_F(AuthTest, test_empty_intersection) {
     provided_token.authorization_type = types::authorization::AuthorizationType::PlugAndCharge;
     provided_token.certificate.emplace("TestCertificate");
     provided_token.connectors.emplace(connectors);
+
+    EXPECT_CALL(mock_publish_token_validation_status_callback, Call(types::authorization::TokenValidationStatus::Accepted));
 
     const auto result = this->auth_handler->on_token(provided_token);
     ASSERT_TRUE(result == TokenHandlingResult::ACCEPTED);
