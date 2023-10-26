57917c6d3a534de66bdbc33e4c978733c02770c5
MyThreads <miriam.thome@pionix.de>
2023-10-26 11:45:44 +0200

    deleted SLIPCommHub folder

--- modules/SLIPCommHub/crc16.hpp
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: Apache-2.0
-// Copyright Pionix GmbH and Contributors to EVerest
-#ifndef CRC16_HPP
-#define CRC16_HPP
-#include <stdint.h>
-
-uint16_t calculate_modbus_crc16(const uint8_t* buf, int len);
-#endif
