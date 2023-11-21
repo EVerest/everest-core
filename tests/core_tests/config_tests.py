f695d730a09a4f0da842b2aba32c9b0dc185322a
Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
2023-11-21 11:58:31 +0100

    Refactor config_tests to automatically run all configs found
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>

--- tests/core_tests/config_tests.py
+++ tests/core_tests/config_tests.py
@@ -19,46 +19,12 @@ from everest.testing.core_utils.configuration.everest_configuration_visitors.eve
     EverestConfigAdjustmentVisitor
 
 
-@pytest.mark.everest_core_config('config-example.yaml')
-@pytest.mark.asyncio
-async def test_start_config_example(everest_core: EverestCore):
-    logging.info(">>>>>>>>> test_start_config_example <<<<<<<<<")
-    everest_core.start()
-
-
-@pytest.mark.everest_core_config('config-sil-dc-sae-v2g.yaml')
-@pytest.mark.asyncio
-async def test_start_config_sil_dc_sae_v2g(everest_core: EverestCore):
-    logging.info(">>>>>>>>> test_start_config_sil_dc_sae_v2g <<<<<<<<<")
-    everest_core.start()
-
-
-@pytest.mark.everest_core_config('config-sil-dc-sae-v2h.yaml')
-@pytest.mark.asyncio
-async def test_start_config_sil_dc_sae_v2h(everest_core: EverestCore):
-    logging.info(">>>>>>>>> test_start_config_sil_dc_sae_v2h <<<<<<<<<")
-    everest_core.start()
-
-
-@pytest.mark.everest_core_config('config-sil-dc.yaml')
-@pytest.mark.asyncio
-async def test_start_config_sil_dc(everest_core: EverestCore):
-    logging.info(">>>>>>>>> test_start_config_sil_dc <<<<<<<<<")
-    everest_core.start()
-
-
-@pytest.mark.everest_core_config('config-sil-energy-management.yaml')
-@pytest.mark.asyncio
-async def test_start_config_sil_energy_management(everest_core: EverestCore):
-    logging.info(">>>>>>>>> test_config_sil_energy_management <<<<<<<<<")
-    everest_core.start()
-
-
 class EverestCoreConfigSilGenPmConfigurationVisitor(EverestConfigAdjustmentVisitor):
     def __init__(self):
         self.temporary_directory = mkdtemp()
         self.serial_port_0, self.serial_port_1 = pty.openpty()
-        self.serial_port_0_name = os.ttyname(self.serial_port_0) # FIXME: cleanup socket after test
+        # FIXME: cleanup socket after test
+        self.serial_port_0_name = os.ttyname(self.serial_port_0)
 
     def adjust_everest_configuration(self, everest_config: Dict):
         adjusted_config = deepcopy(everest_config)
@@ -77,51 +43,19 @@ async def test_start_config_sil_gen_pm(everest_core: EverestCore):
     everest_core.start()
 
 
-@pytest.mark.everest_core_config('config-sil-ocpp-custom-extension.yaml')
-@pytest.mark.asyncio
-async def test_start_config_sil_ocpp_custom_extension(everest_core: EverestCore):
-    logging.info(
-        ">>>>>>>>> test_start_config_sil_ocpp_custom_extension <<<<<<<<<")
-    everest_core.start()
+class TestConfigsInDirectory:
+    @pytest.fixture(params=pytest.everest_configs['params'], ids=pytest.everest_configs['ids'])
+    def core_config(self, request) -> EverestEnvironmentCoreConfiguration:
+        everest_prefix = Path(request.config.getoption("--everest-prefix"))
 
+        everest_config_path = request.param
 
-@pytest.mark.everest_core_config('config-sil-ocpp-pnc.yaml')
-@pytest.mark.asyncio
-async def test_start_config_sil_ocpp_pnc(everest_core: EverestCore):
-    logging.info(">>>>>>>>> test_start_config_sil_ocpp_pnc <<<<<<<<<")
-    everest_core.start()
+        return EverestEnvironmentCoreConfiguration(
+            everest_core_path=everest_prefix,
+            template_everest_config_path=everest_config_path,
+        )
 
-
-@pytest.mark.everest_core_config('config-sil-ocpp.yaml')
-@pytest.mark.asyncio
-async def test_start_config_sil_ocpp(everest_core: EverestCore):
-    logging.info(">>>>>>>>> test_start_config_sil_ocpp <<<<<<<<<")
-    everest_core.start()
-
-
-@pytest.mark.everest_core_config('config-sil-ocpp201.yaml')
-@pytest.mark.asyncio
-async def test_start_config_sil_ocpp201(everest_core: EverestCore):
-    logging.info(">>>>>>>>> test_start_config_sil_ocpp201 <<<<<<<<<")
-    everest_core.start()
-
-
-@pytest.mark.everest_core_config('config-sil-two-evse-dc.yaml')
-@pytest.mark.asyncio
-async def test_start_config_sil_two_evse_dc(everest_core: EverestCore):
-    logging.info(">>>>>>>>> test_start_config_sil_two_evse_dc <<<<<<<<<")
-    everest_core.start()
-
-
-@pytest.mark.everest_core_config('config-sil-two-evse.yaml')
-@pytest.mark.asyncio
-async def test_start_config_sil_two_evse(everest_core: EverestCore):
-    logging.info(">>>>>>>>> test_start_config_sil_two_evse <<<<<<<<<")
-    everest_core.start()
-
-
-@pytest.mark.everest_core_config('config-sil.yaml')
-@pytest.mark.asyncio
-async def test_start_config_sil(everest_core: EverestCore):
-    logging.info(">>>>>>>>> test_start_config_sil <<<<<<<<<")
-    everest_core.start()
+    @pytest.mark.asyncio
+    async def test_config(self, everest_core: EverestCore):
+        logging.info(">>>>>>>>> test_config <<<<<<<<<")
+        everest_core.start()
