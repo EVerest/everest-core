b0ea8330701339b786aec309129511f50763af58
Dominik <dominik@pionix.de>
2023-12-18 18:17:22 +0100

    `install` and `everestpy` to integration-tests
    Signed-off-by: Dominik <dominik@pionix.de>

--- .ci/build-kit/test_and_install.sh
+++ .ci/build-kit/test_and_install.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
 
-# ninja -j$(nproc) -C build tests
-ninja -j$(nproc) -C build install
-
-# install everestpy via cmake target from everest-framework
-ninja -C build everestpy_pip_install_dist
-
-rsync -a "$EXT_MOUNT/source/tests" ./
-
-rm -rf build
+ninja -j$(nproc) -C build tests
--- .ci/e2e/scripts/tests.sh
+++ .ci/e2e/scripts/tests.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
+ninja -j$(nproc) -C build install
+
+# install everestpy via cmake target from everest-framework
+ninja -C build everestpy_pip_install_dist
+
+rsync -a "$EXT_MOUNT/source/tests" ./
+
+rm -rf build
+
 cd tests
 pytest --everest-prefix ../dist core_tests/*.py framework_tests/*.py
