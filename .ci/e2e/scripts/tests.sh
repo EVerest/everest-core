483526ace87c90d6b830b756ef874658e3ecc0ac
Dominik <dominik@pionix.de>
2023-12-22 22:44:35 +0100

    tests.sh: remove obsolete cmds
    Signed-off-by: Dominik <dominik@pionix.de>

--- .ci/e2e/scripts/tests.sh
+++ .ci/e2e/scripts/tests.sh
@@ -6,9 +6,5 @@ ninja -j$(nproc) -C build install
 # install everest testing by cmake target to make sure using the version defined in dependencies.yaml
 ninja -C build install_everest_testing
 
-rsync -a "tests" ./
-
-rm -rf build
-
 cd tests
 pytest --everest-prefix ../dist core_tests/*.py framework_tests/*.py
