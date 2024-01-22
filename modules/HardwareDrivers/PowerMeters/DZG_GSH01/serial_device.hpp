73f96359fb6dda62039e944593347112a91eb1d4
James Chapman <james.chapman@pionix.de>
2024-01-22 13:00:11 +0100

    Rename Powermeter DZG_GHS01
    Fix build (#4)
    
    * Make nanopb relocatable
    
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
    
    * Adapt to new powermeter signed meter value type
    
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
    
    * Adapt to new powermeter signed meter value type
    
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
    
    ---------
    
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
    
    Fix/license  (#33)
    
    * Extension of API for OTA update
    
    * Fixed missing topic
    
    Signed-off-by: Jan Christoph Habig <jan.habig@pionix.de>
    
    * Preparations for logs in cloud
    
    * Logs for cloudconnector implemented
    
    Signed-off-by: Jan Christoph Habig <jan.habig@pionix.de>
    
    * Logs after sync with cloud team
    
    * firmware update topics and firmwareupdate messag
    
    * rauc status like in cloud
    
    * Empty message for system reboot
    
    * Finalized switch to new api
    
    * Systemversion from /etc/everest/release_full.json
    
    * Fixed License information and removed obsolet files
    
    * Added license to merged files
    
    * Update LogStatusTypeEnum_API.hpp
    
    ---------
    
    Signed-off-by: Jan Christoph Habig <jan.habig@pionix.de>
    
    fix ast power meter
    
    Signed-off-by: Cornelius Claussen <cc@pionix.de>
    
    Adapter to isomonitor interface change
    
    Signed-off-by: Cornelius Claussen <cc@pionix.de>
    
    Run clang-format (#76)
    
    Signed-off-by: Andreas Heinrich <andreas.heinrich@rwth-aachen.de>
    
    DZG update for Communication error handling and Eichrecht (#176)
    
    * DZG update for Communication error handling and Eichrecht
    
    Signed-off-by: florinmihut <florinmihut1@gmail.com>
    
    Fix missing includes (#270)
    
    Signed-off-by: florinmihut <florinmihut1@gmail.com>
    
    Integate the latest everest core (#273)
    
    * Integate the latest everest core
    
    Signed-off-by: florinmihut <florinmihut1@gmail.com>
    
    import DZG_GSH01 power meter from BaseCamp
    
    Signed-off-by: James Chapman <james.chapman@pionix.de>
    Signed-off-by: Piet Gömpel <pietgoempel@gmail.com>

---
+++ modules/HardwareDrivers/PowerMeters/DZG_GSH01/serial_device.hpp
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2020 - 2025 Pionix GmbH and Contributors to EVerest
+
+/*
+ This is an implementation for custom serial communications
+*/
+#ifndef SERIAL_DEVICE
+#define SERIAL_DEVICE
+
+#include <everest/logging.hpp>
+#include <mutex>
+#include <optional>
+#include <stdint.h>
+#include <termios.h>
+
+namespace serial_device {
+
+constexpr int SERIAL_RX_INITIAL_TIMEOUT_MS = 500;
+constexpr int SERIAL_RX_WITHIN_MESSAGE_TIMEOUT_MS = 100;
+
+struct Retry {
+    uint8_t num_of_retries{0};
+    uint8_t num_of_retries_done{0};
+};
+
+class SerialDevice {
+
+public:
+    SerialDevice() = default;
+    ~SerialDevice();
+
+    bool open_device(const std::string& device, int baud, bool ignore_echo, uint8_t _num_of_retries);
+    int tx_rx_blocking(std::vector<uint8_t>& request, std::vector<uint8_t>& rxbuf,
+                       std::optional<int> initial_timeout_ms, std::optional<int> in_msg_timeout_ms);
+
+private:
+    // Serial interface
+    int fd{0};
+    bool ignore_echo{false};
+
+    void tx(std::vector<uint8_t>& request);
+    int rx(std::vector<uint8_t>& rxbuf, std::optional<int> initial_timeout_ms, std::optional<int> in_msg_timeout_ms);
+
+    std::mutex serial_mutex;
+    std::mutex txrx_mutex;
+    Retry retry_struct;
+};
+
+} // namespace serial_device
+#endif // SERIAL_DEVICE
