6bf7e645827d446f73b2627bad2be9904fc254db
Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
2024-03-26 22:12:09 +0100

    Fix optional access and json conversion of DataTransfer data
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>

--- modules/OCPP201/data_transfer/ocpp_data_transferImpl.cpp
+++ modules/OCPP201/data_transfer/ocpp_data_transferImpl.cpp
@@ -19,6 +19,9 @@ ocpp_data_transferImpl::handle_data_transfer(types::ocpp::DataTransferRequest& r
     ocpp::v201::DataTransferRequest ocpp_request;
     ocpp_request.vendorId = request.vendor_id;
     ocpp_request.messageId = request.vendor_id;
+    if (request.data.has_value()) {
+        ocpp_request.data = json::parse(request.data.value());
+    }
     if (request.custom_data.has_value()) {
         auto custom_data = request.custom_data.value();
         json custom_data_json = json::parse(custom_data.data);
@@ -31,7 +34,9 @@ ocpp_data_transferImpl::handle_data_transfer(types::ocpp::DataTransferRequest& r
     auto ocpp_response = mod->charge_point->data_transfer_req(ocpp_request);
     types::ocpp::DataTransferResponse response;
     response.status = conversions::to_everest_data_transfer_status(ocpp_response.status);
-    response.data = ocpp_response.data;
+    if (ocpp_response.data.has_value()) {
+        response.data = ocpp_response.data.value().dump();
+    }
     if (ocpp_response.customData.has_value()) {
         auto ocpp_custom_data = ocpp_response.customData.value();
         types::ocpp::CustomData custom_data{ocpp_custom_data.at("vendorId"), ocpp_custom_data.dump()};
