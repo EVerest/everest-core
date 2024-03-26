fe078b70454939b44c793f1042c059faa2e1f330
Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
2024-03-26 17:06:22 +0100

    Add custom data extension to OCPP data transfer
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>

--- modules/OCPP201/data_transfer/ocpp_data_transferImpl.cpp
+++ modules/OCPP201/data_transfer/ocpp_data_transferImpl.cpp
@@ -16,10 +16,27 @@ void ocpp_data_transferImpl::ready() {
 
 types::ocpp::DataTransferResponse
 ocpp_data_transferImpl::handle_data_transfer(types::ocpp::DataTransferRequest& request) {
-    auto ocpp_response = mod->charge_point->data_transfer_req(request.vendor_id, request.message_id, request.data);
+    ocpp::v201::DataTransferRequest ocpp_request;
+    ocpp_request.vendorId = request.vendor_id;
+    ocpp_request.messageId = request.vendor_id;
+    if (request.custom_data.has_value()) {
+        auto custom_data = request.custom_data.value();
+        json custom_data_json = json::parse(custom_data.data);
+        if (not custom_data_json.contains("vendorId")) {
+            EVLOG_warning << "DataTransferRequest custom_data.data does not contain vendorId, automatically adding it";
+            custom_data_json["vendorId"] = custom_data.vendor_id;
+        }
+        ocpp_request.customData = custom_data_json;
+    }
+    auto ocpp_response = mod->charge_point->data_transfer_req(ocpp_request);
     types::ocpp::DataTransferResponse response;
     response.status = conversions::to_everest_data_transfer_status(ocpp_response.status);
     response.data = ocpp_response.data;
+    if (ocpp_response.customData.has_value()) {
+        auto ocpp_custom_data = ocpp_response.customData.value();
+        types::ocpp::CustomData custom_data{ocpp_custom_data.at("vendorId"), ocpp_custom_data.dump()};
+        response.custom_data = ocpp_custom_data;
+    }
     return response;
 }
 
