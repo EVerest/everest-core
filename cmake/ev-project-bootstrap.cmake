71efeb974629e6c025355a2d5153783cd91e0db3
Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
2024-04-22 12:37:20 +0200

    Use everest-cmake 0.3
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>

--- cmake/ev-project-bootstrap.cmake
+++ cmake/ev-project-bootstrap.cmake
@@ -3,7 +3,6 @@
 include(${CMAKE_CURRENT_LIST_DIR}/ev-cli.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/config-run-script.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/config-run-nodered-script.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/ev-version-information.cmake)
 
 # dependencies
 require_ev_cli_version("0.1.0")
