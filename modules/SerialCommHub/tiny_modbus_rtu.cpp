02658a919ec8a30119c4d4c6b51fbabc31bb855d
Moritz Barsnick <moritz.barsnick@chargebyte.com>
2024-05-07 14:10:53 +0200

    SerialCommHub: report modbus address in short packet messages
    Signed-off-by: Moritz Barsnick <moritz.barsnick@chargebyte.com>

--- modules/SerialCommHub/tiny_modbus_rtu.cpp
+++ modules/SerialCommHub/tiny_modbus_rtu.cpp
@@ -79,10 +79,10 @@ static std::vector<uint16_t> decode_reply(const uint8_t* buf, int len, uint8_t e
                                           FunctionCode function) {
     std::vector<uint16_t> result;
     if (len == 0) {
-        EVLOG_error << fmt::format("Packet receive timeout.");
+        EVLOG_error << fmt::format("Packet receive timeout (device address {}).", expected_device_address);
         return result;
     } else if (len < MODBUS_MIN_REPLY_SIZE) {
-        EVLOG_error << fmt::format("Packet too small: {} bytes.", len);
+        EVLOG_error << fmt::format("Packet too small: {} bytes (device address {}).", len, expected_device_address);
         return result;
     }
     if (expected_device_address != buf[DEVICE_ADDRESS_POS]) {
