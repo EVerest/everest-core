e8ec2c4acc9734d6c68b67f5ae2239486af9df6a
corneliusclaussen <62659547+corneliusclaussen@users.noreply.github.com>
2024-05-15 08:02:02 +0200

    Phytec PhyVerso BSP driver (#648)
    * Phytec PhyVerso BSP driver
    
    Signed-off-by: Cornelius Claussen <cc@pionix.de>
    
    * Fixup
    
    Signed-off-by: Dima Dorezyuk <ddo@qwello.eu>
    
    * More fixes
    
    Signed-off-by: Dima Dorezyuk <ddo@qwello.eu>
    
    * clang-format
    
    Signed-off-by: Dima Dorezyuk <ddo@qwello.eu>
    
    * fix reviewer remarks
    
    Signed-off-by: Dima Dorezyuk <ddo@qwello.eu>
    
    * reviewer comments
    
    Signed-off-by: Dima Dorezyuk <ddo@qwello.eu>
    
    ---------
    
    Signed-off-by: Cornelius Claussen <cc@pionix.de>
    Signed-off-by: Dima Dorezyuk <ddo@qwello.eu>
    Co-authored-by: Dima Dorezyuk <ddo@qwello.eu>
    Co-authored-by: Dima Dorezyuk <dmitrij.dorezyuk@hotmail.de>

---
+++ modules/PhyVersoBSP/PhyVersoBSP.cpp
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Pionix GmbH and Contributors to EVerest
+
+#include "PhyVersoBSP.hpp"
+#include <filesystem>
+
+namespace module {
+
+void PhyVersoBSP::init() {
+    // initialize serial driver
+    if (!serial.open_device(config.serial_port.c_str(), config.baud_rate)) {
+        EVLOG_AND_THROW(EVEXCEPTION(Everest::EverestConfigError, "Could not open serial port ", config.serial_port,
+                                    " with baud rate ", config.baud_rate));
+        return;
+    }
+
+    invoke_init(*p_connector_1);
+    invoke_init(*p_connector_2);
+    invoke_init(*p_rcd_1);
+    invoke_init(*p_rcd_2);
+    invoke_init(*p_connector_lock_1);
+    invoke_init(*p_connector_lock_2);
+
+    std::filesystem::path mcu_config_file = config.mcu_config_file;
+
+    if (mcu_config_file.is_relative()) {
+        // Add everest config folder prefix if relative path is given
+        mcu_config_file = info.paths.etc / mcu_config_file;
+    }
+
+    if (!verso_config.open_file(mcu_config_file.string())) {
+        EVLOG_AND_THROW(EVEXCEPTION(Everest::EverestConfigError, "Could not open config file ", mcu_config_file));
+    }
+
+    serial.signal_config_request.connect([&]() {
+        serial.send_config(verso_config);
+        EVLOG_info << "Sent config packet to MCU";
+    });
+}
+
+void PhyVersoBSP::ready() {
+    serial.run();
+
+    invoke_ready(*p_connector_1);
+    invoke_ready(*p_connector_2);
+    invoke_ready(*p_rcd_1);
+    invoke_ready(*p_rcd_2);
+    invoke_ready(*p_connector_lock_1);
+    invoke_ready(*p_connector_lock_2);
+}
+
+} // namespace module
