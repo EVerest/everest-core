f6031f6a12040a070045a33ba4497b2ad108bf74
Andreas Heinrich <andreas.heinrich@rwth-aachen.de>
2024-05-16 13:03:20 +0200

    test
    Signed-off-by: Andreas Heinrich <andreas.heinrich@rwth-aachen.de>

--- .ci/build-kit/prepare_integration_tests.sh
+++ .ci/build-kit/prepare_integration_tests.sh
@@ -2,6 +2,8 @@
 
 # ninja -j$(nproc) -C build tests
 ninja -j$(nproc) -C build install
+ninja -C build everestpy_install_wheel
+ninja -C build everest-testing_install_wheel
 
 # install everest testing by cmake target to make sure using the version defined in dependencies.yaml
 ninja -C build install_everest_testing
