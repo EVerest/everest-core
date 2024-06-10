fbe883f36941c3f551243890ed9e6c8c12201b88
aw <aw@pionix.de>
2024-06-10 20:14:19 +0200

    feat(edm): watch dependencies.yaml for rerun
    - add the file `dependencies.yaml` to the list of files which trigger a
      rerun of cmake, when changed
    
    Signed-off-by: aw <aw@pionix.de>

--- edm.cmake
+++ edm.cmake
@@ -5,6 +5,14 @@ function (evc_setup_edm)
         message(FATAL_ERROR "Could not find EVerest dependency manager. Please make it available in your PATH.")
     endif()
 
+    # add the dependencies.yaml to the list of files which trigger
+    # a cmake rerun, when changed
+    set_property(
+        DIRECTORY
+        APPEND
+        PROPERTY CMAKE_CONFIGURE_DEPENDS dependencies.yaml
+    )
+
     execute_process(
         COMMAND "${EVEREST_DEPENDENCY_MANAGER}"
             --cmake
