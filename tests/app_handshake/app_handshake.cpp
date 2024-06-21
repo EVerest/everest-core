ec437e6060e30bfa2850139c87a10c3f61f546a0
Sebastian Lukas <45936573+SebaLukas@users.noreply.github.com>
2024-06-21 10:40:57 +0200

    Refactoring unit tests (#15)
    - Simplify tests & adding utility functions
    - Adding tests to test prober en- & decoding ISO20 bpt control mode
    - Fixing session setup res tests
    
    Co-authored-by: aw <aw@pionix.de>
    Signed-off-by: Sebastian Lukas <sebastian.lukas@pionix.de>

--- tests/app_handshake/app_handshake.cpp
+++ tests/app_handshake/app_handshake.cpp
@@ -5,25 +5,22 @@
 #include <cbv2g/app_handshake/appHand_Datatypes.h>
 #include <cbv2g/app_handshake/appHand_Decoder.h>
 
+#include "test_utils/codec.hpp"
+
 SCENARIO("Encode and decode app protocol request messages") {
 
     GIVEN("Decode an AppProtocolReq document") {
 
-        uint8_t doc_raw[] = "\x80"
-                            "\x00\xf3\xab\x93\x71\xd3\x4b\x9b\x79\xd3\x9b\xa3\x21\xd3\x4b\x9b\x79"
-                            "\xd1\x89\xa9\x89\x89\xc1\xd1\x69\x91\x81\xd2\x0a\x18\x01\x00\x00\x04"
-                            "\x00\x40";
-
-        exi_bitstream_t exi_stream_in;
-        size_t pos1 = 0;
-        int errn = 0;
-
-        exi_bitstream_init(&exi_stream_in, reinterpret_cast<uint8_t*>(doc_raw), sizeof(doc_raw), pos1, nullptr);
+        uint8_t doc_raw[] = {0x80, 0x00, 0xf3, 0xab, 0x93, 0x71, 0xd3, 0x4b, 0x9b, 0x79, 0xd3, 0x9b, 0xa3,
+                             0x21, 0xd3, 0x4b, 0x9b, 0x79, 0xd1, 0x89, 0xa9, 0x89, 0x89, 0xc1, 0xd1, 0x69,
+                             0x91, 0x81, 0xd2, 0x0a, 0x18, 0x01, 0x00, 0x00, 0x04, 0x00, 0x40};
 
         THEN("It should be decoded succussfully") {
-            appHand_exiDocument request;
+            const auto result = test_utils::decode<appHand_exiDocument>(doc_raw, sizeof(doc_raw));
+            REQUIRE(result.decoding_successful);
 
-            REQUIRE(decode_appHand_exiDocument(&exi_stream_in, &request) == 0);
+            const auto& request = result.value;
+
             REQUIRE(request.supportedAppProtocolReq_isUsed == 1);
 
             REQUIRE(request.supportedAppProtocolReq.AppProtocol.arrayLen == 1);
@@ -45,18 +42,14 @@ SCENARIO("Encode and decode app protocol response messages") {
 
     GIVEN("Decode an AppProtocolRes document") {
 
-        uint8_t doc_raw[] = "\x80\x40\x00\x40";
-
-        exi_bitstream_t exi_stream_in;
-        size_t pos1 = 0;
-        int errn = 0;
-
-        exi_bitstream_init(&exi_stream_in, reinterpret_cast<uint8_t*>(doc_raw), sizeof(doc_raw), pos1, nullptr);
+        uint8_t doc_raw[] = {0x80, 0x40, 0x00, 0x40};
 
         THEN("It should be decoded succussfully") {
-            appHand_exiDocument response;
+            const auto result = test_utils::decode<appHand_exiDocument>(doc_raw, sizeof(doc_raw));
+            REQUIRE(result.decoding_successful);
 
-            REQUIRE(decode_appHand_exiDocument(&exi_stream_in, &response) == 0);
+            const auto& response = result.value;
+
             REQUIRE(response.supportedAppProtocolRes_isUsed == 1);
 
             REQUIRE(response.supportedAppProtocolRes.ResponseCode == appHand_responseCodeType_OK_SuccessfulNegotiation);
--- tests/din/service_discovery.cpp
+++ tests/din/service_discovery.cpp
@@ -1,15 +1,17 @@
 #include <catch2/catch_test_macros.hpp>
 
+#include <array>
 #include <cstdint>
 #include <iterator>
 #include <string>
 #include <vector>
 
-#include <array>
 #include <cbv2g/din/din_msgDefDecoder.h>
 #include <cbv2g/din/din_msgDefEncoder.h>
 
-SCENARIO("Encode and decode a DIN service discovery message") {
+#include "test_utils/codec.hpp"
+
+SCENARIO("Encode and decode a DIN70121 service discovery message") {
 
     // Exi Stream: 809a0211d63f74d2297ac9119400
     // XML:
@@ -26,7 +28,7 @@ SCENARIO("Encode and decode a DIN service discovery message") {
 
     GIVEN("Good case - Encode a ServiceDiscoveryReq document") {
 
-        uint8_t doc_raw[] = "\x80\x9a\x02\x11\xd6\x3f\x74\xd2\x29\x7a\xc9\x11\x94\x00";
+        uint8_t doc_raw[] = {0x80, 0x9a, 0x02, 0x11, 0xd6, 0x3f, 0x74, 0xd2, 0x29, 0x7a, 0xc9, 0x11, 0x94, 0x00};
 
         din_exiDocument request;
         init_din_exiDocument(&request);
@@ -52,40 +54,24 @@ SCENARIO("Encode and decode a DIN service discovery message") {
         body.ServiceDiscoveryReq.ServiceCategory_isUsed = true;
         body.ServiceDiscoveryReq.ServiceCategory = din_serviceCategoryType_EVCharging;
 
-        uint8_t stream[256] = {};
-        exi_bitstream_t exi_stream_in;
-        size_t pos1 = 0;
-        int errn = 0;
-
-        exi_bitstream_init(&exi_stream_in, stream, sizeof(stream), pos1, nullptr);
-
         THEN("It should be encoded succussfully") {
+            const auto result = test_utils::encode_and_compare(request, doc_raw, sizeof(doc_raw));
 
-            REQUIRE(encode_din_exiDocument(&exi_stream_in, &request) == 0);
-
-            const auto encoded_stream =
-                std::vector<uint8_t>(stream, stream + exi_bitstream_get_length(&exi_stream_in) + 1);
-
-            const auto expected_exi_stream = std::vector<uint8_t>(std::begin(doc_raw), std::end(doc_raw));
-
-            REQUIRE(encoded_stream == expected_exi_stream);
+            REQUIRE(result.encoding_successful);
+            REQUIRE(result.bitstream_match);
         }
     }
 
     GIVEN("Good case - Decode a ServiceDiscoveryReq document") {
 
         auto expected_session_id = std::vector<uint8_t>{0x47, 0x58, 0xFD, 0xD3, 0x48, 0xA5, 0xEB, 0x24};
-        uint8_t doc_raw[] = "\x80\x9a\x02\x11\xd6\x3f\x74\xd2\x29\x7a\xc9\x11\x94\x00";
-        exi_bitstream_t exi_stream_in;
-        size_t pos1 = 0;
-        int errn = 0;
-
-        exi_bitstream_init(&exi_stream_in, reinterpret_cast<uint8_t*>(doc_raw), sizeof(doc_raw), pos1, nullptr);
+        uint8_t doc_raw[] = {0x80, 0x9a, 0x02, 0x11, 0xd6, 0x3f, 0x74, 0xd2, 0x29, 0x7a, 0xc9, 0x11, 0x94, 0x00};
 
         THEN("It should be decoded succussfully") {
-            din_exiDocument request;
+            const auto result = test_utils::decode<din_exiDocument>(doc_raw, sizeof(doc_raw));
+            REQUIRE(result.decoding_successful);
 
-            REQUIRE(decode_din_exiDocument(&exi_stream_in, &request) == 0);
+            const auto& request = result.value;
 
             // Check Header
             const auto& header = request.V2G_Message.Header;
@@ -130,7 +116,8 @@ SCENARIO("Encode and decode a DIN service discovery message") {
     // </ns7:V2G_Message>
 
     GIVEN("Good case - Encode an ServiceDiscoveryRes document") {
-        uint8_t doc_raw[] = "\x80\x9a\x02\x11\xd6\x3f\x74\xd2\x29\x7a\xc9\x11\xa0\x01\x20\x02\x41\x00\xc4";
+        uint8_t doc_raw[] = {0x80, 0x9a, 0x02, 0x11, 0xd6, 0x3f, 0x74, 0xd2, 0x29, 0x7a,
+                             0xc9, 0x11, 0xa0, 0x01, 0x20, 0x02, 0x41, 0x00, 0xc4};
 
         din_exiDocument request;
         init_din_exiDocument(&request);
@@ -169,39 +156,24 @@ SCENARIO("Encode and decode a DIN service discovery message") {
         chargeService.FreeService = false;
         chargeService.EnergyTransferType = din_EVSESupportedEnergyTransferType_DC_extended;
 
-        uint8_t stream[256] = {};
-        exi_bitstream_t exi_stream_in;
-        size_t pos1 = 0;
-        int errn = 0;
-
-        exi_bitstream_init(&exi_stream_in, stream, sizeof(stream), pos1, nullptr);
-
         THEN("It should be encoded succussfully") {
+            const auto result = test_utils::encode_and_compare(request, doc_raw, sizeof(doc_raw));
 
-            REQUIRE(encode_din_exiDocument(&exi_stream_in, &request) == 0);
-
-            const auto encoded_stream =
-                std::vector<uint8_t>(stream, stream + exi_bitstream_get_length(&exi_stream_in) + 1);
-
-            const auto expected_exi_stream = std::vector<uint8_t>(std::begin(doc_raw), std::end(doc_raw));
-
-            REQUIRE(encoded_stream == expected_exi_stream);
+            REQUIRE(result.encoding_successful);
+            REQUIRE(result.bitstream_match);
         }
     }
 
     GIVEN("Good case - Decode an ServiceDiscoveryRes document") {
         auto expected_session_id = std::vector<uint8_t>{0x47, 0x58, 0xFD, 0xD3, 0x48, 0xA5, 0xEB, 0x24};
-        uint8_t doc_raw[] = "\x80\x9a\x02\x11\xd6\x3f\x74\xd2\x29\x7a\xc9\x11\xa0\x01\x20\x02\x41\x00\xc4";
-        exi_bitstream_t exi_stream_in;
-        size_t pos1 = 0;
-        int errn = 0;
-
-        exi_bitstream_init(&exi_stream_in, reinterpret_cast<uint8_t*>(doc_raw), sizeof(doc_raw), pos1, nullptr);
+        uint8_t doc_raw[] = {0x80, 0x9a, 0x02, 0x11, 0xd6, 0x3f, 0x74, 0xd2, 0x29, 0x7a,
+                             0xc9, 0x11, 0xa0, 0x01, 0x20, 0x02, 0x41, 0x00, 0xc4};
 
         THEN("It should be decoded succussfully") {
-            din_exiDocument request;
+            const auto result = test_utils::decode<din_exiDocument>(doc_raw, sizeof(doc_raw));
+            REQUIRE(result.decoding_successful);
 
-            REQUIRE(decode_din_exiDocument(&exi_stream_in, &request) == 0);
+            const auto& request = result.value;
 
             // Check Header
             const auto& header = request.V2G_Message.Header;
