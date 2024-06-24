3213f4ff80d69fcb80932439954f66af7f144c0a
Kai Hermann <kai-uwe.hermann@pionix.de>
2024-06-24 15:51:00 +0200

    setup_ev_cli should not return after find_program (#741)
    * Update everest-utils dependency and remove cmake_condition
    ev-cli is not getting installed via this dependency which means it is also needed if not building for testing
    
    * Do not return in setup_ev_cli macro after find_program call
    * Update comment
    
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>

--- cmake/ev-cli.cmake
+++ cmake/ev-cli.cmake
@@ -6,18 +6,18 @@ macro(setup_ev_cli)
     endif()
     if(NOT ${${PROJECT_NAME}_USE_PYTHON_VENV})
         find_program(EV_CLI ev-cli REQUIRED)
-        return()
+    else()
+        ev_is_python_venv_active(
+            RESULT_VAR IS_PYTHON_VENV_ACTIVE
+        )
+        if(NOT ${IS_PYTHON_VENV_ACTIVE})
+            message(FATAL_ERROR "Python venv is not active. Please activate the python venv before running this command.")
+        endif()
+        set(EV_CLI "${${PROJECT_NAME}_PYTHON_VENV_PATH}/bin/ev-cli")
+        add_dependencies(ev-cli
+            ev-dev-tools_pip_install_dist
+        )
     endif()
-    ev_is_python_venv_active(
-        RESULT_VAR IS_PYTHON_VENV_ACTIVE
-    )
-    if(NOT ${IS_PYTHON_VENV_ACTIVE})
-        message(FATAL_ERROR "Python venv is not active. Please activate the python venv before running this command.")
-    endif()
-    set(EV_CLI "${${PROJECT_NAME}_PYTHON_VENV_PATH}/bin/ev-cli")
-    add_dependencies(ev-cli
-        ev-dev-tools_pip_install_dist
-    )
 endmacro()
 
 function(require_ev_cli_version EV_CLI_VERSION_REQUIRED)
