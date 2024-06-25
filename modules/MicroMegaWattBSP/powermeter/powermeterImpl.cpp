df695a5a0f6d57c9556369a2cc583106ae31a19d
Cornelius Claussen <cc@pionix.de>
2024-06-25 14:19:50 +0200

    Powermeter proper start/stop reply
    Signed-off-by: Cornelius Claussen <cc@pionix.de>

--- modules/MicroMegaWattBSP/powermeter/powermeterImpl.cpp
+++ modules/MicroMegaWattBSP/powermeter/powermeterImpl.cpp
@@ -15,12 +15,15 @@ void powermeterImpl::ready() {
 types::powermeter::TransactionStartResponse
 powermeterImpl::handle_start_transaction(types::powermeter::TransactionReq& value) {
     // your code for cmd start_transaction goes here
-    return {};
+    return {types::powermeter::TransactionRequestStatus::NOT_SUPPORTED,
+            "MicroMegaWattBSP powermeter does not support the start_transaction command"};
 }
 
 types::powermeter::TransactionStopResponse powermeterImpl::handle_stop_transaction(std::string& transaction_id) {
-    // your code for cmd stop_transaction goes here
-    return {};
+    return {types::powermeter::TransactionRequestStatus::NOT_SUPPORTED,
+            {},
+            {},
+            "MicroMegaWattBSP powermeter does not support the stop_transaction command"};
 }
 
 } // namespace powermeter
