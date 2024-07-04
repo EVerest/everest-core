36871ebc4442b794f077a2387de619398913a4aa
Sebastian Lukas <sebastian.lukas@pionix.de>
2024-07-04 09:14:30 +0200

    Fix - DcSupplySimulator does not support start transaction request, using LOOP_INTERVAL_MS instead of fixed 0.5 seconds
    Signed-off-by: Sebastian Lukas <sebastian.lukas@pionix.de>

--- modules/simulation/DCSupplySimulator/powermeter/powermeterImpl.cpp
+++ modules/simulation/DCSupplySimulator/powermeter/powermeterImpl.cpp
@@ -14,7 +14,8 @@ void powermeterImpl::ready() {
 
 types::powermeter::TransactionStartResponse
 powermeterImpl::handle_start_transaction(types::powermeter::TransactionReq& value) {
-    return {types::powermeter::TransactionRequestStatus::OK};
+    return {types::powermeter::TransactionRequestStatus::NOT_SUPPORTED,
+            "DcSupplySimulator does not support start transaction request."};
 }
 
 types::powermeter::TransactionStopResponse powermeterImpl::handle_stop_transaction(std::string& transaction_id) {
