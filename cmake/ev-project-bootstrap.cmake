e9d8f3912d9e834823c8ce6776148a8490a29f54
Kai Hermann <kai-uwe.hermann@pionix.de>
2024-07-10 10:45:57 +0200

    Fix ev-cli version check if not using venv (#772)
    * Use global property to store required ev-cli version
    
    The variable used before could sometimes not be set which would lead to errors
    
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
    
    * Remove redundant option to install ev-cli in venv
    
    This was not actually used to decide if ev-cli should be installed in the venv or not.
    
    No we just use the $PROJECT_NAME_USE_PYTHON_VENV for the ev-cli version check if a venv is not to be used
    
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
    
    * Add logging which method of ev-cli usage/installation is used
    
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
    
    ---------
    
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>

--- cmake/ev-project-bootstrap.cmake
+++ cmake/ev-project-bootstrap.cmake
@@ -4,7 +4,10 @@ include(${CMAKE_CURRENT_LIST_DIR}/ev-cli.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/config-run-script.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/config-run-nodered-script.cmake)
 
-set(EVEREST_REQUIRED_EV_CLI_VERSION "0.2.0")
+set_property(
+    GLOBAL
+    PROPERTY EVEREST_REQUIRED_EV_CLI_VERSION "0.2.0"
+)
 
 # source generate scripts / setup
 include(${CMAKE_CURRENT_LIST_DIR}/everest-generate.cmake)
