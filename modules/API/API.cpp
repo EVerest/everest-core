7d02eb70485082c971870cb12acf85fcc9a8f833
James Chapman <147724513+james-ctc@users.noreply.github.com>
2024-07-17 12:10:28 +0200

    fix: everest_api/evse_manager/cmd/force_unlock to also stop any transaction (#786)
    Signed-off-by: James Chapman <james.chapman@pionix.de>

--- modules/API/API.cpp
+++ modules/API/API.cpp
@@ -525,7 +525,13 @@ void API::init() {
                                 << ", error: " << e.what();
                 }
             }
-            evse->call_force_unlock(connector_id); //
+            // match processing in ChargePointImpl::handleUnlockConnectorRequest
+            // so that OCPP UnlockConnector and everest_api/evse_manager/cmd/force_unlock
+            // perform the same action
+            types::evse_manager::StopTransactionRequest req;
+            req.reason = types::evse_manager::StopTransactionReason::UnlockCommand;
+            evse->call_stop_transaction(req);
+            evse->call_force_unlock(connector_id);
         });
 
         // Check if a uk_random_delay is connected that matches this evse_manager
