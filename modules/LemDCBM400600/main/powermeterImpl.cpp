269d5fed11512aa7daaa28c91f3794fd1f5ea4c3
florinmihut <florinmihut1@gmail.com>
2024-07-19 12:58:58 +0200

    Raise a CommunicationFault if we can't communicate with the LEM module and clear it once the connection is re-established
    Signed-off-by: florinmihut <florinmihut1@gmail.com>

--- modules/LemDCBM400600/main/powermeterImpl.cpp
+++ modules/LemDCBM400600/main/powermeterImpl.cpp
@@ -7,6 +7,12 @@
 #include <chrono>
 #include <string>
 #include <thread>
+#include <utils/error/error_factory.hpp>
+#include <utils/error/error_state_monitor.hpp>
+
+using Error = Everest::error::Error;
+using Condition = Everest::error::ErrorStateMonitor::StateCondition;
+
 
 namespace module::main {
 
@@ -35,15 +41,25 @@ void powermeterImpl::ready() {
     // Start the live_measure_publisher thread, which periodically publishes the live measurements of the device
     this->live_measure_publisher_thread = std::thread([this] {
         this->publish_public_key_ocmf(this->controller->get_public_key_ocmf());
+        const Condition condition = Condition("powermeter/CommunicationFault", "Communication timed out", true);
         while (true) {
             std::this_thread::sleep_for(std::chrono::milliseconds(1000));
             try {
                 this->publish_powermeter(this->controller->get_powermeter());
+                // if the communication error is set, clear the error
+                if (this->error_state_monitor->is_condition_satisfied(condition)) {
+                    clear_error("powermeter/CommunicationFault", "Communication timed out");
+                }
             } catch (LemDCBM400600Controller::DCBMUnexpectedResponseException& dcbm_exception) {
                 EVLOG_error << "Failed to publish powermeter value due to an invalid device response: "
                             << dcbm_exception.what();
             } catch (HttpClientError& client_error) {
                 EVLOG_error << "Failed to publish powermeter value due to an http error: " << client_error.what();
+                if (!this->error_state_monitor->is_condition_satisfied(condition)) {
+                    Error error = this->error_factory->create_error("powermeter/CommunicationFault", "Communication timed out",
+                                                                    "This error is raised due to communication timeout");
+                    raise_error(error);
+                }
             }
         }
     });
