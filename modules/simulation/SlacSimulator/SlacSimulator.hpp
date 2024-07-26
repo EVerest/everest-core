9f6cb14796edf9202bc5c9ac6cf10521afd2183e
Hasjim Williams <431684+futaris@users.noreply.github.com>
2024-07-26 16:31:51 +1000

    fix
    Signed-off-by: Hasjim Williams <431684+futaris@users.noreply.github.com>

--- modules/simulation/SlacSimulator/SlacSimulator.hpp
+++ modules/simulation/SlacSimulator/SlacSimulator.hpp
@@ -29,14 +29,7 @@ public:
     SlacSimulator() = delete;
     SlacSimulator(const ModuleInfo& info, Everest::MqttProvider& mqtt_provider, std::unique_ptr<slacImplBase> p_evse,
                   std::unique_ptr<ev_slacImplBase> p_ev, Conf& config) :
-        ModuleBase(info),
-        mqtt(mqtt_provider),
-        p_evse(std::move(p_evse)),
-        p_ev(std::move(p_ev)),
-        config(config),
-        config.state_evse(STATE_UNMATCHED),
-        config.state_ev(STATE_UNMATCHED),
-        config.cntmatched(0){};
+        ModuleBase(info), mqtt(mqtt_provider), p_evse(std::move(p_evse)), p_ev(std::move(p_ev)), config(config){};
 
     Everest::MqttProvider& mqtt;
     const std::unique_ptr<slacImplBase> p_evse;
