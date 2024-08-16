033b18cd67a95821c554258d6e4697ae2cdaa633
Katja Romahn <37145145+kromahn@users.noreply.github.com>
2024-08-16 11:59:42 +0200

    Fix build of example v16 binary (#731)
    Signed-off-by: Katja Romahn <romahn@ambibox.de>
    Co-authored-by: Katja Romahn <romahn@ambibox.de>

--- src/charge_point.cpp
+++ src/charge_point.cpp
@@ -247,9 +247,9 @@ int main(int argc, char* argv[]) {
                   << "Setting charging profiles" << std::endl;
     });
 
-    charge_point->register_transaction_updated_callback([](const int32_t connector, const int32_t transaction_id,
-                                                           const std::string& session_id,
-                                                           const IdTagInfo& id_tag_info) {
+    charge_point->register_transaction_updated_callback([](const int32_t connector, const std::string& session_id,
+                                                           const int32_t transaction_id,
+                                                           const ocpp::v16::IdTagInfo& id_tag_info) {
         std::cout << "Callback: Transaction updated at connector# " << connector
                   << " and transaction id: " << transaction_id << std::endl;
     });
