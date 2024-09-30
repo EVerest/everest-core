defe6ad955bcf11cbe3ebdfbf6648661f56e91fb
Cornelius Claussen <cc@pionix.de>
2024-09-30 16:35:35 +0200

    Basic charging: use state F to signal error
    - Set state F for 300ms (configurable) after fault when in state C.
    - Unlock connector in F after 5s to avoid lock/unlock in all temporary t_step_EFs
    
    Signed-off-by: Cornelius Claussen <cc@pionix.de>

--- modules/EvseManager/EvseManager.hpp
+++ modules/EvseManager/EvseManager.hpp
@@ -99,6 +99,7 @@ struct Conf {
     std::string switch_3ph1ph_cp_state;
     int soft_over_current_timeout_ms;
     bool lock_connector_in_state_b;
+    int state_F_after_fault_ms;
 };
 
 class EvseManager : public Everest::ModuleBase {
