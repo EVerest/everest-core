8b8c605edecb8619a9bbd84de6c8502a89c6075d
Yonatan Dankner <yda@qwello.eu>
2024-10-10 17:50:57 +0200

    Published Auth timeout ifor one additional flow
    Signed-off-by: Yonatan Dankner <yda@qwello.eu>

--- modules/Auth/lib/AuthHandler.cpp
+++ modules/Auth/lib/AuthHandler.cpp
@@ -497,6 +497,7 @@ void AuthHandler::notify_evse(int connector_id, const ProvidedIdToken& provided_
                     EVLOG_info << "Authorization timeout for evse#" << evse_index;
                     this->connectors.at(connector_id)->connector.identifier.reset();
                     this->withdraw_authorization_callback(evse_index);
+                    this->publish_token_validation_status_callback(provided_token, TokenValidationStatus::TimedOut);
                 },
                 std::chrono::seconds(this->connection_timeout));
         std::lock_guard<std::mutex> plug_in_lk(this->plug_in_queue_mutex);
