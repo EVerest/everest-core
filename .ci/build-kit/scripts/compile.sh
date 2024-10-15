3f82c2850d27e787d3b45a773769eda37ef95435
aw <aw@pionix.de>
2024-10-15 17:08:08 +0200

    custom docker currently disabled -> use compile.sh
    Signed-off-by: aw <aw@pionix.de>

--- .ci/build-kit/scripts/compile.sh
+++ .ci/build-kit/scripts/compile.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+
+EVEREST_CMAKE_PATH=/usr/lib/cmake/everest-cmake
+EVEREST_CMAKE_VERSION=4d81814
+rm -rf ${EVEREST_CMAKE_PATH}
+git clone https://github.com/EVerest/everest-cmake.git ${EVEREST_CMAKE_PATH} \
+    && cd ${EVEREST_CMAKE_PATH} \
+    && git checkout ${EVEREST_CMAKE_VERSION}
+
 cmake \
     -B "$EXT_MOUNT/build" \
     -S "$EXT_MOUNT/source" \
