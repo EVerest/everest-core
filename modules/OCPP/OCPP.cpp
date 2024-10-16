90e71a16d4b302163726e555a650e332fd75e755
Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
2024-10-16 12:28:47 +0200

    Add missing namespace to conversion
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>

--- modules/OCPP/OCPP.cpp
+++ modules/OCPP/OCPP.cpp
@@ -141,7 +141,7 @@ void OCPP::process_session_event(int32_t evse_id, const types::evse_manager::Ses
                    << "Received TransactionStarted";
         const auto transaction_started = session_event.transaction_started.value();
 
-        const auto timestamp = to_ocpp_datetime_or_now(session_event.timestamp);
+        const auto timestamp = ocpp_conversions::to_ocpp_datetime_or_now(session_event.timestamp);
         const auto energy_Wh_import = transaction_started.meter_value.energy_Wh_import.total;
         const auto session_id = session_event.uuid;
         const auto id_token = transaction_started.id_tag.id_token.value;
@@ -177,7 +177,7 @@ void OCPP::process_session_event(int32_t evse_id, const types::evse_manager::Ses
                     << "Received TransactionFinished";
 
         const auto transaction_finished = session_event.transaction_finished.value();
-        const auto timestamp = to_ocpp_datetime_or_now(session_event.timestamp);
+        const auto timestamp = ocpp_conversions::to_ocpp_datetime_or_now(session_event.timestamp);
         const auto energy_Wh_import = transaction_finished.meter_value.energy_Wh_import.total;
         const auto signed_meter_value = transaction_finished.signed_meter_value;
 
