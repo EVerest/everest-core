a8fae465600ee017335723c8fbeddf8bc6992b50
Christoph Burandt <cburandt@mailbox.org>
2024-10-22 11:58:45 +0200

    Fix hardcoded config path to align with the EVerest install dir

--- lib/staging/linux_libnfc-nci/src/libnfc-utils/src/ConfigPathProvider.cc
+++ lib/staging/linux_libnfc-nci/src/libnfc-utils/src/ConfigPathProvider.cc
@@ -83,17 +83,17 @@ string ConfigPathProvider::getFilePath(FileType type) {
         << StringPrintf("%s: enter FileType:0x%02x", __func__, type);
     switch (type) {
     case VENDOR_NFC_CONFIG: {
-        string path = "//usr//local//etc//libnfc-nxp.conf";
+        string path = CONFIG_PATH "libnfc-nxp.conf";
         addEnvPathIfAvailable(path);
         return path;
     } break;
     case VENDOR_ESE_CONFIG: {
-        string path = "//usr//local//etc//libese-nxp.conf";
+        string path = CONFIG_PATH "libese-nxp.conf";
         addEnvPathIfAvailable(path);
         return path;
     } break;
     case SYSTEM_CONFIG: {
-        const vector<string> searchPath = { "//usr/local//etc//" };
+        const vector<string> searchPath = { CONFIG_PATH };
         for (string path : searchPath) {
             addEnvPathIfAvailable(path);
             path.append("libnfc-nci.conf");
@@ -105,12 +105,12 @@ string ConfigPathProvider::getFilePath(FileType type) {
         return "";
     } break;
     case RF_CONFIG: {
-        string path = "//usr//local//etc//libnfc-nxp_RF.conf";
+        string path = CONFIG_PATH "libnfc-nxp_RF.conf";
         addEnvPathIfAvailable(path);
         return path;
     } break;
     case TRANSIT_CONFIG: {
-        string path = "//usr/local//etc//libnfc-nxpTransit.conf";
+        string path = CONFIG_PATH "libnfc-nxpTransit.conf";
         addEnvPathIfAvailable(path);
         return path;
     } break;
@@ -121,22 +121,22 @@ string ConfigPathProvider::getFilePath(FileType type) {
         return nfc_storage_path;
     } break;
     case FIRMWARE_LIB: {
-        string path = "//usr/local//etc//libsn100u_fw.dll";
+        string path = CONFIG_PATH "libsn100u_fw.dll";
         addEnvPathIfAvailable(path);
         return path;
     } break;
     case CONFIG_TIMESTAMP: {
-        string path = "//usr//local//etc//libnfc-nxpConfigState.bin";
+        string path = CONFIG_PATH "libnfc-nxpConfigState.bin";
         addEnvPathIfAvailable(path);
         return path;
     } break;
     case RF_CONFIG_TIMESTAMP: {
-        string path = "//usr//local//etc//libnfc-nxpRFConfigState.bin";
+        string path = CONFIG_PATH "libnfc-nxpRFConfigState.bin";
         addEnvPathIfAvailable(path);
         return path;
     } break;
     case TRANSIT_CONFIG_TIMESTAMP: {
-        string path = "//usr//local//etc//libnfc-nxpTransitConfigState.bin";
+        string path = CONFIG_PATH "libnfc-nxpTransitConfigState.bin";
         addEnvPathIfAvailable(path);
         return path;
     } break;
