3cf526c8be657bf9b48962a0e42d8f143a77f5cf
Christoph Burandt <christoph.burandt@pionix.de>
2024-10-23 17:12:17 +0200

    Feature: NXP NFC frontend token provider
    * Initial working version of Nxp Nfc Reader Library Support
    * Make NxpNfcFrontendTokenProvider buildable with a stub, without requiring NxpNfcFrontendWrapper
    * Add rst doc
    * Add BringUp module for NXP NFC Reader
    * Change name of CMake option to contain the module name
    * Fix wrong content of BUNfcReader documentation
    * Apply clang-format
    
    Signed-off-by: Christoph Burandt <christoph.burandt@pionix.de>
    
    missing include
    
    Signed-off-by: Cornelius Claussen <cc@pionix.de>
    
    Add Apache 2.0 license
    Move NxpNfcFrontendTokenProvider to modules/HardwareDrivers/NfcReaders/
    
    Add NxpNfcFrontendWrapper code and documentation
    
    Update README.md: Fix Link
    
    Refactor CMake files (+ minor refactoring in cpp files)
    
    Use anonymous namespace for private data structure
    
    Do not require the NxpNfcRdLib zip file to be extracted by the user
    
    Create everest-core compatible folder structure
    
    Add and use nxpnfcrdlib_wrapper for NxpNfcFrontendTokenProvider
    
    Signed-off-by: Christoph Burandt <christoph.burandt@pionix.de>

---
+++ modules/HardwareDrivers/NfcReaders/NxpNfcFrontendTokenProvider/main/auth_token_providerImpl.cpp
@@ -0,0 +1,85 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2020 - 2025 Pionix GmbH and Contributors to EVerest
+
+#include "auth_token_providerImpl.hpp"
+
+#include <iomanip>
+#include <sstream>
+
+namespace module {
+namespace main {
+
+static std::string nfcidToString(const std::vector<std::uint8_t>& nfcid) {
+    std::ostringstream oss;
+
+    for (std::uint16_t byte : nfcid) {
+        oss << std::setfill('0') << std::setw(2) << std::uppercase << std::hex << byte;
+    }
+
+    return oss.str();
+}
+
+void auth_token_providerImpl::detected_rfid_token_callback(
+    const std::pair<std::string, std::vector<std::uint8_t>>& reply) {
+    auto& [protocol, nfcid] = reply;
+
+    const auto now = std::chrono::steady_clock::now();
+    const auto debounce_interval = std::chrono::milliseconds(config.token_debounce_interval_ms);
+
+    types::authorization::ProvidedIdToken provided_token;
+
+    try {
+        provided_token.id_token = {nfcidToString(nfcid), types::authorization::string_to_id_token_type(protocol)};
+    } catch (const std::out_of_range& e) {
+        return;
+    }
+
+    provided_token.authorization_type = types::authorization::AuthorizationType::RFID;
+
+    if (now < last_rfid_submit + debounce_interval) {
+        if (config.debug) {
+            EVLOG_info << "Ignoring rfid/nfc token (debouncing): " << protocol << ": " << nfcidToString(nfcid);
+        }
+        return;
+    }
+
+    if (config.debug) {
+        EVLOG_info << "Publishing new rfid/nfc token: " << protocol << ": " << nfcidToString(nfcid);
+    }
+    publish_provided_token(provided_token);
+    last_rfid_submit = now;
+}
+
+void auth_token_providerImpl::error_log_callback(const std::string& message) {
+    EVLOG_warning << "NxpNfcFrontend: " << message;
+}
+
+void auth_token_providerImpl::init() {
+    stop = false;
+
+    if (config.no_physical_nfc_reader) {
+        return;
+    }
+
+    try {
+        nxpNfcFrontend = std::make_unique<NxpNfcFrontendDataSource>();
+    } catch (const std::exception& e) {
+        EVLOG_error << "Failed to initialize NxpNfcFrontend: " << e.what();
+    }
+    nxpNfcFrontend->setDetectionCallback(
+        [this](auto&&... args) { this->detected_rfid_token_callback(std::forward<decltype(args)>(args)...); });
+    nxpNfcFrontend->setErrorLogCallback(
+        [this](auto&&... args) { this->error_log_callback(std::forward<decltype(args)>(args)...); });
+}
+
+void auth_token_providerImpl::ready() {
+    if (nxpNfcFrontend) {
+        nxpNfcFrontend->run();
+    }
+    while (not stop) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
+}
+
+} // namespace main
+} // namespace module
