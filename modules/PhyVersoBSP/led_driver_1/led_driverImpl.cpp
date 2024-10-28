c66b5079ff1756206773b138d3ec684278d5a1f0
Wojciech Kula <wku@qwello.eu>
2024-10-28 11:03:00 +0100

    More linting
    Signed-off-by: Wojciech Kula <wku@qwello.eu>

--- modules/PhyVersoBSP/led_driver_1/led_driverImpl.cpp
+++ modules/PhyVersoBSP/led_driver_1/led_driverImpl.cpp
@@ -4,16 +4,16 @@
 #include "led_driverImpl.hpp"
 
 namespace {
-    LedState led_state_to_proto(const types::led_state::LedState& led_state) {
-        if (led_state == types::led_state::LedState::Red)
-            return LedState_RED;
-        if (led_state == types::led_state::LedState::Blue)
-            return LedState_BLUE;
-        if (led_state == types::led_state::LedState::Green)
-            return LedState_GREEN;
+LedState led_state_to_proto(const types::led_state::LedState& led_state) {
+    if (led_state == types::led_state::LedState::Red)
         return LedState_RED;
-    }
+    if (led_state == types::led_state::LedState::Blue)
+        return LedState_BLUE;
+    if (led_state == types::led_state::LedState::Green)
+        return LedState_GREEN;
+    return LedState_RED;
 }
+} // namespace
 
 namespace module {
 namespace led_driver_1 {
@@ -26,10 +26,9 @@ void led_driverImpl::ready() {
 
 
 void led_driverImpl::handle_set_led_state(types::led_state::LedState& led_state, int& brightness) {
-    //TODO: Implement me
     EVLOG_info << "Setting LED for connector 1 to " << led_state;
     mod->serial.set_led_state(1, led_state_to_proto(led_state), brightness);
 }
 
-} // namespace led_driver
-}
+} // namespace led_driver_1
+} // namespace module
--- modules/PhyVersoBSP/led_driver_2/led_driverImpl.cpp
+++ modules/PhyVersoBSP/led_driver_2/led_driverImpl.cpp
@@ -4,16 +4,16 @@
 #include "led_driverImpl.hpp"
 
 namespace {
-    LedState led_state_to_proto(const types::led_state::LedState& led_state) {
-        if (led_state == types::led_state::LedState::Red)
-            return LedState_RED;
-        if (led_state == types::led_state::LedState::Blue)
-            return LedState_BLUE;
-        if (led_state == types::led_state::LedState::Green)
-            return LedState_GREEN;
+LedState led_state_to_proto(const types::led_state::LedState& led_state) {
+    if (led_state == types::led_state::LedState::Red)
         return LedState_RED;
-    }
+    if (led_state == types::led_state::LedState::Blue)
+        return LedState_BLUE;
+    if (led_state == types::led_state::LedState::Green)
+        return LedState_GREEN;
+    return LedState_RED;
 }
+} // namespace
 
 namespace module {
 namespace led_driver_2 {
@@ -30,5 +30,5 @@ void led_driverImpl::handle_set_led_state(types::led_state::LedState& led_state,
     mod->serial.set_led_state(2, led_state_to_proto(led_state), brightness);
 }
 
-} // namespace led_driver
-}
+} // namespace led_driver_2
+} // namespace module
