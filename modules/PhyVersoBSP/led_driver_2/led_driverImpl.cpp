5ad67421469aba6fb4647f712886ff6e33f14b91
Wojciech Kula <wku@qwello.eu>
2024-10-31 11:47:39 +0100

    clang format
    Signed-off-by: Wojciech Kula <wku@qwello.eu>

--- modules/PhyVersoBSP/led_driver_2/led_driverImpl.cpp
+++ modules/PhyVersoBSP/led_driver_2/led_driverImpl.cpp
@@ -12,9 +12,9 @@ void led_driverImpl::init() {
 void led_driverImpl::ready() {
 }
 
-
 void led_driverImpl::handle_set_led_state(types::led_state::LedState& led_state, int& brightness) {
-    EVLOG_info << "Setting LED for connector 2 to R:" << led_state.red << " G:" << led_state.green << " B: " << led_state.blue;
+    EVLOG_info << "Setting LED for connector 2 to R:" << led_state.red << " G:" << led_state.green
+               << " B: " << led_state.blue;
     const auto message = LedStateMessage{led_state.red, led_state.green, led_state.blue, brightness};
     mod->serial.set_led_state(2, message);
 }
