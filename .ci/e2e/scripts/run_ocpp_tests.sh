96b770eb63b67c1c42ec1f129280f225e39f3f5c
Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
2024-11-11 19:48:10 +0100

    Add missing run_ocpp_tests script
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>

---
+++ .ci/e2e/scripts/run_ocpp_tests.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd tests
+pytest \
+  -rA \
+  --junitxml="$EXT_MOUNT/result.xml" \
+  --html="$EXT_MOUNT/report.html" \
+  --self-contained-html \
+  ocpp_tests/test_sets/*.py \
+  --everest-prefix "$EXT_MOUNT/dist"
+retVal=$?
+
+if [ $retVal -ne 0 ]; then
+    echo "OCPP tests failed with return code $retVal"
+    exit $retVal
+fi
