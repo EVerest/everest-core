e786c5761910aa30cd2ba1e2c15fc6f087307aa3
Maaike Zijderveld <maaikez@users.noreply.github.com>
2024-11-14 15:07:48 +0100

    Add composed device model storage and everest device model storage. (#846)
    * Add composed device model storage and everest device model storage.
    * Distinguish between ocpp and external variables. 
    * Rename device model interface to device model storage interface.
    
    
    Signed-off-by: Maaike Zijderveld, iolar <git.mail@iolar.nl>

--- modules/OCPP201/OCPP201.cpp
+++ modules/OCPP201/OCPP201.cpp
@@ -8,6 +8,7 @@
 #include <websocketpp_utils/uri.hpp>
 
 #include <conversions.hpp>
+#include <device_model/composed_device_model_storage.hpp>
 #include <error_handling.hpp>
 #include <evse_security_ocpp.hpp>
 #include <external_energy_limits.hpp>
@@ -691,10 +692,13 @@ void OCPP201::ready() {
     const auto sql_init_path = this->ocpp_share_path / SQL_CORE_MIGRATIONS;
 
     std::map<int32_t, int32_t> evse_connector_structure = this->get_connector_structure();
+    std::unique_ptr<module::device_model::ComposedDeviceModelStorage> device_model_storage =
+        std::make_unique<module::device_model::ComposedDeviceModelStorage>(
+            device_model_database_path, true, device_model_database_migration_path, device_model_config_path);
     this->charge_point = std::make_unique<ocpp::v201::ChargePoint>(
-        evse_connector_structure, device_model_database_path, true, device_model_database_migration_path,
-        device_model_config_path, this->ocpp_share_path.string(), this->config.CoreDatabasePath, sql_init_path.string(),
-        this->config.MessageLogPath, std::make_shared<EvseSecurity>(*this->r_security), callbacks);
+        evse_connector_structure, std::move(device_model_storage), this->ocpp_share_path.string(),
+        this->config.CoreDatabasePath, sql_init_path.string(), this->config.MessageLogPath,
+        std::make_shared<EvseSecurity>(*this->r_security), callbacks);
 
     // publish charging schedules at least once on startup
     charging_schedules_callback();
