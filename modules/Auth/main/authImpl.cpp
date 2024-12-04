bc053f94250aa7a95d91a29aafb38bb187c217a6
Maaike Zijderveld, iolar <git.mail@iolar.nl>
2024-12-04 15:20:40 +0100

    Change interfaces and add types to be able to withdraw authorization.
    Signed-off-by: Maaike Zijderveld, iolar <git.mail@iolar.nl>

--- modules/Auth/main/authImpl.cpp
+++ modules/Auth/main/authImpl.cpp
@@ -23,5 +23,9 @@ void authImpl::handle_set_master_pass_group_id(std::string& master_pass_group_id
     this->mod->set_master_pass_group_id(master_pass_group_id);
 }
 
+std::string authImpl::handle_withdraw_authorization(WithdrawAuthorizationRequest& request) {
+    // TODO mz
+}
+
 } // namespace main
 } // namespace module
