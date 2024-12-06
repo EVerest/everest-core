1bbd963ec2ced8e997c554121c0810ea548be2c6
florinmihut <florinmihut1@gmail.com>
2024-12-06 13:55:15 +0100

    Fix multilib - let CMAKE figure out the location of the libs folder (#19)
    * Fix multilib - let CMAKE figure out the location of the libs folder
    * Update bundling.cmake
    
    Signed-off-by: florinmihut <florinmihut1@gmail.com>
    
    ---------
    
    Signed-off-by: florinmihut <florinmihut1@gmail.com>

--- bundling.cmake
+++ bundling.cmake
@@ -4,10 +4,6 @@ if(CMAKE_INSTALL_PREFIX_INITIALIZED_TO_DEFAULT)
     set(CMAKE_INSTALL_PREFIX "${CMAKE_BINARY_DIR}/dist" CACHE PATH "..." FORCE)
 endif()
 
-# FIXME (aw): our packaging does not play well with lib64 and these kind of
-#             variations.  Either we need to change our directory layout or ...?
-set(CMAKE_INSTALL_LIBDIR lib)
-
 # set RPATH to the planned install path of lib folder
 set(CMAKE_INSTALL_RPATH "${CMAKE_INSTALL_PREFIX}/${CMAKE_INSTALL_LIBDIR}")
 
