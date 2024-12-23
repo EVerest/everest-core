4d7df380549e8cacc8ae670ae0cf87592e348312
Piet Gömpel <pietgoempel@gmail.com>
2024-12-23 11:03:47 +0100

    clang format
    Signed-off-by: Piet Gömpel <pietgoempel@gmail.com>

--- modules/OCPP201/OCPP201.cpp
+++ modules/OCPP201/OCPP201.cpp
@@ -699,7 +699,8 @@ void OCPP201::ready() {
             reservation.parent_id_token = request.groupIdToken.value().idToken;
         }
         if (request.connectorType.has_value()) {
-            reservation.connector_type = types::evse_manager::string_to_connector_type_enum(request.connectorType.value());
+            reservation.connector_type =
+                types::evse_manager::string_to_connector_type_enum(request.connectorType.value());
         }
 
         types::reservation::ReservationResult result = this->r_reservation.at(0)->call_reserve_now(reservation);
