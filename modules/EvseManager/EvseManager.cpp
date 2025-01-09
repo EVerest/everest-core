d3f86258f390bcc94ff8ead85048bf3d7b8ec2f6
florinmihut <florinmihut1@gmail.com>
2025-01-09 14:27:03 +0100

    Adding changes as requested during code review
    Signed-off-by: florinmihut <florinmihut1@gmail.com>

--- modules/EvseManager/EvseManager.cpp
+++ modules/EvseManager/EvseManager.cpp
@@ -893,7 +893,7 @@ void EvseManager::ready() {
                        config.ac_hlc_use_5percent, config.ac_enforce_hlc, false,
                        config.soft_over_current_tolerance_percent, config.soft_over_current_measurement_noise_A,
                        config.switch_3ph1ph_delay_s, config.switch_3ph1ph_cp_state, config.soft_over_current_timeout_ms,
-                       config.state_F_after_fault_ms);
+                       config.state_F_after_fault_ms, config.fail_on_powermeter_errors);
     }
 
     telemetryThreadHandle = std::thread([this]() {
@@ -1079,7 +1079,8 @@ void EvseManager::setup_fake_DC_mode() {
     charger->setup(config.has_ventilation, Charger::ChargeMode::DC, hlc_enabled, config.ac_hlc_use_5percent,
                    config.ac_enforce_hlc, false, config.soft_over_current_tolerance_percent,
                    config.soft_over_current_measurement_noise_A, config.switch_3ph1ph_delay_s,
-                   config.switch_3ph1ph_cp_state, config.soft_over_current_timeout_ms, config.state_F_after_fault_ms);
+                   config.switch_3ph1ph_cp_state, config.soft_over_current_timeout_ms, config.state_F_after_fault_ms,
+                   config.fail_on_powermeter_errors);
 
     types::iso15118_charger::EVSEID evseid = {config.evse_id, config.evse_id_din};
 
@@ -1118,7 +1119,8 @@ void EvseManager::setup_AC_mode() {
     charger->setup(config.has_ventilation, Charger::ChargeMode::AC, hlc_enabled, config.ac_hlc_use_5percent,
                    config.ac_enforce_hlc, true, config.soft_over_current_tolerance_percent,
                    config.soft_over_current_measurement_noise_A, config.switch_3ph1ph_delay_s,
-                   config.switch_3ph1ph_cp_state, config.soft_over_current_timeout_ms, config.state_F_after_fault_ms);
+                   config.switch_3ph1ph_cp_state, config.soft_over_current_timeout_ms, config.state_F_after_fault_ms,
+                   config.fail_on_powermeter_errors);
 
     types::iso15118_charger::EVSEID evseid = {config.evse_id, config.evse_id_din};
 
