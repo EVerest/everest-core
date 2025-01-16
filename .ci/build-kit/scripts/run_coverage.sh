458242b7e8e0685e9a72895c8990c56d18eaf85c
Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
2025-01-16 18:01:27 +0100

    Create an empty coverage xml file to make the pipeline work
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>

--- .ci/build-kit/scripts/run_coverage.sh
+++ .ci/build-kit/scripts/run_coverage.sh
@@ -7,6 +7,7 @@ retValHTML=$?
 
 # Copy the generated coverage report to the mounted directory in any case
 cp -R "$EXT_MOUNT/build/everest-core_gcovr_coverage" "$EXT_MOUNT/gcovr-coverage"
+touch "$EXT_MOUNT/gcovr-coverage-xml.xml"
 
 if [ $retValHTML -ne 0 ]; then
     echo "Coverage HTML report failed with return code $retValHTML"
