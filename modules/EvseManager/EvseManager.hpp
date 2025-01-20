f4b93c9ed534711af08a3be818c8feaddbcbe203
florinmihut <florinmihut1@gmail.com>
2025-01-20 17:57:50 +0100

    Introduce EVSE Manager configuration option for failing charging if t… (#993)
    * Introduce EVSE Manager configuration option for failing charging if the powermeter has errors
    * Add behavior in the error handling document
    * Update modules/EvseManager/manifest.yaml
    * Adding changes as requested during code review
    
    Signed-off-by: florinmihut <florinmihut1@gmail.com>
    Co-authored-by: Piet Gömpel <37657534+Pietfried@users.noreply.github.com>
    Signed-off-by: Abby Wheelis <Abigail.Wheelis@nrel.gov>

--- modules/EvseManager/EvseManager.hpp
+++ modules/EvseManager/EvseManager.hpp
@@ -102,6 +102,7 @@ struct Conf {
     int soft_over_current_timeout_ms;
     bool lock_connector_in_state_b;
     int state_F_after_fault_ms;
+    bool fail_on_powermeter_errors;
 };
 
 class EvseManager : public Everest::ModuleBase {
@@ -136,8 +137,7 @@ public:
         r_imd(std::move(r_imd)),
         r_powersupply_DC(std::move(r_powersupply_DC)),
         r_store(std::move(r_store)),
-        config(config) {
-    }
+        config(config){};
 
     Everest::MqttProvider& mqtt;
     Everest::TelemetryProvider& telemetry;
@@ -194,7 +194,7 @@ public:
 
     const std::vector<std::unique_ptr<powermeterIntf>>& r_powermeter_billing();
 
-    // FIXME: this will be removed with proper intergration of BPT on ISO-20
+    // FIXME: this will be removed with proper integration of BPT on ISO-20
     // on DIN SPEC and -2 we claim a positive charging current on ISO protocol,
     // but the power supply switches to discharge if this flag is set.
     std::atomic_bool is_actually_exporting_to_grid{false};
