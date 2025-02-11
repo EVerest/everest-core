3bbc69c9a7a38947a940a0b4d3d99ea430ebf463
AssemblyJohn <ioan.bogdann@gmail.com>
2025-02-11 16:22:11 +0200

    Adding ISO15118-20 AC support: - Add required configurations for AC20 - Added run-sil for AC - Update config for AC charging - Workaround for setup when using AC charging - Added callback for AC charging - Prepare for EvseManager integration of AC - Prepared for AC for ChargingNeeds - Add param filling for v2x charging parameters - Adding new d20 ac cmds to iso15118_charger interface - Fixing generating power_meter_external in YetiSimulator - Adding PyEvJosev config option supported_d20_energy_services to select d20 energy services via config - Adding loops to run_telemetry threads in YetiSimulator, updating dependencies.yaml - adding selected_service_parameters vars to iso15118_charger interface
    ISO15118-20 AC discharge is working now:
    - wip: fix overcurrent not respecting direction of flow
    - wip: un-hardcode yeti simulator
    - wip: add bpt sil nodered flow
    - wip: fix selected_d20_energy_service not set in AC modes
    - wip: fix temperature calculation when discharging
    - wip: allow ac discharge only with ISO-20 AC_BPT
    - wip: fix missing AC_BPT offer
    - wip: evse manager add config option for AC BPT
    - Preparing SIL for d20 ac discharge
    
    Updating export and import limits for discharging in local energy limit. Removing deprecated nodered set limit functions
    Renamed ev_three_phases to ev_phases, removed a comment, clang-format
    Adding vars to iso15118_charger interface: ac_ev_power_limits, ac_ev_present_powers & ac_ev_dynamic_control_mode
    Additional parameters for the services (AC, AC_BPT, DC_BPT & MCS_BPT) can now be configured for ISO15118-20 via EvseManager and config: AcParameter and BPT Parameter
    Adding missing ev_energy_service
    
    Signed-off-by: AssemblyJohn <ioan.bogdann@gmail.com>
    Signed-off-by: Sebastian Lukas <sebastian.lukas@pionix.de>
    Signed-off-by: Mark Oude Elberink <mark.oude.elberink@frickly.systems>
    Co-authored-by: Sebastian Lukas <sebastian.lukas@pionix.de>
    Co-authored-by: Mark Oude Elberink <mark.oude.elberink@frickly.systems>

--- modules/EVSE/IsoMux/charger/ISO15118_chargerImpl.cpp
+++ modules/EVSE/IsoMux/charger/ISO15118_chargerImpl.cpp
@@ -249,6 +249,24 @@ void ISO15118_chargerImpl::init() {
         }
     });
 
+    mod->r_iso20->subscribe_ac_ev_power_limits([this](const auto o) {
+        if (mod->selected_iso20()) {
+            publish_ac_ev_power_limits(o);
+        }
+    });
+
+    mod->r_iso20->subscribe_ac_ev_present_powers([this](const auto o) {
+        if (mod->selected_iso20()) {
+            publish_ac_ev_present_powers(o);
+        }
+    });
+
+    mod->r_iso20->subscribe_ac_ev_dynamic_control_mode([this](const auto o) {
+        if (mod->selected_iso20()) {
+            publish_ac_ev_dynamic_control_mode(o);
+        }
+    });
+
     mod->r_iso2->subscribe_dc_ev_energy_capacity([this](const auto o) {
         if (not mod->selected_iso20()) {
             publish_dc_ev_energy_capacity(o);
@@ -490,6 +508,12 @@ void ISO15118_chargerImpl::handle_session_setup(std::vector<types::iso15118::Pay
                                     central_contract_validation_allowed);
 }
 
+void ISO15118_chargerImpl::handle_bpt_setup(types::iso15118::BptSetup& bpt_config) {
+    if (mod->selected_iso20()) {
+        mod->r_iso20->call_bpt_setup(bpt_config);
+    }
+}
+
 void ISO15118_chargerImpl::handle_authorization_response(
     types::authorization::AuthorizationStatus& authorization_status,
     types::authorization::CertificateStatus& certificate_status) {
@@ -562,6 +586,36 @@ void ISO15118_chargerImpl::handle_update_energy_transfer_modes(
 void ISO15118_chargerImpl::handle_update_ac_max_current(double& max_current) {
     mod->r_iso20->call_update_ac_max_current(max_current);
     mod->r_iso2->call_update_ac_max_current(max_current);
+}
+
+void ISO15118_chargerImpl::handle_update_ac_parameters(types::iso15118::AcParameters& ac_parameters) {
+    if (mod->selected_iso20()) {
+        mod->r_iso20->call_update_ac_parameters(ac_parameters);
+    }
+}
+
+void ISO15118_chargerImpl::handle_update_ac_maximum_limits(types::iso15118::AcEvseMaximumPower& maximum_limits) {
+    if (mod->selected_iso20()) {
+        mod->r_iso20->call_update_ac_maximum_limits(maximum_limits);
+    }
+}
+
+void ISO15118_chargerImpl::handle_update_ac_minimum_limits(types::iso15118::AcEvseMinimumPower& minimum_limits) {
+    if (mod->selected_iso20()) {
+        mod->r_iso20->call_update_ac_minimum_limits(minimum_limits);
+    }
+}
+
+void ISO15118_chargerImpl::handle_update_ac_target_values(types::iso15118::AcTargetValues& target_values) {
+    if (mod->selected_iso20()) {
+        mod->r_iso20->call_update_ac_target_values(target_values);
+    }
+}
+
+void ISO15118_chargerImpl::handle_update_ac_present_power(types::units::Power& present_power) {
+    if (mod->selected_iso20()) {
+        mod->r_iso20->call_update_ac_present_power(present_power);
+    }
 }
 
 void ISO15118_chargerImpl::handle_update_dc_maximum_limits(types::iso15118::DcEvseMaximumLimits& maximum_limits) {
