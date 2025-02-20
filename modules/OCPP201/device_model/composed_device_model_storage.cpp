7013486eb929cbc06023d1c4107ba227c9ba79ff
Piet Gömpel <pietgoempel@gmail.com>
2025-02-20 12:09:01 +0100

    Renamed all occuranced of v201 to v2 and V201 to V2 in modules/ and lib/ directories. This change is required because of the changed names in libocpp
    Signed-off-by: Piet Gömpel <pietgoempel@gmail.com>

--- modules/OCPP201/device_model/composed_device_model_storage.cpp
+++ modules/OCPP201/device_model/composed_device_model_storage.cpp
@@ -11,23 +11,23 @@ ComposedDeviceModelStorage::ComposedDeviceModelStorage(const std::string& libocp
                                                        const std::string& device_model_migration_path,
                                                        const std::string& device_model_config_path) :
     everest_device_model_storage(std::make_unique<EverestDeviceModelStorage>()),
-    libocpp_device_model_storage(std::make_unique<ocpp::v201::DeviceModelStorageSqlite>(
+    libocpp_device_model_storage(std::make_unique<ocpp::v2::DeviceModelStorageSqlite>(
         libocpp_device_model_storage_address, device_model_migration_path, device_model_config_path,
         libocpp_initialize_device_model)),
     device_model_map(get_device_model()) {
 }
 
-ocpp::v201::DeviceModelMap ComposedDeviceModelStorage::get_device_model() {
-    ocpp::v201::DeviceModelMap everest_dm = everest_device_model_storage->get_device_model();
-    ocpp::v201::DeviceModelMap libocpp_dm = libocpp_device_model_storage->get_device_model();
+ocpp::v2::DeviceModelMap ComposedDeviceModelStorage::get_device_model() {
+    ocpp::v2::DeviceModelMap everest_dm = everest_device_model_storage->get_device_model();
+    ocpp::v2::DeviceModelMap libocpp_dm = libocpp_device_model_storage->get_device_model();
     everest_dm.merge(libocpp_dm);
     return everest_dm;
 }
 
-std::optional<ocpp::v201::VariableAttribute>
-ComposedDeviceModelStorage::get_variable_attribute(const ocpp::v201::Component& component_id,
-                                                   const ocpp::v201::Variable& variable_id,
-                                                   const ocpp::v201::AttributeEnum& attribute_enum) {
+std::optional<ocpp::v2::VariableAttribute>
+ComposedDeviceModelStorage::get_variable_attribute(const ocpp::v2::Component& component_id,
+                                                   const ocpp::v2::Variable& variable_id,
+                                                   const ocpp::v2::AttributeEnum& attribute_enum) {
     if (get_variable_source(component_id, variable_id) == VARIABLE_SOURCE_OCPP) {
         return libocpp_device_model_storage->get_variable_attribute(component_id, variable_id, attribute_enum);
     }
@@ -35,10 +35,10 @@ ComposedDeviceModelStorage::get_variable_attribute(const ocpp::v201::Component& 
     return everest_device_model_storage->get_variable_attribute(component_id, variable_id, attribute_enum);
 }
 
-std::vector<ocpp::v201::VariableAttribute>
-ComposedDeviceModelStorage::get_variable_attributes(const ocpp::v201::Component& component_id,
-                                                    const ocpp::v201::Variable& variable_id,
-                                                    const std::optional<ocpp::v201::AttributeEnum>& attribute_enum) {
+std::vector<ocpp::v2::VariableAttribute>
+ComposedDeviceModelStorage::get_variable_attributes(const ocpp::v2::Component& component_id,
+                                                    const ocpp::v2::Variable& variable_id,
+                                                    const std::optional<ocpp::v2::AttributeEnum>& attribute_enum) {
     if (get_variable_source(component_id, variable_id) == VARIABLE_SOURCE_OCPP) {
         return libocpp_device_model_storage->get_variable_attributes(component_id, variable_id, attribute_enum);
     }
@@ -46,9 +46,9 @@ ComposedDeviceModelStorage::get_variable_attributes(const ocpp::v201::Component&
     return everest_device_model_storage->get_variable_attributes(component_id, variable_id, attribute_enum);
 }
 
-bool ComposedDeviceModelStorage::set_variable_attribute_value(const ocpp::v201::Component& component_id,
-                                                              const ocpp::v201::Variable& variable_id,
-                                                              const ocpp::v201::AttributeEnum& attribute_enum,
+bool ComposedDeviceModelStorage::set_variable_attribute_value(const ocpp::v2::Component& component_id,
+                                                              const ocpp::v2::Variable& variable_id,
+                                                              const ocpp::v2::AttributeEnum& attribute_enum,
                                                               const std::string& value, const std::string& source) {
     if (get_variable_source(component_id, variable_id) == VARIABLE_SOURCE_OCPP) {
         return libocpp_device_model_storage->set_variable_attribute_value(component_id, variable_id, attribute_enum,
@@ -58,9 +58,9 @@ bool ComposedDeviceModelStorage::set_variable_attribute_value(const ocpp::v201::
                                                                       source);
 }
 
-std::optional<ocpp::v201::VariableMonitoringMeta>
-ComposedDeviceModelStorage::set_monitoring_data(const ocpp::v201::SetMonitoringData& data,
-                                                const ocpp::v201::VariableMonitorType type) {
+std::optional<ocpp::v2::VariableMonitoringMeta>
+ComposedDeviceModelStorage::set_monitoring_data(const ocpp::v2::SetMonitoringData& data,
+                                                const ocpp::v2::VariableMonitorType type) {
     return libocpp_device_model_storage->set_monitoring_data(data, type);
 }
 
@@ -69,10 +69,10 @@ bool ComposedDeviceModelStorage::update_monitoring_reference(const int32_t monit
     return libocpp_device_model_storage->update_monitoring_reference(monitor_id, reference_value);
 }
 
-std::vector<ocpp::v201::VariableMonitoringMeta>
-ComposedDeviceModelStorage::get_monitoring_data(const std::vector<ocpp::v201::MonitoringCriterionEnum>& criteria,
-                                                const ocpp::v201::Component& component_id,
-                                                const ocpp::v201::Variable& variable_id) {
+std::vector<ocpp::v2::VariableMonitoringMeta>
+ComposedDeviceModelStorage::get_monitoring_data(const std::vector<ocpp::v2::MonitoringCriterionEnum>& criteria,
+                                                const ocpp::v2::Component& component_id,
+                                                const ocpp::v2::Variable& variable_id) {
     if (get_variable_source(component_id, variable_id) == VARIABLE_SOURCE_OCPP) {
         return libocpp_device_model_storage->get_monitoring_data(criteria, component_id, variable_id);
     }
@@ -80,8 +80,8 @@ ComposedDeviceModelStorage::get_monitoring_data(const std::vector<ocpp::v201::Mo
     return everest_device_model_storage->get_monitoring_data(criteria, component_id, variable_id);
 }
 
-ocpp::v201::ClearMonitoringStatusEnum ComposedDeviceModelStorage::clear_variable_monitor(int monitor_id,
-                                                                                         bool allow_protected) {
+ocpp::v2::ClearMonitoringStatusEnum ComposedDeviceModelStorage::clear_variable_monitor(int monitor_id,
+                                                                                       bool allow_protected) {
     return libocpp_device_model_storage->clear_variable_monitor(monitor_id, allow_protected);
 }
 
@@ -94,14 +94,13 @@ void ComposedDeviceModelStorage::check_integrity() {
     libocpp_device_model_storage->check_integrity();
 }
 
-std::string
-module::device_model::ComposedDeviceModelStorage::get_variable_source(const ocpp::v201::Component& component,
-                                                                      const ocpp::v201::Variable& variable) {
+std::string module::device_model::ComposedDeviceModelStorage::get_variable_source(const ocpp::v2::Component& component,
+                                                                                  const ocpp::v2::Variable& variable) {
     std::optional<std::string> variable_source = device_model_map[component][variable].source;
     if (variable_source.has_value() && variable_source.value() != VARIABLE_SOURCE_OCPP) {
         // For now, this just throws because we only have the libocpp source. When the config service is
         // implemented, this should not throw.
-        throw ocpp::v201::DeviceModelError("Source is not 'OCPP', not sure what to do");
+        throw ocpp::v2::DeviceModelError("Source is not 'OCPP', not sure what to do");
     }
 
     return VARIABLE_SOURCE_OCPP;
--- modules/OCPP201/device_model/everest_device_model_storage.hpp
+++ modules/OCPP201/device_model/everest_device_model_storage.hpp
@@ -3,30 +3,30 @@
 
 #pragma once
 
-#include <ocpp/v201/device_model_storage_interface.hpp>
+#include <ocpp/v2/device_model_storage_interface.hpp>
 
 namespace module::device_model {
-class EverestDeviceModelStorage : public ocpp::v201::DeviceModelStorageInterface {
+class EverestDeviceModelStorage : public ocpp::v2::DeviceModelStorageInterface {
 public:
     virtual ~EverestDeviceModelStorage() override = default;
-    virtual ocpp::v201::DeviceModelMap get_device_model() override;
-    virtual std::optional<ocpp::v201::VariableAttribute>
-    get_variable_attribute(const ocpp::v201::Component& component_id, const ocpp::v201::Variable& variable_id,
-                           const ocpp::v201::AttributeEnum& attribute_enum) override;
-    virtual std::vector<ocpp::v201::VariableAttribute>
-    get_variable_attributes(const ocpp::v201::Component& component_id, const ocpp::v201::Variable& variable_id,
-                            const std::optional<ocpp::v201::AttributeEnum>& attribute_enum) override;
-    virtual bool set_variable_attribute_value(const ocpp::v201::Component& component_id,
-                                              const ocpp::v201::Variable& variable_id,
-                                              const ocpp::v201::AttributeEnum& attribute_enum, const std::string& value,
+    virtual ocpp::v2::DeviceModelMap get_device_model() override;
+    virtual std::optional<ocpp::v2::VariableAttribute>
+    get_variable_attribute(const ocpp::v2::Component& component_id, const ocpp::v2::Variable& variable_id,
+                           const ocpp::v2::AttributeEnum& attribute_enum) override;
+    virtual std::vector<ocpp::v2::VariableAttribute>
+    get_variable_attributes(const ocpp::v2::Component& component_id, const ocpp::v2::Variable& variable_id,
+                            const std::optional<ocpp::v2::AttributeEnum>& attribute_enum) override;
+    virtual bool set_variable_attribute_value(const ocpp::v2::Component& component_id,
+                                              const ocpp::v2::Variable& variable_id,
+                                              const ocpp::v2::AttributeEnum& attribute_enum, const std::string& value,
                                               const std::string& source) override;
-    virtual std::optional<ocpp::v201::VariableMonitoringMeta>
-    set_monitoring_data(const ocpp::v201::SetMonitoringData& data, const ocpp::v201::VariableMonitorType type) override;
+    virtual std::optional<ocpp::v2::VariableMonitoringMeta>
+    set_monitoring_data(const ocpp::v2::SetMonitoringData& data, const ocpp::v2::VariableMonitorType type) override;
     virtual bool update_monitoring_reference(const int32_t monitor_id, const std::string& reference_value) override;
-    virtual std::vector<ocpp::v201::VariableMonitoringMeta>
-    get_monitoring_data(const std::vector<ocpp::v201::MonitoringCriterionEnum>& criteria,
-                        const ocpp::v201::Component& component_id, const ocpp::v201::Variable& variable_id) override;
-    virtual ocpp::v201::ClearMonitoringStatusEnum clear_variable_monitor(int monitor_id, bool allow_protected) override;
+    virtual std::vector<ocpp::v2::VariableMonitoringMeta>
+    get_monitoring_data(const std::vector<ocpp::v2::MonitoringCriterionEnum>& criteria,
+                        const ocpp::v2::Component& component_id, const ocpp::v2::Variable& variable_id) override;
+    virtual ocpp::v2::ClearMonitoringStatusEnum clear_variable_monitor(int monitor_id, bool allow_protected) override;
     virtual int32_t clear_custom_variable_monitors() override;
     virtual void check_integrity() override;
 };
