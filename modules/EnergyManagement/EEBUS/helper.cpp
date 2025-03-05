d235c2577286d87f20f35deed8d60f36a75579fd
Andreas Heinrich <andreas.heinrich@rwth-aachen.de>
2025-03-05 19:06:17 +0100

    feat(EEBus): Implement EEBus LPC Use Case
    This is the first implementation of EEBus and the LPC use case. The
    EEBUS module requires an energy sink to limit the energy flow request
    going to the energy manager.
    
    Signed-off-by: Andreas Heinrich <andreas.heinrich@rwth-aachen.de>
    Signed-off-by: Martin Litre <mnlitre@gmail.com>

---
+++ modules/EnergyManagement/EEBUS/helper.cpp
@@ -0,0 +1,57 @@
+#include "helper.hpp"
+
+namespace module {
+
+bool compare_use_case(const control_service::UseCase& uc1, const control_service::UseCase& uc2) {
+    if (uc1.actor() != uc2.actor()) {
+        return false;
+    }
+    if (uc1.name() != uc2.name()) {
+        return false;
+    }
+    return true;
+}
+
+types::energy::ScheduleReqEntry create_active_schedule_req_entry(std::chrono::time_point<date::utc_clock> timestamp,
+                                                                 double total_power_W) {
+    types::energy::ScheduleReqEntry schedule_req_entry;
+    types::energy::LimitsReq limits_req;
+    schedule_req_entry.timestamp = Everest::Date::to_rfc3339(timestamp);
+    types::energy::NumberWithSource total_power;
+    total_power.value = total_power_W;
+    total_power.source = "EEBUS LPC";
+    limits_req.total_power_W = total_power;
+    schedule_req_entry.limits_to_leaves = limits_req;
+    schedule_req_entry.limits_to_root = limits_req;
+    return schedule_req_entry;
+}
+
+types::energy::ScheduleReqEntry create_inactive_schedule_req_entry(std::chrono::time_point<date::utc_clock> timestamp) {
+    types::energy::ScheduleReqEntry schedule_req_entry;
+    schedule_req_entry.timestamp = Everest::Date::to_rfc3339(timestamp);
+    schedule_req_entry.limits_to_leaves = types::energy::LimitsReq();
+    schedule_req_entry.limits_to_root = types::energy::LimitsReq();
+    return schedule_req_entry;
+}
+
+types::energy::ExternalLimits translate_to_external_limits(const common_types::LoadLimit& load_limit) {
+    types::energy::ExternalLimits limits;
+    std::vector<types::energy::ScheduleReqEntry> schedule_import;
+    if (load_limit.is_active()) {
+        schedule_import.push_back(create_active_schedule_req_entry(
+            date::utc_clock::from_sys(std::chrono::system_clock::now()), load_limit.value()));
+        if (load_limit.duration_nanoseconds() > 0) {
+            std::chrono::time_point<date::utc_clock> timestamp;
+            timestamp = date::utc_clock::from_sys(std::chrono::system_clock::now());
+            timestamp += std::chrono::nanoseconds(load_limit.duration_nanoseconds());
+            schedule_import.push_back(create_inactive_schedule_req_entry(timestamp));
+        }
+    } else {
+        schedule_import.push_back(
+            create_inactive_schedule_req_entry(date::utc_clock::from_sys(std::chrono::system_clock::now())));
+    }
+    limits.schedule_import = schedule_import;
+    return limits;
+}
+
+} // namespace module
