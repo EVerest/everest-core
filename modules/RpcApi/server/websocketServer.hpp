4552531c1a87e5b749d267c098d6b5ad82524d78
Fabian Hartung <fabian.hartung@chargebyte.com>
2025-03-24 15:20:42 +0100

    RpcApi: Refactor WebSocketServer to use UUIDs for client identification
    Signed-off-by: Fabian Hartung <fabian.hartung@chargebyte.com>

--- modules/RpcApi/server/websocketServer.hpp
+++ modules/RpcApi/server/websocketServer.hpp
@@ -15,6 +15,18 @@
 
 #include "transportInterface.hpp"
 
+namespace std {
+    template <>
+    struct hash<boost::uuids::uuid> {
+        std::size_t operator()(const boost::uuids::uuid& u) const {
+            // Combine the two 64-bit parts of the UUID into one 128-bit integer
+            uint64_t part1 = *reinterpret_cast<const uint64_t*>(&u.data[0]);
+            uint64_t part2 = *reinterpret_cast<const uint64_t*>(&u.data[8]);
+            return std::hash<uint64_t>()(part1) ^ (std::hash<uint64_t>()(part2) << 1); // Combine the two parts
+        }
+    };
+}
+
 namespace server {
 
 class WebSocketServer : public TransportInterface
@@ -27,7 +39,7 @@ public:
     // Methods
     bool running() const override;
     void send_data(const ClientId &client_id, const Data &data) override;
-    void kill_client_connection(const int &client_id, const std::string &kill_reason) override;
+    void kill_client_connection(const ClientId &client_id, const std::string &kill_reason) override;
 
     uint connections_count() const override;
 
@@ -35,26 +47,22 @@ public:
     bool stop_server() override;
 
     void on_client_connected();
-    void on_client_disconnected(const int &client_id);
-    void on_data_received(const int &client_id, const std::vector<uint8_t> &data);
+    void on_client_disconnected(const ClientId &client_id);
+    void on_data_received(const ClientId &client_id, const std::vector<uint8_t> &data);
 
 private:
     // Members
-    void *m_server = nullptr; 
     bool m_ssl_enabled;
     struct lws_context_creation_info m_info {};
     struct lws_protocols m_lws_protocols[2];
     std::atomic<bool> m_running {false};
     struct lws_context *m_context = nullptr;
     std::thread m_server_thread;
-    std::unordered_map<int, struct lws *> m_clients;  // Client-Mapping
+    std::unordered_map<ClientId, struct lws *> m_clients;  // Client-Mapping
     std::mutex m_clients_mutex;
 
     // Methods
     static int callback_ws(struct lws *wsi, enum lws_callback_reasons reason, void *user, void *in, size_t len);
-
-    
-
 };
 
 }
