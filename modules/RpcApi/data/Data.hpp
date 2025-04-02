9f4e11c831de305fe70fc961e6fb7f1eb2bca10d
Moritz Barsnick <moritz.barsnick@chargebyte.com>
2025-04-02 11:34:03 +0200

    RpcApi: derive data Store classes from a generic one
    Complete the basic structure to demonstrate inheritance.
    Return a std::optional to indicate availability of the requested values.
    
    Signed-off-by: Moritz Barsnick <moritz.barsnick@chargebyte.com>

--- modules/RpcApi/data/Data.hpp
+++ modules/RpcApi/data/Data.hpp
@@ -1,23 +1,42 @@
 // SPDX-License-Identifier: Apache-2.0
 // Copyright Pionix GmbH and Contributors to EVerest
 
-// This contains types for all the JSON objects
+#include <optional>
+
+// This contains types for all the data objects
 
 namespace data {
 
 class GenericInfoStore {
-    explicit GenericInfoStore() = default;
-    virtual ~GenericInfoStore() = default;
 public:
-    // Do we need a template for the getter?
-    // Does the getter get a generic name?
-    // Who converts to JSON?
+    explicit GenericInfoStore() {
+        this->init_data();
+    };
     // Do the Store classes own their to_json()/from_json()? They should!
-    json get_data() const;
-    void set_data(const json& in);
+
+    // override this to represent the collected data as json according to the JSON RPC API
+    // if the returned value has no value, the data is incomplete or not available
+    virtual std::optional<json> get_data() const = 0;
+    virtual void set_data(const json& in) = 0;
+protected:
+    // override this if structures need special (non-default) initialization
+    virtual void init_data() {};
+private:
+    // whether the non-optional values are valid, so that the RPC interface can generate an error
+    bool data_is_valid;
 };
 
-class ConnectorInfoStore : GenericInfoStore {};
+class ConnectorInfoStore : public GenericInfoStore {
+    std::optional<json> get_data() const {
+        return std::nullopt;
+    }
+    void set_data(const json& in) {
+        return;
+    }
+    void init_data() override {
+        // do something to explicitly initialize our data structures
+    }
+};
 class HardwareCapabilitiesStore {};
 class MeterDataStore {};
 class ACChargeParametersStore {};
