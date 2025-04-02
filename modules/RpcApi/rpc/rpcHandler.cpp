3e123080fc511b3dbf15b7ea1f395db0834b10a9
Fabian Hartung <fabian.hartung@chargebyte.com>
2025-04-02 14:51:43 +0200

    RpcApi: Implement client connection timeout handling
    Signed-off-by: Fabian Hartung <fabian.hartung@chargebyte.com>

--- modules/RpcApi/rpc/rpcHandler.cpp
+++ modules/RpcApi/rpc/rpcHandler.cpp
@@ -1,17 +1,22 @@
 #include "rpcHandler.hpp"
 
 #include <jsonrpccxx/server.hpp>
+#include <thread>
+
 
 namespace rpc {
 // RpcHandler just needs just a tranport interface array
-RpcHandler::RpcHandler(std::vector<std::shared_ptr<server::TransportInterface>> transportInterfaces)
-    : m_transport_interfaces(std::move(transportInterfaces)) {
+RpcHandler::RpcHandler(std::vector<std::shared_ptr<server::TransportInterface>> transport_interfaces)
+    : m_transport_interfaces(std::move(transport_interfaces)) {
     for (const auto& transport_interface : m_transport_interfaces) {
         if (!transport_interface) {
             throw std::runtime_error("Transport interface is null");
         }
-        transport_interface->on_client_connected = std::bind(&RpcHandler::client_connected, this,
-            std::placeholders::_1, std::placeholders::_2); 
+        transport_interface->on_client_connected = [this, transport_interface](
+            const server::TransportInterface::ClientId &client_id,
+            const server::TransportInterface::Address &address) {
+            this->client_connected(transport_interface, client_id, address);
+        };
 
         transport_interface->on_client_disconnected = std::bind(&RpcHandler::client_disconnected, this,
             std::placeholders::_1);
@@ -19,13 +24,29 @@ RpcHandler::RpcHandler(std::vector<std::shared_ptr<server::TransportInterface>> 
         transport_interface->on_data_available = std::bind(&RpcHandler::data_available, this,
             std::placeholders::_1, std::placeholders::_2);
     }
-
 }
 
-void RpcHandler::client_connected(const server::TransportInterface::ClientId &client_id,
-                         const server::TransportInterface::Address &address) {
-    // Handle client connected logic here
+void RpcHandler::client_connected(const std::shared_ptr<server::TransportInterface> &transport_interfaces, const server::TransportInterface::ClientId &client_id,
+                                  const server::TransportInterface::Address &address) {
+    // In case of a new client, we expect that the client will send a API.Hello request within 5 seconds.
+    // If not, we will close the connection.
+    // Start a timer for 5 seconds and wait for condition variable
+
+    std::thread wait_for_hello([this, client_id, transport_interfaces]() {
+        std::unique_lock<std::mutex> lock(m_mtx);
+        if (m_cv.wait_for(lock, std::chrono::seconds(5), [this, client_id] {
+            return m_client_hello_received.find(client_id) != m_client_hello_received.end();
+        })) {
+            // Client sent hello
+            m_client_hello_received.erase(client_id);
+        } else {
+            // Client did not send hello, close connection
+            transport_interfaces->kill_client_connection(client_id, "Disconnected due to timeout");
+        }
+    });
+    wait_for_hello.detach();
 }
+
 void RpcHandler::client_disconnected(const server::TransportInterface::ClientId &client_id) {
     // Handle client disconnected logic here
 }
