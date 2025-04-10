806e06ef4fca612b1eba46f692c2b2baff167e33
Kai Hermann <kai-uwe.hermann@pionix.de>
2025-04-10 18:57:03 +0200

    Change CI to reusable workflow (#253)
    * Change CI to reusable workflow
    
    ---------
    
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
    Signed-off-by: Andreas Heinrich <andreas.heinrich@rwth-aachen.de>
    Co-authored-by: Andreas Heinrich <andreas.heinrich@rwth-aachen.de>

--- .ci/build-kit/scripts/compile.sh
+++ .ci/build-kit/scripts/compile.sh
@@ -7,11 +7,24 @@ set -e
 # source "$HOME/.cargo/env"
 
 cmake \
-    -B build \
+    -B "$EXT_MOUNT/build" \
     -S "$EXT_MOUNT/source" \
     -G Ninja \
     -DEVC_ENABLE_CCACHE=1 \
     -DBUILD_TESTING=ON \
+    -DCMAKE_BUILD_TYPE=Debug \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
     -DEVEREST_ENABLE_RS_SUPPORT=OFF # Turn off rust support until we can properly support Rust.
 
-ninja -j$(nproc) -C build
+retVal=$?
+if [ $retVal -ne 0 ]; then
+    echo "Configuring failed with return code $retVal"
+    exit $retVal
+fi
+
+ninja -C "$EXT_MOUNT/build"
+retVal=$?
+if [ $retVal -ne 0 ]; then
+    echo "Compiling failed with return code $retVal"
+    exit $retVal
+fi
--- .ci/build-kit/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-
-ninja -j$(nproc) -C build test
