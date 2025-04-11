ae377b0b8c5f7fe8c2b6ed9c6c58621a54678eb6
Fabian Hartung <fabian.hartung@chargebyte.com>
2025-04-11 13:33:27 +0200

    RpcApi: implement support of API.Hello method (WIP)
    Signed-off-by: Fabian Hartung <fabian.hartung@chargebyte.com>

--- modules/RpcApi/rpc/RpcHandler.hpp
+++ modules/RpcApi/rpc/RpcHandler.hpp
@@ -15,14 +15,24 @@
 #include <unordered_map>
 #include <mutex>
 #include <condition_variable>
+#include <jsonrpccxx/server.hpp>
+#include <deque>
 
 #include "../server/TransportInterface.hpp"
 
 using namespace server;
+using namespace jsonrpccxx;
 
 namespace rpc {
 
 static const std::chrono::seconds CLIENT_HELLO_TIMEOUT(5);
+
+// struct to store json data, plus the tansport interface
+struct ClientData
+{
+    std::shared_ptr<server::TransportInterface> transport_interface;
+    std::deque<nlohmann::json> data;
+};
 
 class RpcHandler
 {
@@ -37,18 +47,25 @@ public:
     void stop_server();
 
     void client_connected(const std::shared_ptr<server::TransportInterface> &transport_interfaces, const TransportInterface::ClientId &client_id, const TransportInterface::Address &address);
-    void client_disconnected(const server::TransportInterface::ClientId &client_id);
-    void data_available(const TransportInterface::ClientId &client_id,
+    void client_disconnected(const std::shared_ptr<server::TransportInterface> &transport_interfaces, const server::TransportInterface::ClientId &client_id);
+    void data_available(const std::shared_ptr<server::TransportInterface> &transport_interfaces,
+                        const TransportInterface::ClientId &client_id,
                         const TransportInterface::Data &data);
 
     // Members
     std::vector<std::shared_ptr<TransportInterface>> m_transport_interfaces;
 
 private:
-    std::mutex m_mtx;
-    std::condition_variable m_cv;
-    std::unordered_map<TransportInterface::ClientId, bool> m_client_hello_received;
+    void init_rpc_api();
+    void init_transport_interfaces();
 
+    std::mutex m_mtx;
+    std::condition_variable m_cv_client_hello;
+    std::condition_variable m_cv_data_available;
+    std::unordered_map<TransportInterface::ClientId, bool> m_client_hello_received;
+    std::unique_ptr<JsonRpc2Server> m_rpc_server;
+    std::unordered_map<server::TransportInterface::ClientId, ClientData> messages;
+    std::chrono::steady_clock::time_point m_last_tick;
 };
 }
 
