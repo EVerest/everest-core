a3784b2be75d044ec6aded8f9f82ef28158f7360
Moritz Barsnick <moritz.barsnick@chargebyte.com>
2025-04-23 10:33:29 +0200

    RpcApi: implement meter values skeleton
    Signed-off-by: Moritz Barsnick <moritz.barsnick@chargebyte.com>

--- modules/RpcApi/RpcApi.cpp
+++ modules/RpcApi/RpcApi.cpp
@@ -10,8 +10,12 @@ void RpcApi::init() {
     invoke_init(*p_main);
 
     for (const auto& evse_manager : r_evse_manager) {
-        // create on DataStore object per EVSE
+        // create one DataStore object per EVSE
         this->data.evses.emplace_back().connectors.emplace_back();
+        data::DataStoreEvse& evse_data = this->data.evses.back();
+
+        // subscribe to interface variables
+        this->subscribe_evse_manager(evse_manager, evse_data);
     }
 
     m_websocket_server = std::make_unique<server::WebSocketServer>(config.websocket_tls_enabled, config.websocket_port);
@@ -26,6 +30,27 @@ void RpcApi::ready() {
     m_rpc_server->start_server();
 
     invoke_ready(*p_main);
+}
+
+void RpcApi::subscribe_evse_manager(const std::unique_ptr<evse_managerIntf>& evse_manager, data::DataStoreEvse& evse_data) {
+    evse_manager->subscribe_powermeter([this, &evse_data](const types::powermeter::Powermeter& powermeter) {
+        this->meter_interface_to_datastore(powermeter, evse_data.meterdata);
+    });
+
+}
+
+void RpcApi::meter_interface_to_datastore(const types::powermeter::Powermeter& powermeter, data::MeterDataStore& meter_data) {
+    types::json_rpc_api::MeterDataObj meter_data_new; // default initialized
+    if (meter_data.get_data().has_value()) {
+        meter_data_new == meter_data.get_data().value();
+    }
+    // FIXME: copy all interface meter values to our internal object
+    if (powermeter.energy_Wh_import.L1.has_value()) {
+        meter_data_new.energy_Wh_import.L1 = powermeter.energy_Wh_import.L1.value();
+    }
+
+    // submit changes
+    meter_data.set_data(meter_data_new);
 }
 
 } // namespace module
