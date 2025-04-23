a65b6f626b8318fd0b0987500a5805e42062c7ac
Andreas Heinrich <andreas.heinrich@rwth-aachen.de>
2025-04-23 18:07:01 +0200

    remove duplicated pytest configs
    Signed-off-by: Andreas Heinrich <andreas.heinrich@rwth-aachen.de>

--- tests/eebus_tests/conftest.py
+++
@@ -1,6 +0,0 @@
-# SPDX-License-Identifier: Apache-2.0
-# Copyright Pionix GmbH and Contributors to EVerest
-
-def pytest_addoption(parser):
-    parser.addoption("--everest-prefix", action="store", default="../build/dist",
-                     help="everest prefix path; default = '../build/dist'")
