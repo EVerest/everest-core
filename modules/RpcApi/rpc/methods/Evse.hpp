363e2570f2f921fbb4f91af426efe375833f4778
Moritz Barsnick <moritz.barsnick@chargebyte.com>
2025-04-29 15:05:23 +0200

    RpcApi: refactor EVSE methods
    Drop an always-true condition.
    Use a reference for brevity.
    Run clang-format.
    
    Signed-off-by: Moritz Barsnick <moritz.barsnick@chargebyte.com>

--- modules/RpcApi/rpc/methods/Evse.hpp
+++ modules/RpcApi/rpc/methods/Evse.hpp
@@ -4,8 +4,8 @@
 #ifndef EVSE_HPP
 #define EVSE_HPP
 
-#include <vector>
 #include <optional>
+#include <vector>
 
 #include "../../data/DataStore.hpp"
 
@@ -20,11 +20,10 @@ static const std::string METHOD_EVSE_GET_EVSE_INFO = "EVSE.GetEVSEInfos";
 /// This class includes all methods of the EVSE namespace.
 /// It contains the data object and the methods to access it.
 class Evse {
-    public:
+public:
     // Constructor and Destructor
     Evse() = delete;
-    Evse(DataStoreCharger &dataobj) : m_dataobj(dataobj) {
-    };
+    Evse(DataStoreCharger& dataobj) : m_dataobj(dataobj){};
 
     ~Evse() = default;
 
@@ -40,13 +39,14 @@ class Evse {
 
         // Iterate over all EVSEs and add the EVSEInfo objects to the response
         for (const auto& evse : m_dataobj.evses) {
-            if (!evse->evseinfo.get_data().has_value()) {
+            if (not evse->evseinfo.get_data().has_value()) {
                 res.error = RPCDataTypes::ResponseErrorEnum::ErrorNoDataAvailable;
                 return res;
             }
 
-            if (evse->evseinfo.get_data().has_value() && evse->evseinfo.get_data().value().id == evse_id) {
-                res.info = evse->evseinfo.get_data().value();
+            const auto& data = evse->evseinfo.get_data().value();
+            if (data.id == evse_id) {
+                res.info = data;
                 res.error = RPCDataTypes::ResponseErrorEnum::NoError;
                 return res;
             }
@@ -57,7 +57,7 @@ class Evse {
     };
 
 private:
-    DataStoreCharger &m_dataobj;
+    DataStoreCharger& m_dataobj;
 };
 
 } // namespace methods
