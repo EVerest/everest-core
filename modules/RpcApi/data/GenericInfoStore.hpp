0fbdde3142c6ca3c6e81d8553248017e3e332858
Moritz Barsnick <moritz.barsnick@chargebyte.com>
2025-04-29 14:52:00 +0200

    RpcApi: introduce data object locking
    Signed-off-by: Moritz Barsnick <moritz.barsnick@chargebyte.com>

--- modules/RpcApi/data/GenericInfoStore.hpp
+++ modules/RpcApi/data/GenericInfoStore.hpp
@@ -3,8 +3,10 @@
 #ifndef GENERICINFOSTORE_HPP
 #define GENERICINFOSTORE_HPP
 
+#include <atomic>
 #include <functional> // for std::function
 #include <generated/types/json_rpc_api.hpp>
+#include <mutex>
 #include <nlohmann/json.hpp>
 #include <optional>
 #include <vector>
@@ -19,13 +21,16 @@ template <typename T> class GenericInfoStore {
 protected:
     // the associated data store
     T dataobj;
+    // protect the data object
+    // NB: mutable in order to be able to lock the mutex in const functions
+    mutable std::mutex data_mutex;
     // function to call when changes occurred
     std::function<void()> notification_callback;
 
     // override this if structures need special (non-default) initialization
     virtual void init_data(){};
     // whether the non-optional values are valid, so that the RPC interface can generate an error
-    bool data_is_valid{false};
+    std::atomic<bool> data_is_valid{false};
 
 public:
     explicit GenericInfoStore() {
@@ -34,20 +39,26 @@ public:
     // if the returned value has no value, the data is incomplete or not available
     std::optional<T> get_data() const {
         if (this->data_is_valid) {
+            std::unique_lock<std::mutex> data_lock{this->data_mutex};
             return this->dataobj;
         } else {
             return std::nullopt;
         }
     };
     // set the data object. This method must be overridden if the data object is not a simple type
-    // e.g. pointers
+    //   e.g. pointers
+    // Note: all setters, also in derived classes, must use the data mutex
+    //   e.g. std::unique_lock<std::mutex> data_lock(this->data_mutex)
     void set_data(const T& in) {
         // check for changes
+        std::unique_lock<std::mutex> data_lock(this->data_mutex);
         if (in != this->dataobj) {
             this->dataobj = in;
             this->data_is_valid = true;
             // call the notification callback if it is set
             if (this->notification_callback) {
+                // FIXME - TO BE REVIEWED:
+                data_lock.unlock(); // unlock explicitly before entering callback
                 this->notification_callback();
             }
         }
