115e6ae3db13cfd0d693be7a23a345ae1db52535
Andreas Heinrich <andreas.heinrich@rwth-aachen.de>
2025-04-29 10:14:30 +0200

    use python3 -m
    Signed-off-by: Andreas Heinrich <andreas.heinrich@rwth-aachen.de>

--- .ci/e2e/scripts/run_integration_tests.sh
+++ .ci/e2e/scripts/run_integration_tests.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
 cd tests
-pytest --help
-echo "pytest version: $(pytest --version)"
-pytest \
+python3 -m pytest \
   -rA \
   --junitxml="$EXT_MOUNT/result.xml" \
   --html="$EXT_MOUNT/report.html" \
