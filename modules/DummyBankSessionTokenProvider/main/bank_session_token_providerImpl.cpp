20200c38cbd39cfffb84ddb67cb8429602f7b7aa
Christoph Burandt <christoph.burandt@pionix.de>
2025-05-02 18:25:52 +0200

    feat: Allow DummyBankSessionTokenProvider to optionally generate random IDs
    Signed-off-by: Christoph Burandt <christoph.burandt@pionix.de>

--- modules/DummyBankSessionTokenProvider/main/bank_session_token_providerImpl.cpp
+++ modules/DummyBankSessionTokenProvider/main/bank_session_token_providerImpl.cpp
@@ -3,6 +3,11 @@
 
 #include "bank_session_token_providerImpl.hpp"
 
+#include <boost/uuid/random_generator.hpp>
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_io.hpp>
+#include <string>
+
 namespace module {
 namespace main {
 
@@ -15,6 +20,10 @@ void bank_session_token_providerImpl::ready() {
 types::payment_terminal::BankSessionToken bank_session_token_providerImpl::handle_get_bank_session_token() {
     types::payment_terminal::BankSessionToken bank_session_token;
     bank_session_token.token = config.token;
+
+    if (config.randomize) {
+        bank_session_token.token = boost::uuids::to_string(boost::uuids::random_generator()());
+    }
     return bank_session_token;
 }
 
