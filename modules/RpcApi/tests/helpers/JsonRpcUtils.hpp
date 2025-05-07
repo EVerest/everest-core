80a0760979c181ccf00aac8814628f4cf3317807
Fabian Hartung <fabian.hartung@chargebyte.com>
2025-05-07 10:31:16 +0200

    RpcApi: add utility function to check key-value pair in JSON object and update tests
    Signed-off-by: Fabian Hartung <fabian.hartung@chargebyte.com>

--- modules/RpcApi/tests/helpers/JsonRpcUtils.hpp
+++ modules/RpcApi/tests/helpers/JsonRpcUtils.hpp
@@ -31,6 +31,18 @@ inline nlohmann::json create_json_rpc_error_response(int code, const std::string
     return error_response;
 }
 
+// To check if single key-value pair is part of a JSON object. Key-value pair must be stored in a JSON object.
+inline bool is_key_value_pair_in_json_object(const nlohmann::json& json_obj, const nlohmann::json& json_key_value) {
+    if (json_key_value.is_object()) {
+        for (const auto& [key, value] : json_key_value.items()) {
+            if (json_obj.contains(key) && json_obj[key] == value) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
 } // namespace json_rpc_utils
 
 #endif // JSON_RPC_UTILS_HPP
