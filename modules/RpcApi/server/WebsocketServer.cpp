646148908fe72df27b573f075afd6a314daf0785
Fabian Hartung <fabian.hartung@chargebyte.com>
2025-05-08 08:57:16 +0200

    RpcApi: solve confict with m_context (heap-use-after-free)
    Signed-off-by: Fabian Hartung <fabian.hartung@chargebyte.com>

--- modules/RpcApi/server/WebsocketServer.cpp
+++ modules/RpcApi/server/WebsocketServer.cpp
@@ -188,27 +188,17 @@ bool WebSocketServer::stop_server() {
     if (!m_running) {
         return true;
     }
-
-    if (!m_clients.empty()) {
-        for (auto& client : m_clients) {
-            lws_set_timeout(client.second, PENDING_TIMEOUT_CLOSE_SEND, LWS_TO_KILL_ASYNC);
-        }
-        m_clients.clear();
-    }
-
-    m_clients.clear();  // Clear the clients map
-
     m_running = false;
-    lws_cancel_service(m_context); // To stop the server thread loop
 
     if (m_server_thread.joinable()) {
         m_server_thread.join();  // Wait for server thread to finish
     }
+    lws_cancel_service(m_context); // To stop the server thread loop
+
     if (m_context) {
         lws_context_destroy(m_context);
         m_context = nullptr;
     }
-
     EVLOG_info << "WebSocket Server stopped";
 
     return true;
