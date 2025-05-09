54ce5b72877128b988eb0eb873ea81f0d71f077a
Kai Hermann <kai-uwe.hermann@pionix.de>
2025-05-09 12:48:24 +0200

    Skip HLC startup tests in GitHub CI (#1173)
    There seems to be an IPv6 issue with the current runner image: https://github.com/actions/runner-images/issues/12166
    This skips the AC and DC HLC tests when the EXT_MOUNT environment variable is set to "/ext", which is the case for our CI
    
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
    Signed-off-by: Abby Wheelis <Abigail.Wheelis@nrel.gov>

--- tests/core_tests/startup_tests.py
+++ tests/core_tests/startup_tests.py
@@ -4,6 +4,7 @@
 # Copyright 2020 - 2022 Pionix GmbH and Contributors to EVerest
 
 import logging
+import os
 import pytest
 import time
 import threading
@@ -128,6 +129,10 @@ async def test_001_start_test_module(everest_core: EverestCore):
 
 @pytest.mark.everest_core_config('config-sil.yaml')
 @pytest.mark.asyncio
+@pytest.mark.skipif(
+    os.getenv("EXT_MOUNT") == "/ext",
+    reason="HLC tests do not work without a IPv6 link-local address, which currently seems to be the case in GitHub CI"
+)
 async def test_002_start_test_module_ac_hlc(everest_core: EverestCore):
     logging.info(">>>>>>>>> test_002_start_test_module_ac_hlc <<<<<<<<<")
 
@@ -151,6 +156,10 @@ async def test_002_start_test_module_ac_hlc(everest_core: EverestCore):
 
 @pytest.mark.everest_core_config('config-sil-dc.yaml')
 @pytest.mark.asyncio
+@pytest.mark.skipif(
+    os.getenv("EXT_MOUNT") == "/ext",
+    reason="HLC tests do not work without a IPv6 link-local address, which currently seems to be the case in GitHub CI"
+)
 async def test_003_start_test_module_dc(everest_core: EverestCore):
     logging.info(">>>>>>>>> test_003_start_test_module_dc <<<<<<<<<")
 
