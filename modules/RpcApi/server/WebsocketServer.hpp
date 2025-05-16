ce9f730f97988073a4ca151be6c83423b2cc36bf
Moritz Barsnick <moritz.barsnick@chargebyte.com>
2025-05-16 14:56:27 +0200

    RpcApi: clang-format WebsocketServer.hpp
    Also fix a typo in the cpp.
    
    Signed-off-by: Moritz Barsnick <moritz.barsnick@chargebyte.com>

--- modules/RpcApi/server/WebsocketServer.hpp
+++ modules/RpcApi/server/WebsocketServer.hpp
@@ -1,21 +1,20 @@
 #ifndef WEBSOCKETSERVER_H
 #define WEBSOCKETSERVER_H
 
-#include <string>
-#include <unordered_map>
-#include <memory>
-#include <vector>
+#include <atomic>
 #include <boost/asio.hpp>
 #include <libwebsockets.h>
-#include <atomic>
+#include <memory>
+#include <string>
 #include <thread>
+#include <unordered_map>
+#include <vector>
 
 #include "TransportInterface.hpp"
 
 namespace server {
 
-class WebSocketServer : public TransportInterface
-{
+class WebSocketServer : public TransportInterface {
 public:
     // Constructor and Destructor
     explicit WebSocketServer(bool ssl_enabled, int port);
@@ -26,7 +25,7 @@ public:
     void send_data(const std::vector<uint8_t>& data) override;
     void send_data(const ClientId& client_id, const Data& data) override;
     void send_data(struct lws* wsi, const std::vector<uint8_t>& data);
-    void kill_client_connection(const ClientId &client_id, const std::string &kill_reason) override;
+    void kill_client_connection(const ClientId& client_id, const std::string& kill_reason) override;
     uint connections_count() const override;
 
     bool start_server() override;
@@ -37,16 +36,16 @@ private:
     bool m_ssl_enabled;
     struct lws_context_creation_info m_info {};
     struct lws_protocols m_lws_protocols[2];
-    std::atomic<bool> m_running {false};
-    struct lws_context *m_context = nullptr;
+    std::atomic<bool> m_running{false};
+    struct lws_context* m_context = nullptr;
     std::thread m_server_thread;
-    std::unordered_map<ClientId, struct lws *> m_clients;  // Client-Mapping
+    std::unordered_map<ClientId, struct lws*> m_clients; // Client-Mapping
     mutable std::mutex m_clients_mutex;
 
     // Methods
-    static int callback_ws(struct lws *wsi, enum lws_callback_reasons reason, void *user, void *in, size_t len);
+    static int callback_ws(struct lws* wsi, enum lws_callback_reasons reason, void* user, void* in, size_t len);
 };
 
-}
+} // namespace server
 
 #endif // WEBSOCKETSERVER_H
