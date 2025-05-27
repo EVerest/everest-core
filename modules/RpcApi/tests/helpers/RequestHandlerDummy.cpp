5b706303a93c776a78da35351bfcf1f9e440b5fc
Fabian Hartung <fabian.hartung@chargebyte.com>
2025-05-27 10:29:36 +0200

    RpcApi: Rename methods to use snake_case for consistency
    Signed-off-by: Fabian Hartung <fabian.hartung@chargebyte.com>

--- modules/RpcApi/tests/helpers/RequestHandlerDummy.cpp
+++ modules/RpcApi/tests/helpers/RequestHandlerDummy.cpp
@@ -2,31 +2,31 @@
 // Copyright chargebyte GmbH and Contributors to EVerest
 #include "RequestHandlerDummy.hpp"
 
-types::json_rpc_api::ErrorResObj RequestHandlerDummy::setChargingAllowed(const int32_t evse_index, bool charging_allowed) {
+types::json_rpc_api::ErrorResObj RequestHandlerDummy::set_charging_allowed(const int32_t evse_index, bool charging_allowed) {
     types::json_rpc_api::ErrorResObj res {types::json_rpc_api::ResponseErrorEnum::NoError};
     return res;
 }
-types::json_rpc_api::ErrorResObj RequestHandlerDummy::setACCharging(const int32_t evse_index, bool charging_allowed, bool max_current, std::optional<int> phase_count) {
+types::json_rpc_api::ErrorResObj RequestHandlerDummy::set_ac_charging(const int32_t evse_index, bool charging_allowed, bool max_current, std::optional<int> phase_count) {
     types::json_rpc_api::ErrorResObj res {types::json_rpc_api::ResponseErrorEnum::NoError};
     return res;
 }
-types::json_rpc_api::ErrorResObj RequestHandlerDummy::setACChargingCurrent(const int32_t evse_index, float max_current) {
+types::json_rpc_api::ErrorResObj RequestHandlerDummy::set_ac_charging_current(const int32_t evse_index, float max_current) {
     types::json_rpc_api::ErrorResObj res {res.error = types::json_rpc_api::ResponseErrorEnum::NoError};
     return res;
 }
-types::json_rpc_api::ErrorResObj RequestHandlerDummy::setACChargingPhaseCount(const int32_t evse_index, int phase_count) {
+types::json_rpc_api::ErrorResObj RequestHandlerDummy::set_ac_charging_phase_count(const int32_t evse_index, int phase_count) {
     types::json_rpc_api::ErrorResObj res {types::json_rpc_api::ResponseErrorEnum::NoError};
     return res;
 }
-types::json_rpc_api::ErrorResObj RequestHandlerDummy::setDCCharging(const int32_t evse_index, bool charging_allowed, float max_power) {
+types::json_rpc_api::ErrorResObj RequestHandlerDummy::set_dc_charging(const int32_t evse_index, bool charging_allowed, float max_power) {
     types::json_rpc_api::ErrorResObj res {types::json_rpc_api::ResponseErrorEnum::NoError};
     return res;
 }
-types::json_rpc_api::ErrorResObj RequestHandlerDummy::setDCChargingPower(const int32_t evse_index, float max_power) {
+types::json_rpc_api::ErrorResObj RequestHandlerDummy::set_dc_charging_power(const int32_t evse_index, float max_power) {
     types::json_rpc_api::ErrorResObj res {types::json_rpc_api::ResponseErrorEnum::NoError};
     return res;
 }
-types::json_rpc_api::ErrorResObj RequestHandlerDummy::enableConnector(const int32_t evse_index, int connector_id, bool enable, int priority) {
+types::json_rpc_api::ErrorResObj RequestHandlerDummy::enable_connector(const int32_t evse_index, int connector_id, bool enable, int priority) {
     types::json_rpc_api::ErrorResObj res {types::json_rpc_api::ResponseErrorEnum::NoError};
     return res;
 }
