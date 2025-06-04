ab03f01c9b87610b15d43b5ae4e7c94045c10c32
Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
2025-06-04 13:27:14 +0200

    Add test scaffolding for IsabellenhuetteIemDcr module
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>

---
+++ modules/IsabellenhuetteIemDcr/tests/isabellenhuette_iem_dcr_tests.cpp
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Pionix GmbH and Contributors to EVerest
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+namespace {
+
+TEST(DummyTest, test_simple_assertion) {
+    ASSERT_TRUE(true);
+}
+
+} // namespace
