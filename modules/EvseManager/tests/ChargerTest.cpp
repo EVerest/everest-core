3b32e65ca1e71b7dbdc22f35879c65cfb56af5d3
florinmihut <florinmihut1@gmail.com>
2025-06-27 16:08:31 +0200

    Add MRECC22 resistance fault error handling for isolation monitor
    Signed-off-by: florinmihut <florinmihut1@gmail.com>

--- modules/EvseManager/tests/ChargerTest.cpp
+++ modules/EvseManager/tests/ChargerTest.cpp
@@ -770,6 +770,9 @@ void ErrorHandling::raise_powermeter_transaction_start_failed_error(const std::s
 void ErrorHandling::clear_powermeter_transaction_start_failed_error() {
 }
 
+void ErrorHandling::raise_isolation_resistance_fault(const std::string& description) {}
+void ErrorHandling::clear_isolation_resistance_fault() {}
+
 // ----------------------------------------------------------------------------
 // SessionLog stub
 SessionLog::SessionLog() {
