9ad34357e778343f66783bcf281f9166804576a7
Fabian Hartung <fabian.hartung@chargebyte.com>
2025-07-22 15:41:05 +0200

    RpcApi: Refactor error handling to use ErrorObj type in handlers
    This is needed to avoid including the Everest::error::Error type in the
    tests. This type is framework specific and should not be used in the tests.
    
    Signed-off-by: Fabian Hartung <fabian.hartung@chargebyte.com>

--- modules/RpcApi/helpers/ErrorHandler.cpp
+++ modules/RpcApi/helpers/ErrorHandler.cpp
@@ -3,37 +3,34 @@
 
 #include "ErrorHandler.hpp"
 #include <everest/logging.hpp>
-#include "../helpers/Conversions.hpp"
 
 namespace helpers {
 
-void handle_error_raised(data::DataStoreCharger& data, const Everest::error::Error& error) {
-    auto tmp_error = types::json_rpc_api::everest_error_to_rpc_error(error);
+void handle_error_raised(data::DataStoreCharger& data, const types::json_rpc_api::ErrorObj& error) {
     try {
-        data.chargererrors.add_error(tmp_error);
+        data.chargererrors.add_error(error);
     } catch (const std::runtime_error& e) {
         EVLOG_warning << "Error while adding error to the data store: " << e.what();
     }
 
-    if (tmp_error.origin.evse_index.has_value()) {
-        auto tmp_evse_store = data::DataStoreCharger::get_evse_store(data, tmp_error.origin.evse_index.value());
+    if (error.origin.evse_index.has_value()) {
+        auto tmp_evse_store = data::DataStoreCharger::get_evse_store(data, error.origin.evse_index.value());
         if (tmp_evse_store != nullptr) {
             tmp_evse_store->evsestatus.set_error_present(true);
         } else {
-            EVLOG_warning << "No EVSE store found for EVSE index: " << tmp_error.origin.evse_index.value();
+            EVLOG_warning << "No EVSE store found for EVSE index: " << error.origin.evse_index.value();
         }
     }
 }
 
-void handle_error_cleared(data::DataStoreCharger& data, const Everest::error::Error& error) {
-    auto tmp_error = types::json_rpc_api::everest_error_to_rpc_error(error);
+void handle_error_cleared(data::DataStoreCharger& data, const types::json_rpc_api::ErrorObj& error) {
     try {
-        data.chargererrors.clear_error(tmp_error);
+        data.chargererrors.clear_error(error);
     } catch (const std::runtime_error& e) {
         EVLOG_warning << "Error while clearing error from the data store: " << e.what();
     }
 
-    if (!tmp_error.origin.evse_index.has_value()) {
+    if (!error.origin.evse_index.has_value()) {
         return;
     }
 
@@ -41,12 +38,12 @@ void handle_error_cleared(data::DataStoreCharger& data, const Everest::error::Er
     if (tmp_charger_errors.has_value()) {
         for (const auto& charger_error : tmp_charger_errors.value()) {
             if (charger_error.origin.evse_index.has_value() &&
-                charger_error.origin.evse_index.value() == tmp_error.origin.evse_index.value()) {
+                charger_error.origin.evse_index.value() == error.origin.evse_index.value()) {
                 return;
             }
         }
     }
-    auto tmp_evse_store = data::DataStoreCharger::get_evse_store(data, tmp_error.origin.evse_index.value());
+    auto tmp_evse_store = data::DataStoreCharger::get_evse_store(data, error.origin.evse_index.value());
     if (tmp_evse_store != nullptr) {
         tmp_evse_store->evsestatus.set_error_present(false);
     }
