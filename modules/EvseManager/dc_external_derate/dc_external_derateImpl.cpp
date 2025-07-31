0c5886590821dcb5e066482852af7774c9488701
Piet Gömpel <pietgoempel@gmail.com>
2025-07-31 09:38:27 +0200

    Added skeleton for DC external derate interface
    Signed-off-by: Piet Gömpel <pietgoempel@gmail.com>

---
+++ modules/EvseManager/dc_external_derate/dc_external_derateImpl.cpp
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright Pionix GmbH and Contributors to EVerest
+
+#include "dc_external_derateImpl.hpp"
+
+namespace module {
+namespace dc_external_derate {
+
+void dc_external_derateImpl::init() {
+}
+
+void dc_external_derateImpl::ready() {
+}
+
+void dc_external_derateImpl::handle_set_external_capabilities(std::string& capabilities) {
+    // your code for cmd set_external_capabilities goes here
+}
+
+} // namespace dc_external_derate
+} // namespace module
