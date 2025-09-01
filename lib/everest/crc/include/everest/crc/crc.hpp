16acd32eed61e3e72e72716f7190681c0ec79996
Piet Gömpel <pietgoempel@gmail.com>
2025-09-01 09:25:36 +0200

    Added crc library + tests
    Signed-off-by: Piet Gömpel <pietgoempel@gmail.com>

---
+++ lib/everest/crc/include/everest/crc/crc.hpp
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2020 - 2025 Pionix GmbH and Contributors to EVerest
+
+#ifndef CRC16_HPP
+#define CRC16_HPP
+#include <stdint.h>
+#include <vector>
+
+uint16_t calculate_xModem_crc16(const std::vector<uint8_t>& message);
+#endif
