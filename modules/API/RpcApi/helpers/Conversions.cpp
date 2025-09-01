da7f9c3b3f2d0940a3e2eb87ea614c5bc025e890
Fabian Hartung <fabian.hartung@chargebyte.com>
2025-09-01 10:37:45 +0200

    RpcApi: Removed unnecessary types::json_rpc_api namespace usage
    Signed-off-by: Fabian Hartung <fabian.hartung@chargebyte.com>

--- modules/API/RpcApi/helpers/Conversions.cpp
+++ modules/API/RpcApi/helpers/Conversions.cpp
@@ -8,15 +8,14 @@ namespace json_rpc_api {
 
 EVSEStateEnum evse_manager_session_event_to_evse_state(types::evse_manager::SessionEvent state) {
     using Event = types::evse_manager::SessionEventEnum;
-    using State = types::json_rpc_api::EVSEStateEnum;
 
     switch (state.event) {
     case Event::Enabled:
-        return State::Unplugged;
+        return EVSEStateEnum::Unplugged;
     case Event::Disabled:
-        return State::Disabled;
+        return EVSEStateEnum::Disabled;
     case Event::AuthRequired:
-        return State::AuthRequired;
+        return EVSEStateEnum::AuthRequired;
     case Event::PrepareCharging:
         [[fallthrough]];
     case Event::SessionStarted:
@@ -24,40 +23,40 @@ EVSEStateEnum evse_manager_session_event_to_evse_state(types::evse_manager::Sess
     case Event::SessionResumed:
         [[fallthrough]];
     case Event::TransactionStarted:
-        return State::Preparing;
+        return EVSEStateEnum::Preparing;
     case Event::ChargingResumed:
         [[fallthrough]];
     case Event::ChargingStarted:
-        return State::Charging;
+        return EVSEStateEnum::Charging;
     case Event::ChargingPausedEV:
-        return State::ChargingPausedEV;
+        return EVSEStateEnum::ChargingPausedEV;
     case Event::ChargingPausedEVSE:
-        return State::ChargingPausedEVSE;
+        return EVSEStateEnum::ChargingPausedEVSE;
     case Event::WaitingForEnergy:
-        return State::WaitingForEnergy;
+        return EVSEStateEnum::WaitingForEnergy;
     case Event::ChargingFinished:
-        return State::Finished;
+        return EVSEStateEnum::Finished;
     case Event::StoppingCharging:
-        return State::FinishedEV;
+        return EVSEStateEnum::FinishedEV;
     case Event::TransactionFinished: {
         if (state.transaction_finished.has_value() &&
             state.transaction_finished->reason == types::evse_manager::StopTransactionReason::Local) {
-            return State::FinishedEVSE;
+            return EVSEStateEnum::FinishedEVSE;
         } else {
-            return State::Finished;
+            return EVSEStateEnum::Finished;
         }
         break;
     }
     case Event::PluginTimeout:
-        return State::AuthTimeout;
+        return EVSEStateEnum::AuthTimeout;
     case Event::ReservationStart:
-        return State::Reserved;
+        return EVSEStateEnum::Reserved;
     case Event::ReservationEnd:
         [[fallthrough]];
     case Event::SessionFinished:
-        return State::Unplugged;
+        return EVSEStateEnum::Unplugged;
     case Event::SwitchingPhases:
-        return State::SwitchingPhases;
+        return EVSEStateEnum::SwitchingPhases;
     case Event::ReplugStarted:
         [[fallthrough]];
     case Event::ReplugFinished:
@@ -67,7 +66,7 @@ EVSEStateEnum evse_manager_session_event_to_evse_state(types::evse_manager::Sess
     case Event::Deauthorized:
         [[fallthrough]];
     default:
-        return State::Unknown;
+        return EVSEStateEnum::Unknown;
     }
 }
 
@@ -87,23 +86,23 @@ ChargeProtocolEnum evse_manager_protocol_to_charge_protocol(const std::string& p
     }
 }
 
-types::json_rpc_api::ErrorObj everest_error_to_rpc_error(const Everest::error::Error& error_object) {
-    std::vector<types::json_rpc_api::ErrorObj> rpc_errors;
+ErrorObj everest_error_to_rpc_error(const Everest::error::Error& error_object) {
+    std::vector<ErrorObj> rpc_errors;
 
-    types::json_rpc_api::ErrorObj rpc_error;
+    ErrorObj rpc_error;
     rpc_error.type = error_object.type;
     rpc_error.description = error_object.description;
     rpc_error.message = error_object.message;
 
     switch (error_object.severity) {
     case Everest::error::Severity::High:
-        rpc_error.severity = types::json_rpc_api::Severity::High;
+        rpc_error.severity = Severity::High;
         break;
     case Everest::error::Severity::Medium:
-        rpc_error.severity = types::json_rpc_api::Severity::Medium;
+        rpc_error.severity = Severity::Medium;
         break;
     case Everest::error::Severity::Low:
-        rpc_error.severity = types::json_rpc_api::Severity::Low;
+        rpc_error.severity = Severity::Low;
         break;
     default:
         throw std::out_of_range("Provided severity " + std::to_string(static_cast<int>(error_object.severity)) +
@@ -129,64 +128,64 @@ types::json_rpc_api::ErrorObj everest_error_to_rpc_error(const Everest::error::E
     return rpc_error;
 }
 
-std::vector<types::json_rpc_api::EnergyTransferModeEnum> iso15118_energy_transfer_modes_to_json_rpc_api(
+std::vector<EnergyTransferModeEnum> iso15118_energy_transfer_modes_to_json_rpc_api(
     const std::vector<types::iso15118::EnergyTransferMode>& supported_energy_transfer_modes,
     bool& is_ac_transfer_mode) {
-    std::vector<types::json_rpc_api::EnergyTransferModeEnum> tmp{};
+    std::vector<EnergyTransferModeEnum> tmp{};
     is_ac_transfer_mode = false;
 
     for (const auto& mode : supported_energy_transfer_modes) {
         switch (mode) {
         case types::iso15118::EnergyTransferMode::AC_single_phase_core:
-            tmp.push_back(types::json_rpc_api::EnergyTransferModeEnum::AC_single_phase_core);
+            tmp.push_back(EnergyTransferModeEnum::AC_single_phase_core);
             is_ac_transfer_mode = true;
             break;
         case types::iso15118::EnergyTransferMode::AC_two_phase:
-            tmp.push_back(types::json_rpc_api::EnergyTransferModeEnum::AC_two_phase);
+            tmp.push_back(EnergyTransferModeEnum::AC_two_phase);
             is_ac_transfer_mode = true;
             break;
         case types::iso15118::EnergyTransferMode::AC_three_phase_core:
-            tmp.push_back(types::json_rpc_api::EnergyTransferModeEnum::AC_three_phase_core);
+            tmp.push_back(EnergyTransferModeEnum::AC_three_phase_core);
             is_ac_transfer_mode = true;
             break;
         case types::iso15118::EnergyTransferMode::DC_core:
-            tmp.push_back(types::json_rpc_api::EnergyTransferModeEnum::DC_core);
+            tmp.push_back(EnergyTransferModeEnum::DC_core);
             break;
         case types::iso15118::EnergyTransferMode::DC_extended:
-            tmp.push_back(types::json_rpc_api::EnergyTransferModeEnum::DC_extended);
+            tmp.push_back(EnergyTransferModeEnum::DC_extended);
             break;
         case types::iso15118::EnergyTransferMode::DC_combo_core:
-            tmp.push_back(types::json_rpc_api::EnergyTransferModeEnum::DC_combo_core);
+            tmp.push_back(EnergyTransferModeEnum::DC_combo_core);
             break;
         case types::iso15118::EnergyTransferMode::DC_unique:
-            tmp.push_back(types::json_rpc_api::EnergyTransferModeEnum::DC_unique);
+            tmp.push_back(EnergyTransferModeEnum::DC_unique);
             break;
         case types::iso15118::EnergyTransferMode::DC:
-            tmp.push_back(types::json_rpc_api::EnergyTransferModeEnum::DC);
+            tmp.push_back(EnergyTransferModeEnum::DC);
             break;
         case types::iso15118::EnergyTransferMode::AC_BPT:
-            tmp.push_back(types::json_rpc_api::EnergyTransferModeEnum::AC_BPT);
+            tmp.push_back(EnergyTransferModeEnum::AC_BPT);
             is_ac_transfer_mode = true;
             break;
         case types::iso15118::EnergyTransferMode::AC_BPT_DER:
-            tmp.push_back(types::json_rpc_api::EnergyTransferModeEnum::AC_BPT_DER);
+            tmp.push_back(EnergyTransferModeEnum::AC_BPT_DER);
             is_ac_transfer_mode = true;
             break;
         case types::iso15118::EnergyTransferMode::AC_DER:
-            tmp.push_back(types::json_rpc_api::EnergyTransferModeEnum::AC_DER);
+            tmp.push_back(EnergyTransferModeEnum::AC_DER);
             is_ac_transfer_mode = true;
             break;
         case types::iso15118::EnergyTransferMode::DC_BPT:
-            tmp.push_back(types::json_rpc_api::EnergyTransferModeEnum::DC_BPT);
+            tmp.push_back(EnergyTransferModeEnum::DC_BPT);
             break;
         case types::iso15118::EnergyTransferMode::DC_ACDP:
-            tmp.push_back(types::json_rpc_api::EnergyTransferModeEnum::DC_ACDP);
+            tmp.push_back(EnergyTransferModeEnum::DC_ACDP);
             break;
         case types::iso15118::EnergyTransferMode::DC_ACDP_BPT:
-            tmp.push_back(types::json_rpc_api::EnergyTransferModeEnum::DC_ACDP_BPT);
+            tmp.push_back(EnergyTransferModeEnum::DC_ACDP_BPT);
             break;
         case types::iso15118::EnergyTransferMode::WPT:
-            tmp.push_back(types::json_rpc_api::EnergyTransferModeEnum::WPT);
+            tmp.push_back(EnergyTransferModeEnum::WPT);
             is_ac_transfer_mode = true; // TBD
             break;
         default:
@@ -197,9 +196,9 @@ std::vector<types::json_rpc_api::EnergyTransferModeEnum> iso15118_energy_transfe
     return tmp;
 }
 
-void to_json(json& j, const types::json_rpc_api::EnergyTransferModeEnum& k) {
+void to_json(json& j, const EnergyTransferModeEnum& k) {
     // the required parts of the type
-    j = types::json_rpc_api::energy_transfer_mode_enum_to_string(k);
+    j = energy_transfer_mode_enum_to_string(k);
 }
 
 } // namespace json_rpc_api
