81a8c14b3453090aa8189f9bb66eee7139b52f80
Jan <154529400+djchhp@users.noreply.github.com>
2025-09-05 08:57:25 +0200

    Adding lib_everest_api_types (#1339)
    Signed-off-by: Jan Christoph Habig <jan.habig@pionix.de>

---
+++ lib/everest/everest_api_types/include/everest_api_types/isolation_monitor/API.hpp
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2020 - 2025 Pionix GmbH and Contributors to EVerest
+
+#pragma once
+
+#include <optional>
+#include <string>
+
+namespace everest::lib::API::V1_0::types::isolation_monitor {
+
+struct IsolationMeasurement {
+    float resistance_F_Ohm;
+    std::optional<float> voltage_V;
+};
+
+enum class ErrorEnum {
+    DeviceFault,
+    CommunicationFault,
+    VendorError,
+    VendorWarning
+};
+
+struct Error {
+    ErrorEnum type;
+    std::optional<std::string> sub_type;
+    std::optional<std::string> message;
+};
+
+} // namespace everest::lib::API::V1_0::types::isolation_monitor
