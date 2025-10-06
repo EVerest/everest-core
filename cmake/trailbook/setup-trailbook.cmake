b71f61339ee354febb488209d38a537709f291c4
Andreas Heinrich <andreas.heinrich@rwth-aachen.de>
2025-10-06 18:53:03 +0200

    Add doc to buildsystem
    Signed-off-by: Andreas Heinrich <andreas.heinrich@rwth-aachen.de>

---
+++ cmake/trailbook/setup-trailbook.cmake
@@ -0,0 +1,50 @@
+macro(_find_sphinx_build)
+    find_program(
+        _SPHINX_BUILD_EXECUTABLE
+        NAMES 
+            "${Python3_EXECUTABLE} -m sphinx.cmd.build"
+            "sphinx-build"
+        DOC "Path to the sphinx-build executable"
+        OPTIONAL
+        PATHS
+            $ENV{VIRTUAL_ENV}/bin
+    )
+endmacro()
+
+macro(_find_and_fix_sphinx_build)
+    _find_sphinx_build()
+
+    if("${_SPHINX_BUILD_EXECUTABLE}" STREQUAL "_SPHINX_BUILD_EXECUTABLE-NOTFOUND")
+        ev_is_python_venv_active(
+            RESULT_VAR IS_PYTHON_VENV_ACTIVE
+        )
+        if(IS_PYTHON_VENV_ACTIVE)
+            message(STATUS "sphinx-build executable not found in system, but python venv is active. Trying to use 'python3 -m pip install sphinx'.")
+            execute_process(
+                COMMAND ${Python3_EXECUTABLE} -m pip install sphinx
+            )
+            _find_sphinx_build()
+        endif()
+    endif()
+
+    if("${_SPHINX_BUILD_EXECUTABLE}" STREQUAL "_SPHINX_BUILD_EXECUTABLE-NOTFOUND")
+        message(FATAL_ERROR "sphinx-build executable not found. Please install Sphinx. You can install it via pip: pip install sphinx")
+    endif()
+
+    message(STATUS "Found sphinx-build: ${_SPHINX_BUILD_EXECUTABLE}")
+endmacro()
+
+# Internal macro to set up the trailbook environment.
+# This macro should only be called once.
+macro(_setup_trailbook)
+    if(_TRAILBOOK_SETUP_DONE)
+        message(FATAL_ERROR 
+            "setup-trailbook.cmake: _setup_trailbook() has already been called.
+            This function should only be called once."
+        )
+    endif()
+
+    _find_and_fix_sphinx_build()
+
+    set(_TRAILBOOK_SETUP_DONE TRUE)
+endmacro()
