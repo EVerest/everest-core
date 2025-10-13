9030f045a91aced8e3f70c74fa132db9630f73ae
Jan Christoph Habig <jan.habig@pionix.de>
2025-10-13 10:51:37 +0200

    Improvements on event_fd
    * new common base class event_fd_base
    * specializations for semaphore_fd and event_fd

--- lib/everest/io/include/everest/io/event/event_fd.hpp
+++ lib/everest/io/include/everest/io/event/event_fd.hpp
@@ -12,15 +12,29 @@
 namespace everest::lib::io::event {
 
 /**
- * event_fd creates an <a href="https://man7.org/linux/man-pages/man2/eventfd.2.html">event</a>.
- * The lifetime of the event is bound to the lifetime of this object. Events can be queued
+ * event_fd_base creates an <a href="https://man7.org/linux/man-pages/man2/eventfd.2.html">event</a>.
+ * The lifetime of the event is bound to the lifetime of this object.
  */
-class event_fd {
+class event_fd_base {
 public:
     /**
      * @brief Constructor
+     * @param[in] initval The initial value for the internal counter of the eventfd
+     * @param[in] flags for the creation of the internal eventfd.
      */
-    event_fd();
+    event_fd_base(unsigned int initval, int flags);
+
+    /**
+     * @brief Declared virtual to ensure proper cleanup via base class pointer,
+     * but defaulted to use the compiler-generated implementation.
+     */
+    virtual ~event_fd_base() = default;
+
+    event_fd_base(const event_fd_base&) = delete;
+    event_fd_base& operator=(const event_fd_base&) = delete;
+
+    event_fd_base(event_fd_base&&) = default;
+    event_fd_base& operator=(event_fd_base&&) = default;
 
     /**
      * @brief Explicit conversion to file descriptor
@@ -41,17 +55,21 @@ public:
     bool valid() const;
 
     /**
-     * @brief Read from the event queue
-     * @details Read an event from the event queue. Poll on this object will
-     * return immediately until all events have been handled.
-     * @return The value read from the event. This is the value previously written with \ref write
+     * @brief Read from the eventfd
+     * @details This returns the value of the eventfd internal counter.
+     * Return immediately is the counter is non-zero. Depending on wether the eventfd
+     * is used as a semaphore or not, calling this function either decrements the internal counter
+     * (semaphore) or resets it to zero.
+     * @return The value of the event counter read from the eventfd.
      * If the event cannot be read, the optional is a 'nullopt'
      */
     std::optional<std::uint64_t> read();
 
     /**
-     * @brief Write to the event queue
-     * @details Add an element to the event queue
+     * @brief Write to the eventfd
+     * @details Adds 'data' to the eventfds internal counter.
+     * This call blocks if adding 'data' to the internal counter would exceed the maximum value.
+     * A call to read() is necessary to unblock.
      * @param[in] data Payload of the event
      * @return True on success, false otherwise
      */
@@ -59,13 +77,31 @@ public:
 
     /**
      * @brief Add a single event with default payload '1' to the event queue
-     * @details Calles \ref write internally
+     * @details Calles \ref write(1) internally.
      * @return True on success, false otherwise
      */
     bool notify();
 
 private:
     unique_fd m_fd;
+};
+
+/**
+ * event_fd creates a blocking eventfd with initial value '0'
+ * The lifetime of the event is bound to the lifetime of this object.
+ */
+class event_fd : public event_fd_base {
+public:
+    event_fd();
+};
+
+/**
+ * event_fd creates a blocking eventfd as semaphore with initial value '0'
+ * The lifetime of the event is bound to the lifetime of this object.
+ */
+class semaphore_fd : public event_fd_base {
+public:
+    semaphore_fd();
 };
 
 } // namespace everest::lib::io::event
