f9a962ba3d27388cadf70613fb4d02c35e57a32e
Jan Christoph Habig <jan.habig@pionix.de>
2025-10-13 10:51:37 +0200

    Improvements on event_fd
    * new common base class event_fd_base
    * specializations for semaphore_fd and event_fd

--- lib/everest/io/src/event/event_fd.cpp
+++ lib/everest/io/src/event/event_fd.cpp
@@ -8,25 +8,25 @@
 
 namespace everest::lib::io::event {
 
-event_fd::event_fd() : m_fd(::eventfd(0, 0)) {
+event_fd_base::event_fd_base(unsigned int initval, int flags) : m_fd(::eventfd(initval, flags)) {
     if (m_fd == -1) {
         throw std::runtime_error("failed to create an eventfd");
     }
 }
 
-event_fd::operator int() const {
+event_fd_base::operator int() const {
     return get_raw_fd();
 }
 
-int event_fd::get_raw_fd() const {
+int event_fd_base::get_raw_fd() const {
     return m_fd;
 }
 
-bool event_fd::valid() const {
+bool event_fd_base::valid() const {
     return m_fd != unique_fd::NO_DESCRIPTOR_SENTINEL;
 }
 
-std::optional<uint64_t> event_fd::read() {
+std::optional<uint64_t> event_fd_base::read() {
     eventfd_t eventfd_buffer{0};
     if (eventfd_read(m_fd, &eventfd_buffer) == 0) {
         return eventfd_buffer;
@@ -34,12 +34,18 @@ std::optional<uint64_t> event_fd::read() {
     return std::nullopt;
 }
 
-bool event_fd::write(std::uint64_t data) {
+bool event_fd_base::write(std::uint64_t data) {
     return eventfd_write(m_fd, data) == 0;
 }
 
-bool event_fd::notify() {
+bool event_fd_base::notify() {
     return write(1);
+}
+
+event_fd::event_fd() : event_fd_base(0, 0) {
+}
+
+semaphore_fd::semaphore_fd() : event_fd_base(0, EFD_SEMAPHORE) {
 }
 
 } // namespace everest::lib::io::event
