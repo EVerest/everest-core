c883913d0ae9f3d2e542c093418e25e6769d5e6d
James Chapman <james.chapman@pionix.de>
2025-11-03 11:19:22 +0000

    Squashed 'everest-core/' content from commit 3beacf9b
    git-subtree-dir: everest-core
    git-subtree-split: 3beacf9bd5efe1109ff53f58a40f68582bfe99b3

---
+++ lib/everest/everest_api_types/src/everest_api_types/isolation_monitor/wrapper.cpp
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2020 - 2025 Pionix GmbH and Contributors to EVerest
+
+#include "isolation_monitor/wrapper.hpp"
+#include "isolation_monitor/API.hpp"
+
+namespace everest::lib::API::V1_0::types::isolation_monitor {
+
+IsolationMeasurement_Internal to_internal_api(IsolationMeasurement_External const& val) {
+    IsolationMeasurement_Internal result;
+    result.resistance_F_Ohm = val.resistance_F_Ohm;
+    result.voltage_V = val.voltage_V;
+    return result;
+}
+IsolationMeasurement_External to_external_api(IsolationMeasurement_Internal const& val) {
+    IsolationMeasurement_External result;
+    result.resistance_F_Ohm = val.resistance_F_Ohm;
+    result.voltage_V = val.voltage_V;
+    return result;
+}
+
+} // namespace everest::lib::API::V1_0::types::isolation_monitor
