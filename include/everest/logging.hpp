224baf8ab10943e86c4647e6e82a872d9cf33f24
James Chapman <james.chapman@pionix.de>
2025-11-03 13:08:29 +0000

    Squashed 'liblog/' changes from 97a221e4..08ff519b
    08ff519b Support reinitialization of the logger (#30)
    d6896ddf Fix typo for LOG_INSTALL variable (#28)
    24c1a5de Rename target log to everest_log to avoid name collisions (#26)
    9fa45080 Change CI to use reusable workflow (#27)
    763a56a4 Add escaped message to simplify parsing of logs (#25)
    3eab950e Bump version of used github action run-clang-format to v1.1.0 (#20)
    d07aaa98 Bugfix/fix ci (#19)
    6e80d58d Add GitHub Actions CI/CD Run for liblog (#16)
    19dce0ec Indicate that liblog needs C++17 (#14)
    5c132fb0 Use std::filesystem by default but allow fallback to boost:filesystem (#13)
    03b49840 CMake refactoring
    fd1dbb1d Set host for cross compiling (#10)
    0a900760 beautify logging output
    8ba68a81 improve logging
    a88820f3 Add compiler warnings (#8)
    6f21ab83 Fix wrong license header for metamacros (#7)
    6b1e6fde Disable shallow clone for libbacktrace dependency (#6)
    db074490 Fix build in ninja by specifying libbacktrace.a as a build byproduct. (#5)
    e0fe306e Added libbacktrace into cmake workflow (#4)
    f3a09f0c Build backtrace support by default.
    634588ed Make backtrace support optional (#3)
    ebdad59f Adding backtracing functionality (#2)
    7c353383 Kh add license headers (#1)
    302158c1 add code style configs
    
    git-subtree-dir: liblog
    git-subtree-split: 08ff519b647beaa51f8f25ab04b88c079ca253a7

--- include/everest/logging.hpp
+++ include/everest/logging.hpp
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2020 - 2022 Pionix GmbH and Contributors to EVerest
 #ifndef LOGGING_HPP
 #define LOGGING_HPP
 
@@ -17,8 +19,8 @@
 namespace Everest {
 namespace Logging {
 
-enum severity_level
-{
+enum severity_level {
+    verbose,
     debug,
     info,
     warning,
@@ -26,16 +28,43 @@ enum severity_level
     critical,
 };
 
+/// \brief Initialize a completely silenced logger
+void init();
+
+/// \brief Initialize logger using the config pointed to by \p logconf
 void init(const std::string& logconf);
+
+/// \brief Initialize logger using the config pointed to by \p logconf additionally setting the \p process_name
 void init(const std::string& logconf, std::string process_name);
+
 void update_process_name(std::string process_name);
+std::string trace();
 } // namespace Logging
 
 // clang-format off
-#define EVLOG(severity)                                                                                                \
-    BOOST_LOG_SEV(::global_logger::get(), ::Everest::Logging::severity)                                                \
+#define EVLOG_verbose                                                                                                  \
+    BOOST_LOG_SEV(::global_logger::get(), ::Everest::Logging::verbose)                                                 \
         << boost::log::BOOST_LOG_VERSION_NAMESPACE::add_value("file", __FILE__)                                        \
         << boost::log::BOOST_LOG_VERSION_NAMESPACE::add_value("line", __LINE__)                                        \
+        << boost::log::BOOST_LOG_VERSION_NAMESPACE::add_value("function", BOOST_CURRENT_FUNCTION)
+
+#define EVLOG_debug                                                                                                    \
+    BOOST_LOG_SEV(::global_logger::get(), ::Everest::Logging::debug)                                                   \
+        << boost::log::BOOST_LOG_VERSION_NAMESPACE::add_value("function", BOOST_CURRENT_FUNCTION)
+
+#define EVLOG_info                                                                                                     \
+    BOOST_LOG_SEV(::global_logger::get(), ::Everest::Logging::info)
+
+#define EVLOG_warning                                                                                                  \
+    BOOST_LOG_SEV(::global_logger::get(), ::Everest::Logging::warning)                                                 \
+        << boost::log::BOOST_LOG_VERSION_NAMESPACE::add_value("function", BOOST_CURRENT_FUNCTION)
+
+#define EVLOG_error                                                                                                    \
+    BOOST_LOG_SEV(::global_logger::get(), ::Everest::Logging::error)                                                   \
+        << boost::log::BOOST_LOG_VERSION_NAMESPACE::add_value("function", BOOST_CURRENT_FUNCTION)
+
+#define EVLOG_critical                                                                                                 \
+    BOOST_LOG_SEV(::global_logger::get(), ::Everest::Logging::critical)                                                \
         << boost::log::BOOST_LOG_VERSION_NAMESPACE::add_value("function", BOOST_CURRENT_FUNCTION)
 // clang-format on
 
@@ -45,7 +74,7 @@ void update_process_name(std::string process_name);
             BOOST_THROW_EXCEPTION(boost::enable_error_info(ex)                                                         \
                                   << boost::log::BOOST_LOG_VERSION_NAMESPACE::current_scope());                        \
         } catch (std::exception & e) {                                                                                 \
-            EVLOG(error) << e.what();                                                                                  \
+            EVLOG_error << e.what();                                                                                   \
             throw;                                                                                                     \
         }                                                                                                              \
     } while (0)
@@ -59,6 +88,6 @@ void update_process_name(std::string process_name);
 
 BOOST_LOG_INLINE_GLOBAL_LOGGER_DEFAULT(
     global_logger,
-    boost::log::BOOST_LOG_VERSION_NAMESPACE::sources::severity_logger_mt<Everest::Logging::severity_level>);
+    boost::log::BOOST_LOG_VERSION_NAMESPACE::sources::severity_logger_mt<Everest::Logging::severity_level>)
 
 #endif // LOGGING_HPP
