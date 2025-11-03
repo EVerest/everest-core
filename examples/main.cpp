84cd773b2b44c0f9b44992a08abcd7b0936a9693
James Chapman <james.chapman@pionix.de>
2025-11-03 13:08:37 +0000

    Squashed 'libtimer/' changes from c0a0af59..445ae5d7
    445ae5d7 Bugfix: Replace deprecated usage of boost::asio::basic_waitable_timer::expires_from_now with expires_after (#12)
    2f2b5c76 Fix missing whitespace in tests/CMakeLists.txt (#13)
    9ce401c9 Use reusable worklfow v.1.4.6 (#14)
    173e96e8 Bump version of used github action run-clang-format to v1.1.0 (#10)
    ccec2f89 Adding test and ci to repo (#8)
    d7ee45a5 deleting dynamically allocated memory in destructor of timer
    b720662d stopping io_context and joining timer_thread in Timer destructor
    d6036624 CMake refactoring
    ef673912 Removed obsolete EVEREST_CROSS_COMPILE option
    67181c26 Use system tzdata
    4dd60e98 Switched to utc_clock (#2)
    7ec3d74d Add license headers (#1)
    bbc8e2f6 add code style configs
    
    git-subtree-dir: libtimer
    git-subtree-split: 445ae5d7022c98603c00119506798ada85923e54

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,4 +1,8 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2020 - 2021 Pionix GmbH and Contributors to EVerest
 #include <chrono>
+#include <date/date.h>
+#include <date/tz.h>
 #include <iostream>
 #include <thread>
 
@@ -28,7 +32,7 @@ int main(int argc, char* argv[]) {
 
     boost::asio::io_service io_service;
 
-    std::cout << "start time: " << std::chrono::system_clock::now() << std::endl;
+    std::cout << "start time: " << date::utc_clock::now() << std::endl;
 
     int count_t1 = 0;
     int count_t2 = 0;
@@ -37,44 +41,38 @@ int main(int argc, char* argv[]) {
     Everest::SteadyTimer* t1 = new Everest::SteadyTimer(&io_service);
     t1->at(
         [&]() {
-            std::cout << "t1 (asio) after 5s: " << std::chrono::system_clock::now() << std::endl;
+            std::cout << "t1 (asio) after 5s: " << date::utc_clock::now() << std::endl;
             t1->interval(
                 [&]() {
-                    std::cout << "t1 (asio) interval (1s): " << std::chrono::system_clock::now() << std::endl;
+                    std::cout << "t1 (asio) interval (1s): " << date::utc_clock::now() << std::endl;
                     count_t1++;
                     if (count_t1 > 3) {
                         t1->timeout(
-                            []() {
-                                std::cout << "t1 (asio) timeout (3s): " << std::chrono::system_clock::now()
-                                          << std::endl;
-                            },
+                            []() { std::cout << "t1 (asio) timeout (3s): " << date::utc_clock::now() << std::endl; },
                             3s);
                     }
                 },
                 1s);
         },
-        std::chrono::steady_clock::now() + 5s);
+        date::utc_clock::now() + 5s);
 
     Everest::SteadyTimer* t2 = new Everest::SteadyTimer();
     t2->at(
         [&]() {
-            std::cout << "t2 (thread) after 12s: " << std::chrono::system_clock::now() << std::endl;
+            std::cout << "t2 (thread) after 12s: " << date::utc_clock::now() << std::endl;
             t2->interval(
                 [&]() {
-                    std::cout << "t2 (thread) interval (1s): " << std::chrono::system_clock::now() << std::endl;
+                    std::cout << "t2 (thread) interval (1s): " << date::utc_clock::now() << std::endl;
                     count_t2++;
                     if (count_t2 > 3) {
                         t2->timeout(
-                            []() {
-                                std::cout << "t2 (thread) timeout (3s): " << std::chrono::system_clock::now()
-                                          << std::endl;
-                            },
+                            []() { std::cout << "t2 (thread) timeout (3s): " << date::utc_clock::now() << std::endl; },
                             3s);
                     }
                 },
                 1s);
         },
-        std::chrono::steady_clock::now() + 12s);
+        date::utc_clock::now() + 12s);
 
     io_service.run();
 
