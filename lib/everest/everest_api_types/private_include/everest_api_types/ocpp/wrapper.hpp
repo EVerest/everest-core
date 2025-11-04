5b64cfae1ce5ca12fc3d6e80e6382ec1bdc3372b
James Chapman <james.chapman@pionix.de>
2025-11-04 14:23:13 +0000

    Added OCPP stable API for monitor variables
    Signed-off-by: James Chapman <james.chapman@pionix.de>
    
    Added OCPP stable API for EventData
    
    Signed-off-by: James Chapman <james.chapman@pionix.de>
    
    Added OCPP stable API for charging schedules
    
    Signed-off-by: James Chapman <james.chapman@pionix.de>
    
    Allow all valid names for enums and structs to be parsed
    
    Signed-off-by: Christoph Burandt <christoph.burandt@pionix.de>
    
    Change double->float; std::int32_t->int32_t
    
    Signed-off-by: Christoph Burandt <christoph.burandt@pionix.de>
    
    Add missing to_internal_api implementations + fix to_external_api functions
    
    Signed-off-by: Christoph Burandt <christoph.burandt@pionix.de>
    
    fix: corrected optional vector handling so that unit tests pass
    
    Signed-off-by: James Chapman <james.chapman@pionix.de>
    
    savepoint: starting to add bringup module for teating OCPP consumer API
    
    Signed-off-by: James Chapman <james.chapman@pionix.de>
    
    fix: monitor variables had the wrong subscribe string
    feat: added bringup support for get/set variables
    
    Signed-off-by: James Chapman <james.chapman@pionix.de>
    
    fix: monitor_variables API didn't match the EVerest internal one - updated
    feat: added monitor support to BUOcppConsumer
    
    Signed-off-by: James Chapman <james.chapman@pionix.de>

--- lib/everest/everest_api_types/private_include/everest_api_types/ocpp/wrapper.hpp
+++ lib/everest/everest_api_types/private_include/everest_api_types/ocpp/wrapper.hpp
@@ -12,154 +12,60 @@
 
 namespace everest::lib::API::V1_0::types::ocpp {
 
-using AttributeEnum_Internal = ::types::ocpp::AttributeEnum;
-using AttributeEnum_External = AttributeEnum;
+#define create_interface(A)                                                                                            \
+    using A##_Internal = ::types::ocpp::A;                                                                             \
+    using A##_External = A;                                                                                            \
+    A##_Internal to_internal_api(A##_External const& val);                                                             \
+    A##_External to_external_api(A##_Internal const& val)
 
-AttributeEnum_Internal to_internal_api(AttributeEnum_External const& val);
-AttributeEnum_External to_external_api(AttributeEnum_Internal const& val);
+#define create_interface2(A, B)                                                                                        \
+    using A##_Internal = B;                                                                                            \
+    using A##_External = A;                                                                                            \
+    A##_Internal to_internal_api(A##_External const& val);                                                             \
+    A##_External to_external_api(A##_Internal const& val)
 
-using GetVariableStatusEnumType_Internal = ::types::ocpp::GetVariableStatusEnumType;
-using GetVariableStatusEnumType_External = GetVariableStatusEnumType;
+create_interface(AttributeEnum);
+create_interface(GetVariableStatusEnumType);
+create_interface(SetVariableStatusEnumType);
+create_interface(EventTriggerEnum);
+create_interface(EventNotificationType);
+create_interface(DataTransferStatus);
+create_interface(RegistrationStatus);
+create_interface(TransactionEvent);
+create_interface(CustomData);
+create_interface(DataTransferRequest);
+create_interface(DataTransferResponse);
+create_interface(EVSE);
+create_interface(Component);
+create_interface(Variable);
+create_interface(ComponentVariable);
+create_interface(GetVariableRequest);
+create_interface(GetVariableResult);
+create_interface(SetVariableRequest);
+create_interface(SetVariableResult);
+create_interface2(GetVariableRequestList, std::vector<::types::ocpp::GetVariableRequest>);
+create_interface2(GetVariableResultList, std::vector<::types::ocpp::GetVariableResult>);
+create_interface2(SetVariableRequestList, std::vector<::types::ocpp::SetVariableRequest>);
+create_interface2(SetVariableResultList, std::vector<::types::ocpp::SetVariableResult>);
+create_interface(SecurityEvent);
+create_interface(StatusInfoType);
+create_interface(BootNotificationResponse);
+create_interface(OcppTransactionEvent);
 
-GetVariableStatusEnumType_Internal to_internal_api(GetVariableStatusEnumType_External const& val);
-GetVariableStatusEnumType_External to_external_api(GetVariableStatusEnumType_Internal const& val);
+// MonitorVariableRequest is a ComponentVariable
+using MonitorVariableRequest_Internal = ::types::ocpp::ComponentVariable;
+using MonitorVariableRequest_External = ComponentVariable;
 
-using SetVariableStatusEnumType_Internal = ::types::ocpp::SetVariableStatusEnumType;
-using SetVariableStatusEnumType_External = SetVariableStatusEnumType;
+create_interface2(MonitorVariableRequestList, std::vector<::types::ocpp::ComponentVariable>);
+create_interface(EventData);
+create_interface(ChargingSchedules);
+create_interface(ChargingSchedule);
+create_interface(ChargingSchedulePeriod);
+create_interface2(OperationMode, ::types::ocpp::Operation_mode);
+create_interface(V2XSignalWattPointCurve);
+create_interface(V2XFreqWattPointType);
 
-SetVariableStatusEnumType_Internal to_internal_api(SetVariableStatusEnumType_External const& val);
-SetVariableStatusEnumType_External to_external_api(SetVariableStatusEnumType_Internal const& val);
-
-using DataTransferStatus_Internal = ::types::ocpp::DataTransferStatus;
-using DataTransferStatus_External = DataTransferStatus;
-
-DataTransferStatus_Internal to_internal_api(DataTransferStatus_External const& val);
-DataTransferStatus_External to_external_api(DataTransferStatus_Internal const& val);
-
-using RegistrationStatus_Internal = ::types::ocpp::RegistrationStatus;
-using RegistrationStatus_External = RegistrationStatus;
-
-RegistrationStatus_Internal to_internal_api(RegistrationStatus_External const& val);
-RegistrationStatus_External to_external_api(RegistrationStatus_Internal const& val);
-
-using TransactionEvent_Internal = ::types::ocpp::TransactionEvent;
-using TransactionEvent_External = TransactionEvent;
-
-TransactionEvent_Internal to_internal_api(TransactionEvent_External const& val);
-TransactionEvent_External to_external_api(TransactionEvent_Internal const& val);
-
-using CustomData_Internal = ::types::ocpp::CustomData;
-using CustomData_External = CustomData;
-
-CustomData_Internal to_internal_api(CustomData_External const& val);
-CustomData_External to_external_api(CustomData_Internal const& val);
-
-using DataTransferRequest_Internal = ::types::ocpp::DataTransferRequest;
-using DataTransferRequest_External = DataTransferRequest;
-
-DataTransferRequest_Internal to_internal_api(DataTransferRequest_External const& val);
-DataTransferRequest_External to_external_api(DataTransferRequest_Internal const& val);
-
-using DataTransferResponse_Internal = ::types::ocpp::DataTransferResponse;
-using DataTransferResponse_External = DataTransferResponse;
-
-DataTransferResponse_Internal to_internal_api(DataTransferResponse_External const& val);
-DataTransferResponse_External to_external_api(DataTransferResponse_Internal const& val);
-
-using EVSE_Internal = ::types::ocpp::EVSE;
-using EVSE_External = EVSE;
-
-EVSE_Internal to_internal_api(EVSE_External const& val);
-EVSE_External to_external_api(EVSE_Internal const& val);
-
-using Component_Internal = ::types::ocpp::Component;
-using Component_External = Component;
-
-Component_Internal to_internal_api(Component_External const& val);
-Component_External to_external_api(Component_Internal const& val);
-
-using Variable_Internal = ::types::ocpp::Variable;
-using Variable_External = Variable;
-
-Variable_Internal to_internal_api(Variable_External const& val);
-Variable_External to_external_api(Variable_Internal const& val);
-
-using ComponentVariable_Internal = ::types::ocpp::ComponentVariable;
-using ComponentVariable_External = ComponentVariable;
-
-ComponentVariable_Internal to_internal_api(ComponentVariable_External const& val);
-ComponentVariable_External to_external_api(ComponentVariable_Internal const& val);
-
-using GetVariableRequest_Internal = ::types::ocpp::GetVariableRequest;
-using GetVariableRequest_External = GetVariableRequest;
-
-GetVariableRequest_Internal to_internal_api(GetVariableRequest_External const& val);
-GetVariableRequest_External to_external_api(GetVariableRequest_Internal const& val);
-
-using GetVariableResult_Internal = ::types::ocpp::GetVariableResult;
-using GetVariableResult_External = GetVariableResult;
-
-GetVariableResult_Internal to_internal_api(GetVariableResult_External const& val);
-GetVariableResult_External to_external_api(GetVariableResult_Internal const& val);
-
-using SetVariableRequest_Internal = ::types::ocpp::SetVariableRequest;
-using SetVariableRequest_External = SetVariableRequest;
-
-SetVariableRequest_Internal to_internal_api(SetVariableRequest_External const& val);
-SetVariableRequest_External to_external_api(SetVariableRequest_Internal const& val);
-
-using SetVariableResult_Internal = ::types::ocpp::SetVariableResult;
-using SetVariableResult_External = SetVariableResult;
-
-SetVariableResult_Internal to_internal_api(SetVariableResult_External const& val);
-SetVariableResult_External to_external_api(SetVariableResult_Internal const& val);
-
-using GetVariableRequestList_Internal = std::vector<::types::ocpp::GetVariableRequest>;
-using GetVariableRequestList_External = GetVariableRequestList;
-
-GetVariableRequestList_Internal to_internal_api(GetVariableRequestList_External const& val);
-GetVariableRequestList_External to_external_api(GetVariableRequestList_Internal const& val);
-
-using GetVariableResultList_Internal = std::vector<::types::ocpp::GetVariableResult>;
-using GetVariableResultList_External = GetVariableResultList;
-
-GetVariableResultList_Internal to_internal_api(GetVariableResultList_External const& val);
-GetVariableResultList_External to_external_api(GetVariableResultList_Internal const& val);
-
-using SetVariableRequestList_Internal = std::vector<::types::ocpp::SetVariableRequest>;
-using SetVariableRequestList_External = SetVariableRequestList;
-
-SetVariableRequestList_Internal to_internal_api(SetVariableRequestList_External const& val);
-SetVariableRequestList_External to_external_api(SetVariableRequestList_Internal const& val);
-
-using SetVariableResultList_Internal = std::vector<::types::ocpp::SetVariableResult>;
-using SetVariableResultList_External = SetVariableResultList;
-
-SetVariableResultList_Internal to_internal_api(SetVariableResultList_External const& val);
-SetVariableResultList_External to_external_api(SetVariableResultList_Internal const& val);
-
-using SecurityEvent_Internal = ::types::ocpp::SecurityEvent;
-using SecurityEvent_External = SecurityEvent;
-
-SecurityEvent_Internal to_internal_api(SecurityEvent_External const& val);
-SecurityEvent_External to_external_api(SecurityEvent_Internal const& val);
-
-using StatusInfoType_Internal = ::types::ocpp::StatusInfoType;
-using StatusInfoType_External = StatusInfoType;
-
-StatusInfoType_Internal to_internal_api(StatusInfoType_External const& val);
-StatusInfoType_External to_external_api(StatusInfoType_Internal const& val);
-
-using BootNotificationResponse_Internal = ::types::ocpp::BootNotificationResponse;
-using BootNotificationResponse_External = BootNotificationResponse;
-
-BootNotificationResponse_Internal to_internal_api(BootNotificationResponse_External const& val);
-BootNotificationResponse_External to_external_api(BootNotificationResponse_Internal const& val);
-
-using OcppTransactionEvent_Internal = ::types::ocpp::OcppTransactionEvent;
-using OcppTransactionEvent_External = OcppTransactionEvent;
-
-OcppTransactionEvent_Internal to_internal_api(OcppTransactionEvent_External const& val);
-OcppTransactionEvent_External to_external_api(OcppTransactionEvent_Internal const& val);
+#undef create_interface
+#undef create_interface2
 
 } // namespace everest::lib::API::V1_0::types::ocpp
---
+++ modules/BringUp/BUOcppConsumer/docs/index.rst
@@ -0,0 +1,23 @@
+.. _everest_modules_handwritten_BUOcppConsumer:
+
+..  This file is a placeholder for optional multiple files
+    handwritten documentation for the BUOcppConsumer module.
+    Please decide whether you want to use the doc.rst file
+    or a set of files in the doc/ directory.
+    In the latter case, you can delete the doc.rst file.
+    In the former case, you can delete the doc/ directory.
+    
+..  This handwritten documentation is optional. In case
+    you do not want to write it, you can delete this file
+    and the doc/ directory.
+
+..  The documentation can be written in reStructuredText,
+    and will be converted to HTML and PDF by Sphinx.
+    This index.rst file is the entry point for the module documentation.
+
+*******************************************
+BUOcppConsumer
+*******************************************
+
+:ref:`Link <everest_modules_BUOcppConsumer>` to the module's reference.
+Interactive bring up helper for OCPP consumer interface
