7e4186d72d3caf76c90896e0401b70f6fab4753e
Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
2025-12-02 19:58:49 +0100

    Remove 3rd party dependency hash_library
    Replaced with everest::tls sha256 openssl wrapper
    
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>

--- lib/everest/libslac/src/slac.cpp
+++ lib/everest/libslac/src/slac.cpp
@@ -9,7 +9,7 @@
 #include <arpa/inet.h>
 #include <endian.h>
 
-#include <hash_library/sha256.h>
+#include <everest/tls/openssl_util.hpp>
 
 namespace slac {
 namespace utils {
@@ -22,45 +22,37 @@ namespace utils {
 //   - for the generation of NID, the NMK is fed into sha256, so having
 //     a const char* as input should be the proper byte ordering already
 void generate_nmk_hs(uint8_t nmk_hs[slac::defs::NMK_LEN], const char* plain_password, int password_len) {
-    SHA256 sha256;
-
     // do pbkdf1 (use sha256 as hashing function, iterate 1000 times,
     // use salt)
-    sha256.add(plain_password, password_len);
-    sha256.add(slac::defs::NMK_HASH, sizeof(slac::defs::NMK_HASH));
-
-    uint8_t hash[SHA256::HashBytes];
-    sha256.getHash(hash);
+    std::vector<std::uint8_t> input(plain_password, plain_password + password_len);
+    input.insert(input.end(), slac::defs::NMK_HASH_ARR.begin(), slac::defs::NMK_HASH_ARR.end());
+    openssl::sha_256_digest_t digest;
+    openssl::sha_256(input.data(), input.size(), digest);
     for (int i = 0; i < 1000 - 1; ++i) {
-        sha256(hash, sizeof(hash));
-        sha256.getHash(hash);
+        openssl::sha_256(digest.data(), openssl::sha_256_digest_size, digest);
     }
 
-    memcpy(nmk_hs, hash, slac::defs::NMK_LEN);
+    memcpy(nmk_hs, digest.data(), slac::defs::NMK_LEN);
 }
 
 void generate_nid_from_nmk(uint8_t nid[slac::defs::NID_LEN], const uint8_t nmk[slac::defs::NMK_LEN]) {
-    SHA256 sha256;
-
     // msb of least significant octet of NMK should be the leftmost bit
     // of the input, which corresponds to the usual const char* order
 
     // do pbkdf1 (use sha256 as hashing function, iterate 5 times, no
     // salt)
-    uint8_t hash[SHA256::HashBytes];
-    sha256(nmk, slac::defs::NMK_LEN);
-    sha256.getHash(hash);
+    openssl::sha_256_digest_t digest;
+    openssl::sha_256(nmk, slac::defs::NMK_LEN, digest);
     for (int i = 0; i < 5 - 1; ++i) {
-        sha256(hash, sizeof(hash));
-        sha256.getHash(hash);
+        openssl::sha_256(digest.data(), openssl::sha_256_digest_size, digest);
     }
 
     // use leftmost 52 bits of the hash output
     // left most bit should be bit 7 of the nid
-    memcpy(nid, hash, slac::defs::NID_LEN - 1); // (bits 52 - 5)
+    memcpy(nid, digest.data(), slac::defs::NID_LEN - 1); // (bits 52 - 5)
     nid[slac::defs::NID_LEN - 1] =
         (slac::defs::NID_SECURITY_LEVEL_SIMPLE_CONNECT << slac::defs::NID_SECURITY_LEVEL_OFFSET) |
-        ((static_cast<uint8_t>(hash[6])) >> slac::defs::NID_MOST_SIGNIFANT_BYTE_SHIFT);
+        ((static_cast<uint8_t>(digest.data()[6])) >> slac::defs::NID_MOST_SIGNIFANT_BYTE_SHIFT);
 }
 
 } // namespace utils
