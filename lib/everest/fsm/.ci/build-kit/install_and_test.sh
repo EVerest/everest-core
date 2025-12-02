9f84e3d620915242cbb82a37d9f7691fbb1d6172
Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
2025-12-02 20:35:24 +0100

    Integrate libfsm main (v0.2.0 + 2 commits)
    This contains the libfsm source code from its last v0.2.0 release including 2 unreleased commits that have since been merged into its main.
    This version of libslac can be found at https://github.com/EVerest/libfsm/commit/0c796dbc5cae110f2a08acc56f7f0234ffde82f1
    
    Co-authored-by: Andreas Heinrich <andreas.heinrich@rwth-aachen.de>
    Co-authored-by: aw <aw@pionix.de>
    Co-authored-by: Christopher Davis <150722105+christopher-davis-afs@users.noreply.github.com>
    Co-authored-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
    
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>

---
+++ lib/everest/fsm/.ci/build-kit/install_and_test.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -e
+
+cmake \
+    -B build \
+    -G Ninja \
+    -S "$EXT_MOUNT/source" \
+    -DBUILD_EXAMPLES=ON \
+    -DBUILD_TESTING=ON \
+    -DCMAKE_BUILD_TYPE=Debug \
+    -DCMAKE_INSTALL_PREFIX="$WORKSPACE_PATH/dist"
+
+ninja -j$(nproc) -C build install
+
+ninja -j$(nproc) -C build test
+
+cp build/Testing/Temporary/LastTest.log /ext/ctest-report
