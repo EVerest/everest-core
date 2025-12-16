7d902114d5d287ed422a3d119d07f23a0a650b02
Kacper Dalach <kdalach@elektrometal.com.pl>
2025-12-16 12:17:16 +0100

    libiso15118: d2: Add ServiceDiscovery req/res
    Signed-off-by: Kacper Dalach <kdalach@elektrometal.com.pl>

---
+++ lib/everest/iso15118/src/iso15118/message/d2/service_discovery.cpp
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2025 Pionix GmbH and Contributors to EVerest
+#include <algorithm>
+#include <iso15118/message/d2/service_discovery.hpp>
+
+#include <iso15118/detail/variant_access.hpp>
+
+#include <cbv2g/iso_2/iso2_msgDefDecoder.h>
+#include <cbv2g/iso_2/iso2_msgDefEncoder.h>
+
+#include <iso15118/detail/helper.hpp>
+
+namespace iso15118::d2::msg {
+
+template <> void convert(const data_types::Service& in, struct iso2_ServiceType& out) {
+    out.ServiceID = in.service_id;
+    CPP2CB_STRING_IF_USED(in.service_name, out.ServiceName);
+    cb_convert_enum(in.service_category, out.ServiceCategory);
+    CPP2CB_STRING_IF_USED(in.service_scope, out.ServiceScope);
+    out.FreeService = in.FreeService;
+}
+
+template <> void convert(const data_types::ChargeService& in, struct iso2_ChargeServiceType& out) {
+    out.ServiceID = in.service_id;
+    CPP2CB_STRING_IF_USED(in.service_name, out.ServiceName);
+    cb_convert_enum(in.service_category, out.ServiceCategory);
+    CPP2CB_STRING_IF_USED(in.service_scope, out.ServiceScope);
+    out.FreeService = in.FreeService;
+
+    int modeIndex = 0;
+    for (auto const& mode : in.supported_energy_transfer_mode) {
+        // TODO(kd) guard for accessing iso2_ struct array out of bounds?
+        cb_convert_enum(mode, out.SupportedEnergyTransferMode.EnergyTransferMode.array[modeIndex++]);
+    }
+    out.SupportedEnergyTransferMode.EnergyTransferMode.arrayLen = in.supported_energy_transfer_mode.size();
+}
+
+template <> void convert(const struct iso2_ServiceDiscoveryReqType& in, ServiceDiscoveryRequest& out) {
+    if (in.ServiceCategory_isUsed) {
+        d2::msg::data_types::ServiceCategory category{};
+        cb_convert_enum(in.ServiceCategory, category);
+        out.service_category = category;
+    }
+    CB2CPP_STRING_IF_USED(in.ServiceScope, out.service_scope);
+}
+
+template <>
+void insert_type(VariantAccess& va, const struct iso2_ServiceDiscoveryReqType& in,
+                 const struct iso2_MessageHeaderType& header) {
+    va.insert_type<ServiceDiscoveryRequest>(in, header);
+}
+
+template <> void convert(const ServiceDiscoveryResponse& in, struct iso2_ServiceDiscoveryResType& out) {
+    init_iso2_ServiceDiscoveryResType(&out);
+
+    cb_convert_enum(in.response_code, out.ResponseCode);
+
+    int optionIndex = 0;
+    for (auto const& option : in.payment_option_list) {
+        cb_convert_enum(option, out.PaymentOptionList.PaymentOption.array[optionIndex++]);
+    }
+    out.PaymentOptionList.PaymentOption.arrayLen = in.payment_option_list.size();
+
+    convert(in.charge_service, out.ChargeService);
+
+    int serviceIndex = 0;
+    if (in.service_list) {
+        for (auto const& service : in.service_list.value()) {
+            convert(service, out.ServiceList.Service.array[serviceIndex++]);
+        }
+        out.ServiceList.Service.arrayLen = in.service_list->size();
+        CB_SET_USED(out.ServiceList);
+    }
+}
+
+template <> int serialize_to_exi(const ServiceDiscoveryResponse& in, exi_bitstream_t& out) {
+
+    iso2_exiDocument doc;
+    init_iso2_exiDocument(&doc);
+    init_iso2_BodyType(&doc.V2G_Message.Body);
+
+    convert(in.header, doc.V2G_Message.Header);
+
+    CB_SET_USED(doc.V2G_Message.Body.ServiceDiscoveryRes);
+    convert(in, doc.V2G_Message.Body.ServiceDiscoveryRes);
+
+    return encode_iso2_exiDocument(&out, &doc);
+}
+
+template <> size_t serialize(const ServiceDiscoveryResponse& in, const io::StreamOutputView& out) {
+    return serialize_helper(in, out);
+}
+
+} // namespace iso15118::d2::msg
