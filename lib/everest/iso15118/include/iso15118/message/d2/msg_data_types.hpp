6ea023e14819941cbe466b27ba9eec79373facbd
Kacper Dalach <kdalach@elektrometal.com.pl>
2025-12-16 13:12:27 +0100

    libiso15118: Add -2 authorization messages
    Signed-off-by: Kacper Dalach <kdalach@elektrometal.com.pl>

--- lib/everest/iso15118/include/iso15118/message/d2/msg_data_types.hpp
+++ lib/everest/iso15118/include/iso15118/message/d2/msg_data_types.hpp
@@ -17,6 +17,9 @@ namespace data_types {
 constexpr auto SESSION_ID_LENGTH = 8;
 using SESSION_ID = std::array<uint8_t, SESSION_ID_LENGTH>; // hexBinary, max length 8
 
+constexpr auto GEN_CHALLENGE_LENGTH = 16;
+using GEN_CHALLENGE = std::array<char, GEN_CHALLENGE_LENGTH>; // base64 binary
+
 enum class ResponseCode {
     OK,
     OK_NewSessionEstablished,
@@ -50,6 +53,12 @@ enum class FaultCode {
     ParsingError,
     NoTLSRootCertificatAvailable,
     UnknownError,
+};
+
+enum class EVSEProcessing {
+    Finished,
+    Ongoing,
+    Ongoing_WaitingForCustomerInteraction
 };
 
 struct Notification {
