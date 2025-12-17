beb3ef8aaaafce50a01f047ae08ebcc985b1dee1
Kacper Dalach <kdalach@elektrometal.com.pl>
2025-12-17 07:37:01 +0100

    libiso15118: Add EV/EVSE status structs
    These structs are a common part in many messages.
    
    Signed-off-by: Kacper Dalach <kdalach@elektrometal.com.pl>

--- lib/everest/iso15118/include/iso15118/message/d2/msg_data_types.hpp
+++ lib/everest/iso15118/include/iso15118/message/d2/msg_data_types.hpp
@@ -17,6 +17,8 @@ namespace data_types {
 constexpr auto SESSION_ID_LENGTH = 8;
 using SESSION_ID = std::array<uint8_t, SESSION_ID_LENGTH>; // hexBinary, max length 8
 
+using PercentValue = uint8_t; // [0 - 100]
+
 enum class ResponseCode {
     OK,
     OK_NewSessionEstablished,
@@ -52,9 +54,73 @@ enum class FaultCode {
     UnknownError,
 };
 
+enum class DC_EVErrorCode {
+    NO_ERROR,
+    FAILED_RESSTemperatureInhibit,
+    FAILED_EVShiftPosition,
+    FAILED_ChargerConnectorLockFault,
+    FAILED_EVRESSMalfunction,
+    FAILED_ChargingCurrentdifferential,
+    FAILED_ChargingVoltageOutOfRange,
+    Reserved_A,
+    Reserved_B,
+    Reserved_C,
+    FAILED_ChargingSystemIncompatibility,
+    NoData,
+};
+
+enum class EVSENotification {
+    None,
+    StopCharging,
+    ReNegotiation,
+};
+
+enum class DC_EVSEStatusCode {
+    EVSE_NotReady,
+    EVSE_Ready,
+    EVSE_Shutdown,
+    EVSE_UtilityInterruptEvent,
+    EVSE_IsolationMonitoringActive,
+    EVSE_EmergencyShutdown,
+    EVSE_Malfunction,
+    Reserved_8,
+    Reserved_9,
+    Reserved_A,
+    Reserved_B,
+    Reserved_C
+};
+
+enum class isolationLevel {
+    Invalid,
+    Valid,
+    Warning,
+    Fault,
+    No_IMD
+};
+
 struct Notification {
     FaultCode fault_code;
     std::optional<std::string> fault_msg;
+};
+
+struct EVSEStatus {
+    uint16_t notification_max_delay{0};
+    EVSENotification evse_notification{EVSENotification::None};
+};
+
+struct AC_EVSEStatus : public EVSEStatus {
+    bool rcd;
+};
+
+struct DC_EVSEStatus : public EVSEStatus {
+    std::optional<isolationLevel> evse_isolation_status;
+    DC_EVSEStatusCode evse_status_code;
+};
+
+struct DC_EVStatus {
+    bool ev_ready;
+    DC_EVErrorCode ev_error_code;
+    PercentValue ev_ress_soc;
 };
 
 } // namespace data_types
@@ -67,5 +133,9 @@ struct Header {
 
 void convert(const struct iso2_MessageHeaderType& in, Header& out);
 void convert(const Header& in, struct iso2_MessageHeaderType& out);
+
+void convert(const iso2_DC_EVStatusType& in, data_types::DC_EVStatus& out);
+void convert(const data_types::AC_EVSEStatus& in, iso2_AC_EVSEStatusType& out);
+void convert(const data_types::DC_EVSEStatus& in, iso2_DC_EVSEStatusType& out);
 
 } // namespace iso15118::d2::msg
