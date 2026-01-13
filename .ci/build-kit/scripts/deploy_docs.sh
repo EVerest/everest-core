62f48b3239c1126942d033068b2759114c4a6924
Piet Gömpel <pietgoempel@gmail.com>
2026-01-13 20:27:28 +0100

    address codacy issues
    Signed-off-by: Piet Gömpel <pietgoempel@gmail.com>

--- .ci/build-kit/scripts/deploy_docs.sh
+++ .ci/build-kit/scripts/deploy_docs.sh
@@ -6,17 +6,17 @@ mkdir -p ~/.ssh
 ssh-keyscan github.com >> ~/.ssh/known_hosts
 chmod 600 ~/.ssh/known_hosts
 
-git -C $DEPLOYED_DOCS_REPO config user.email "compiler@pionix.de"
-git -C $DEPLOYED_DOCS_REPO config user.name "Pionix Github Service Account"
+git -C "$DEPLOYED_DOCS_REPO" config user.email "compiler@pionix.de"
+git -C "$DEPLOYED_DOCS_REPO" config user.name "Pionix Github Service Account"
 
-git -C $DEPLOYED_DOCS_REPO add .
+git -C "$DEPLOYED_DOCS_REPO" add .
 retVal=$?
 if [ $retVal -ne 0 ]; then
     echo "Staging changes failed with return code $retVal"
     exit $retVal
 fi
 
-git -C $DEPLOYED_DOCS_REPO commit -m "Update nightly documentation from commit $GITHUB_SHA"
+git -C "$DEPLOYED_DOCS_REPO" commit -m "Update nightly documentation from commit $GITHUB_SHA"
 retVal=$?
 if [ $retVal -ne 0 ]; then
     echo "Committing changes failed with return code $retVal"
