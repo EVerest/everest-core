f367eecda454fe5c49fcbc471396667d55347ccf
Christoph Burandt <christoph.burandt@pionix.de>
2026-02-02 17:13:23 +0100

    Fix(API): Remove 'receive_' prefix from isolation_monitor_API topics (#1783)
    Signed-off-by: Christoph Burandt <christoph.burandt@pionix.de>

--- modules/API/isolation_monitor_API/main/isolation_monitorImpl.cpp
+++ modules/API/isolation_monitor_API/main/isolation_monitorImpl.cpp
@@ -18,16 +18,16 @@ void isolation_monitorImpl::ready() {
 }
 
 void isolation_monitorImpl::handle_start() {
-    mod->mqtt.publish(mod->get_topics().everest_to_extern("receive_start"), "{}");
+    mod->mqtt.publish(mod->get_topics().everest_to_extern("start"), "{}");
 }
 
 void isolation_monitorImpl::handle_stop() {
-    mod->mqtt.publish(mod->get_topics().everest_to_extern("receive_stop"), "{}");
+    mod->mqtt.publish(mod->get_topics().everest_to_extern("stop"), "{}");
 }
 
 void isolation_monitorImpl::handle_start_self_test(double& test_voltage_V) {
     auto value = API_generic::serialize(test_voltage_V);
-    mod->mqtt.publish(mod->get_topics().everest_to_extern("receive_start_self_test"), value);
+    mod->mqtt.publish(mod->get_topics().everest_to_extern("start_self_test"), value);
 }
 
 } // namespace main
