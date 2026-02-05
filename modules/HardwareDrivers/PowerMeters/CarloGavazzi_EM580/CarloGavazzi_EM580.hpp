04144895fa18daec9fd704f7e00a76647f3ac6b6
Florin Mihut <florinmihut1@gmail.com>
2026-02-05 15:28:47 +0100

    Fix formatting
    Signed-off-by: Florin Mihut <florinmihut1@gmail.com>

--- modules/HardwareDrivers/PowerMeters/CarloGavazzi_EM580/CarloGavazzi_EM580.hpp
+++ modules/HardwareDrivers/PowerMeters/CarloGavazzi_EM580/CarloGavazzi_EM580.hpp
@@ -27,32 +27,35 @@ struct Conf {};
 
 class CarloGavazzi_EM580 : public Everest::ModuleBase {
 public:
-    CarloGavazzi_EM580() = delete;
-    CarloGavazzi_EM580(const ModuleInfo& info, std::unique_ptr<powermeterImplBase> p_main,
-                       std::unique_ptr<serial_communication_hubIntf> r_modbus, Conf& config) :
-        ModuleBase(info), p_main(std::move(p_main)), r_modbus(std::move(r_modbus)), config(config){};
+  CarloGavazzi_EM580() = delete;
+  CarloGavazzi_EM580(const ModuleInfo &info,
+                     std::unique_ptr<powermeterImplBase> p_main,
+                     std::unique_ptr<serial_communication_hubIntf> r_modbus,
+                     Conf &config)
+      : ModuleBase(info), p_main(std::move(p_main)),
+        r_modbus(std::move(r_modbus)), config(config){};
 
-    const std::unique_ptr<powermeterImplBase> p_main;
-    const std::unique_ptr<serial_communication_hubIntf> r_modbus;
-    const Conf& config;
+  const std::unique_ptr<powermeterImplBase> p_main;
+  const std::unique_ptr<serial_communication_hubIntf> r_modbus;
+  const Conf &config;
 
-    // ev@1fce4c5e-0ab8-41bb-90f7-14277703d2ac:v1
-    // insert your public definitions here
-    // ev@1fce4c5e-0ab8-41bb-90f7-14277703d2ac:v1
+  // ev@1fce4c5e-0ab8-41bb-90f7-14277703d2ac:v1
+  // insert your public definitions here
+  // ev@1fce4c5e-0ab8-41bb-90f7-14277703d2ac:v1
 
 protected:
-    // ev@4714b2ab-a24f-4b95-ab81-36439e1478de:v1
-    // insert your protected definitions here
-    // ev@4714b2ab-a24f-4b95-ab81-36439e1478de:v1
+  // ev@4714b2ab-a24f-4b95-ab81-36439e1478de:v1
+  // insert your protected definitions here
+  // ev@4714b2ab-a24f-4b95-ab81-36439e1478de:v1
 
 private:
-    friend class LdEverest;
-    void init();
-    void ready();
+  friend class LdEverest;
+  void init();
+  void ready();
 
-    // ev@211cfdbe-f69a-4cd6-a4ec-f8aaa3d1b6c8:v1
-    // insert your private definitions here
-    // ev@211cfdbe-f69a-4cd6-a4ec-f8aaa3d1b6c8:v1
+  // ev@211cfdbe-f69a-4cd6-a4ec-f8aaa3d1b6c8:v1
+  // insert your private definitions here
+  // ev@211cfdbe-f69a-4cd6-a4ec-f8aaa3d1b6c8:v1
 };
 
 // ev@087e516b-124c-48df-94fb-109508c7cda9:v1
