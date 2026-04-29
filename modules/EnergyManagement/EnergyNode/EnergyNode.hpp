f3f83646c651c2df0106dc362978689239703ac0
James Chapman <james.chapman@pionix.de>
2026-04-29 13:12:00 +0100

    feat: updated C++ modules via ev-cli
    Signed-off-by: James Chapman <james.chapman@pionix.de>

--- modules/EnergyManagement/EnergyNode/EnergyNode.hpp
+++ modules/EnergyManagement/EnergyNode/EnergyNode.hpp
@@ -26,11 +26,16 @@
 
 namespace module {
 
+struct RwConf {};
+
 struct Conf {
     double fuse_limit_A;
     int phase_count;
     bool enhance_external_schedule;
     double nominal_voltage_V;
+
+    Conf() = default;
+    Conf(const RwConf&){};
 };
 
 class EnergyNode : public Everest::ModuleBase {
